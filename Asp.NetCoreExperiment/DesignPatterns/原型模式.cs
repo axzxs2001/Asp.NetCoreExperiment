2963f44db3f600df78364309d9fb94643271d432
桂素伟 <axzxs2001@163.com>
2017-04-20 15:43:22 +0800

    原型模式完成。

---
+++ Asp.NetCoreExperiment/DesignPatterns/原型模式.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    /****************************************************************************
+    * 原型模式
+    * 用原型实例指定创建对象的种类，并且通过拷贝这些原型创建新对象。
+    ****************************************************************************/
+    /// <summary>
+    /// 抽象类
+    /// </summary>
+    public abstract class Prototype
+    {       
+        public Prototype(string id)
+        {
+            ID = id;
+        }
+        public string ID
+        {
+            get;
+            private set;
+        }
+        public abstract Prototype Clone();
+    }
+    /// <summary>
+    /// 子类
+    /// </summary>
+    public class ConcretePrototype1 : Prototype
+    {
+        public ConcretePrototype1(string id) : base(id)
+        {
+        }
+        public override Prototype Clone()
+        {
+            Console.WriteLine("ConcretePrototype1.Clone");
+            return (Prototype)this.MemberwiseClone();
+        }
+    }
+}
