cffcf1661cf6460bbbdc8d231dfbd707069487c7
桂素伟 <axzxs2001@163.com>
2017-04-20 14:32:46 +0800

    完成装饰模式

--- Asp.NetCoreExperiment/DesignPatterns/Program.cs
+++ Asp.NetCoreExperiment/DesignPatterns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace DesignPatterns
 {
@@ -6,14 +7,54 @@ namespace DesignPatterns
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("选择模式编号：");
-            switch (Console.ReadLine())
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            while (true)
             {
-                case "1":
-                    var opertion = OperationFactory.CreateOperate(1);
-                    opertion.GetResult();
-                    break;
+                Console.WriteLine("==================================");
+                Console.WriteLine("1、简单工厂  2、策略模式  3、装饰模式");
+                Console.WriteLine("==================================");
+                Console.WriteLine("选择模式编号：");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Invock1();
+                        break;
+                    case "2":
+                        Invock2();
+                        break;
+                    case "3":
+                        Invock3();
+                        break;
+                }
             }
+        }
+        /// <summary>
+        /// 简单工厂客户端
+        /// </summary>
+        static void Invock1()
+        {
+            var opertion = OperationFactory.CreateOperate(1);
+            opertion.GetResult();
+        }
+        /// <summary>
+        /// 策略模式客户端
+        /// </summary>
+        static void Invock2()
+        {
+            var context = new Context("1");
+            context.GetCompute();
+        }
+        /// <summary>
+        /// 装饰模式客户端
+        /// </summary>
+        static void Invock3()
+        {
+            var c = new ConcreteComponent();
+            var dA = new ConcreteDecoratorA();
+            var dB = new ConcreteDecoratorB();
+            dA.Component = c;
+            dB.Component = dA;
+            dB.Operation();
         }
     }
 }
