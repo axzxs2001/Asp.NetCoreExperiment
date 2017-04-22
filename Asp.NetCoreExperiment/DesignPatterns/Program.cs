32f0043a572d43dddd188b72712bb2014a5c2c5d
桂素伟 <axzxs2001@163.com>
2017-04-22 08:02:26 +0800

    状态模式完成。

--- Asp.NetCoreExperiment/DesignPatterns/Program.cs
+++ Asp.NetCoreExperiment/DesignPatterns/Program.cs
@@ -11,7 +11,7 @@ namespace DesignPatterns
             while (true)
             {
                 Console.WriteLine("===================================================");
-                Console.WriteLine("1、简单工厂  2、策略模式  3、装饰模式  4、代理模式  5、工厂方法  6、原型模式  7、模板方法  8、外观模式  9、建造者模式  10、观察者模式  11、抽象工厂");
+                Console.WriteLine("1、简单工厂  2、策略模式  3、装饰模式  4、代理模式  5、工厂方法  6、原型模式  7、模板方法  8、外观模式  9、建造者模式  10、观察者模式  11、抽象工厂  12、状态模式");
                 Console.WriteLine("==================================================");
                 Console.WriteLine("选择模式编号：");
                 switch (Console.ReadLine())
@@ -48,6 +48,9 @@ namespace DesignPatterns
                         break;
                     case "11":
                         Invock11();
+                        break;
+                    case "12":
+                        Invock12();
                         break;
                 }
             }
@@ -146,7 +149,6 @@ namespace DesignPatterns
             bObsSub.Notify();
         }
         #endregion
-
         #region 抽象工厂模式客户端  
         static void Invock11()
         {
@@ -156,6 +158,18 @@ namespace DesignPatterns
             departmentOpt.Insert(department);
             departmentOpt.GetDepartment(10);
 
+        }
+        #endregion
+        #region 状态模式客户端  
+        static void Invock12()
+        {
+            StateContext context = new StateContext(new StateA());
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
+            context.Request();
         }
         #endregion
     }
---
+++ Asp.NetCoreExperiment/DesignPatterns/状态模式.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    /****************************************************************************
+    * 状态模式
+    * 当一个对象的内在状态改变时允许改变其行为，这个对象看起来像是改变了其类。
+    ****************************************************************************/
+
+
+    /// <summary>
+    /// 内容状态
+    /// </summary>
+    public class StateContext
+    {
+        State _state;
+        public StateContext(State state)
+        {
+            _state = state;
+        }
+        public State State
+        {
+            get
+            {
+                return _state;
+            }
+            set
+            {
+                _state = value;
+                Console.WriteLine($"状态：{_state.GetType().Name}");
+            }
+        }
+
+        public void Request()
+        {
+            State.Handle(this);
+        }
+    }
+    /// <summary>
+    /// 抽象状态类
+    /// </summary>
+    public abstract class State
+    {
+        public abstract void Handle(StateContext context);
+    }
+    /// <summary>
+    /// 状态A
+    /// </summary>
+    public class StateA : State
+    {
+        public override void Handle(StateContext context)
+        {
+            context.State = new StateB();
+        }
+    }
+    /// <summary>
+    /// 状态B
+    /// </summary>
+    public class StateB : State
+    {
+        public override void Handle(StateContext context)
+        {
+            context.State = new StateA();
+        }
+    }
+
+}
