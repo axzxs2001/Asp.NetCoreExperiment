49cdf89fc76ca7dc86ec7df2b4cc6224ab4cd9be
桂素伟 <axzxs2001@163.com>
2017-04-23 22:42:08 +0800

    创建Redis数据库项目。

---
+++ Asp.NetCoreExperiment/RedisDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace RedisDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            IDatabase db = redis.GetDatabase();
+            string value = "abcdefg";
+            db.StringSet("mykey", value);
+            string value1 = db.StringGet("mykey");
+            Console.WriteLine(value1); // writes: "abcdefg"
+            Console.ReadLine();
+        }
+    }
+}
