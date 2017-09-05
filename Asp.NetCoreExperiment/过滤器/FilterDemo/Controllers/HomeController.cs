d8161652a8e8d2fb2762d8a7a8bc5fd819dd7971
桂素伟 <axzxs2001@163.com>
2017-09-05 14:19:33 +0800

    过滤器项目新建。

---
+++ Asp.NetCoreExperiment/过滤器/FilterDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FilterDemo.Models;
+
+namespace FilterDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        [AddHeader("author","gui su wei")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/过滤器/FilterDemo/Models/AddHeaderAttribute.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilterDemo.Models
+{
+    public class AddHeaderAttribute : ResultFilterAttribute
+    {
+        private readonly string _name;
+        private readonly string _value;
+
+        public AddHeaderAttribute(string name, string value)
+        {
+            _name = name;
+            _value = value;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+        
+            context.HttpContext.Response.Headers.Add(
+                _name, new string[] { _value });
+            base.OnResultExecuting(context);
+        }
+    }
+}
