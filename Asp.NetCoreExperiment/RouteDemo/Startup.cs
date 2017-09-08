0e2f9efedd1f688da7032cf90297c5d90abfa263
桂素伟 <axzxs2001@163.com>
2017-09-08 15:21:23 +0800

    路由项目创建。

---
+++ Asp.NetCoreExperiment/RouteDemo/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Routing;
+
+namespace RouteDemo
+{
+    public class Startup
+    {
+   
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
+        {
+            var trackPackageRouteHandler = new RouteHandler(context =>
+            {
+                var routeValues = context.GetRouteData().Values;
+                return context.Response.WriteAsync(
+                    $"Hello! Route values: {string.Join(", ", routeValues)}");
+            });
+
+            var routeBuilder = new RouteBuilder(app, trackPackageRouteHandler);
+
+            routeBuilder.MapRoute(
+                "Track Package Route",
+                "package/{operation:regex(^(track|create|detonate)$)}/{id:int}");
+
+            routeBuilder.MapGet("hello/{name}", context =>
+            {
+                var name = context.GetRouteValue("name");
+                // This is the route handler when HTTP GET "hello/<anything>"  matches
+                // To match HTTP GET "hello/<anything>/<anything>,
+                // use routeBuilder.MapGet("hello/{*name}"
+                return context.Response.WriteAsync($"Hi, {name}!");
+            });
+
+            var routes = routeBuilder.Build();
+            app.UseRouter(routes);
+        }
+    }
+}
