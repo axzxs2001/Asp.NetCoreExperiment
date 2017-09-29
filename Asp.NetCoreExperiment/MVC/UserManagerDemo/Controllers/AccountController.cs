ad254ce94dece08f1e0e65458252cccb9a1b5a01
桂素伟 <axzxs2001@163.com>
2017-09-29 22:34:27 +0800

    UserManage add

---
+++ Asp.NetCoreExperiment/MVC/UserManagerDemo/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+
+namespace UserManagerDemo.Controllers
+{
+    [Authorize]
+    public class AccountController : Controller
+    {
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+        [AllowAnonymous]
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            await _signInManager.PasswordSignInAsync(username, password, false, false);
+
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/MVC/UserManagerDemo/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserManagerDemo.Controllers;
+using Microsoft.AspNetCore.Identity;
+
+namespace UserManagerDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                
+                          //.AddEntityFrameworkStores<ApplicationDbContext>()
+                          .AddDefaultTokenProviders();
+
+            // Add application services.
+         
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
