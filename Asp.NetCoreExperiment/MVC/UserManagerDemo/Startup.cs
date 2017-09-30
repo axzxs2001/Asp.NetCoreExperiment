9490705b348d0ba69a6a70067b609e1a072d0950
桂素伟 <axzxs2001@163.com>
2017-09-30 08:09:55 +0800

    study user manage

--- Asp.NetCoreExperiment/MVC/UserManagerDemo/Startup.cs
+++ Asp.NetCoreExperiment/MVC/UserManagerDemo/Startup.cs
@@ -23,10 +23,10 @@ namespace UserManagerDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddIdentity<ApplicationUser, IdentityRole>()
+           // services.AddIdentity<ApplicationUser, IdentityRole>()
                 
                           //.AddEntityFrameworkStores<ApplicationDbContext>()
-                          .AddDefaultTokenProviders();
+                          //.AddDefaultTokenProviders();
 
             // Add application services.
          
