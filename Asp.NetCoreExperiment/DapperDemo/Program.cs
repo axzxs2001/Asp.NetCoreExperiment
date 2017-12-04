77c4904fed62dac28db6a98df977022a0f10a4a4
桂素伟 <axzxs2001@163.com>
2017-12-04 21:16:40 +0800

    Dapper时间戳案例。

---
+++ Asp.NetCoreExperiment/DapperDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+using Newtonsoft.Json;
+namespace DapperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            UpdateTimeStamp();
+
+        }
+        /// <summary>
+        /// 时间戳更新
+        /// </summary>
+        static void UpdateTimeStamp()
+        {
+            using (var conn = new SqlConnection("server=.;database=test;uid=sa;pwd=1"))
+            {
+                var sig = conn.Query<dynamic>("select * from ftable").FirstOrDefault();
+
+                var s = Newtonsoft.Json.JsonConvert.SerializeObject(sig, new JsonSerializerSettings { });
+                Console.WriteLine(s);
+                var o = Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(s);
+
+                var ss = conn.Execute("update ftable set age=@age where timespan=@timespan", new { age = 49, timespan = (byte[])o.timespan });
+
+            }
+        }
+    }
+
+
+}
