5acd1fae54c13e675d6c53a463260008aec26d72
桂素伟 <axzxs2001@163.com>
2017-12-23 15:16:58 +0800

    添加SignalR项目。

---
+++ Asp.NetCoreExperiment/SignalRDemo01/StreamingHub.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRDemo01
+{
+    public class StreamingHub : Hub
+    {
+        public void SendStreamInit()
+        {            //开启客户端订阅
+            Clients.All.InvokeAsync("streamStarted");
+        }
+        //被订阅的消息
+        public IObservable<string> StartStreaming()
+        {
+            return Observable.Create(
+          async (IObserver<string> observer) =>
+          {
+              for (int i = 0; i < 10; i++)
+              {
+                  observer.OnNext($"发送内容......{i}"); await Task.Delay(1000);
+              }
+          });
+        }
+    }
+
+    internal class Observable
+    {
+        internal static IObservable<string> Create(Func<IObserver<string>, Task> p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
