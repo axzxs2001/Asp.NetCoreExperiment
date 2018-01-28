d171df0db3e89c451dc492847e1e6b775bb1f30d
桂素伟 <axzxs2001@163.com>
2018-01-28 11:37:04 +0800

    配置文件热更项目。

---
+++ Asp.NetCoreExperiment/SnapshotDemo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SnapshotDemo.Models;
+
+namespace SnapshotDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        IConfiguration _con;
+        public HomeController(IConfiguration con)
+        {
+            _con = con;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            //读取配置
+            ViewData["Message"] = "Your application description page." + _con.GetSection("appsetting").GetSection("par1").Value;
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            //写入配置的值
+            _con.GetSection("appsetting").GetSection("par1").Value = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
