57b03e02c8b1c547216575599f58f2eb3ccd04a8
桂素伟 <axzxs2001@163.com>
2018-01-30 09:33:39 +0800

    配置Demo1

---
+++ Asp.NetCoreExperiment/ConfigurationDemo01/Program.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Memory;
+using System;
+using System.Collections.Generic;
+
+namespace ConfigurationDemo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var source = new Dictionary<string, string>
+            {
+                ["LongDatePattern"] = "dddd, MMMM d, yyyy",
+                ["LongTimePattern"] = "h:mm:ss tt",
+                ["ShortDatePattern"] = "M/d/yyyy",
+                ["ShortTimePattern"] = "h:mm tt"
+            };
+
+            var memorySource = new MemoryConfigurationSource();
+            memorySource.InitialData = source;    
+            var configuration = new ConfigurationBuilder()
+                    .Add(memorySource)
+                    .Build();
+
+            var settings = new DateTimeFormatSettings(configuration);
+            Console.WriteLine("{0,-16}: {1}", "LongDatePattern", settings.LongDatePattern);
+            Console.WriteLine("{0,-16}: {1}", "LongTimePattern", settings.LongTimePattern);
+            Console.WriteLine("{0,-16}: {1}", "ShortDatePattern", settings.ShortDatePattern);
+            Console.WriteLine("{0,-16}: {1}", "ShortTimePattern", settings.ShortTimePattern);
+        }
+    }
+
+    public class DateTimeFormatSettings
+    {
+        //其他成员
+        public DateTimeFormatSettings(IConfiguration configuration)
+        {
+            this.LongDatePattern = configuration["LongDatePattern"];
+            this.LongTimePattern = configuration["LongTimePattern"];
+            this.ShortDatePattern = configuration["ShortDatePattern"];
+            this.ShortTimePattern = configuration["ShortTimePattern"];
+        }
+
+        public string LongDatePattern { get; set; }
+        public string LongTimePattern { get; set; }
+        public string ShortDatePattern { get; set; }
+        public string ShortTimePattern { get; set; }
+    }
+}
