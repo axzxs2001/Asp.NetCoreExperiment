13d8a6513a9e76215a4e4b9bca3be161c1b345ca
桂素伟 <axzxs2001@163.com>
2018-02-01 13:53:56 +0800

    Proto.Actor Demo

---
+++ Asp.NetCoreExperiment/ProtoActorDemo/Program.cs
@@ -0,0 +1,54 @@
+﻿using Proto;
+using System;
+using System.Threading.Tasks;
+
+namespace ProtoActorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Test2();
+        }
+        static void Test2()
+        {
+            var props = Actor.FromProducer(() => new HelloActor());
+            var pid = Actor.Spawn(props);
+            pid.Tell(new Hello
+            {
+                Who = "Alex"
+            });
+        }
+        static void Test1()
+        {
+            var props = Actor.FromFunc(ctx =>
+            {
+                if (ctx.Message is string)
+                    ctx.Respond("hey");
+                return Actor.Done;
+            });
+            var pid = Actor.Spawn(props);
+
+            var reply = pid.RequestAsync<object>("hello").Result;
+            Console.WriteLine(reply);
+        }
+    }
+
+    internal class Hello
+    {
+        public string Who;
+    }
+    internal class HelloActor : IActor
+    {
+        public Task ReceiveAsync(IContext context)
+        {
+            var msg = context.Message;
+            if (msg is Hello r)
+            {
+                Console.WriteLine($"Hello {r.Who}");
+            }
+            return Actor.Done;
+        }
+    }
+}
