d236e0f4a2918c30b197f1a82d63e2d6ba781f6b
桂素伟 <axzxs2001@163.com>
2018-02-22 09:17:03 +0800

    Quartz.net的demo

---
+++ Asp.NetCoreExperiment/QuartzNetDemo2/Program.cs
@@ -0,0 +1,139 @@
+﻿using Quartz;
+using Quartz.Impl;
+using Quartz.Logging;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace QuartzNetDemo2
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Demo2();
+        }
+
+        #region demo2
+        static void Demo2()
+        {
+            LogProvider.SetCurrentLogProvider(new ConsoleLogProvider());
+
+            RunProgramRunExample().GetAwaiter().GetResult();
+
+            Console.WriteLine("Press any key to close the application");
+            Console.ReadKey();
+        }
+        private static async Task RunProgramRunExample()
+        {
+            try
+            {
+                // Grab the Scheduler instance from the Factory
+                var props = new NameValueCollection
+                {
+                    { "quartz.serializer.type", "binary" }
+                };
+                var factory = new StdSchedulerFactory(props);
+                var scheduler = await factory.GetScheduler();
+
+                // and start it off
+                await scheduler.Start();
+
+                // define the job and tie it to our HelloJob class
+                var job = JobBuilder.Create<HelloJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+                // Trigger the job to run now, and then repeat every 10 seconds
+                var trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger1", "group1")
+                    .StartNow()
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInSeconds(10)
+                        .RepeatForever())
+                    .Build();
+
+                // Tell quartz to schedule the job using our trigger
+                await scheduler.ScheduleJob(job, trigger);
+
+                // some sleep to show what's happening
+                await Task.Delay(TimeSpan.FromSeconds(60));
+
+                // and last shut down the scheduler when you are ready to close your program
+                await scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
+
+        }
+        #endregion
+        #region demo1
+        static void Demo1()
+        {
+            // trigger async evaluation
+            RunProgram().GetAwaiter().GetResult();
+        }
+        private static async Task RunProgram()
+        {
+            try
+            {
+                // Grab the Scheduler instance from the Factory
+                var props = new NameValueCollection
+                {
+                    { "quartz.serializer.type", "binary" }
+                };
+                var factory = new StdSchedulerFactory(props);
+                var scheduler = await factory.GetScheduler();
+
+                // and start it off
+                await scheduler.Start();
+
+                // some sleep to show what's happening
+                await Task.Delay(TimeSpan.FromSeconds(60));
+
+                // and last shut down the scheduler when you are ready to close your program
+                await scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                await Console.Error.WriteLineAsync(se.ToString());
+            }
+        }
+        #endregion
+    }
+
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync($"{DateTime .Now.ToString()}:Greetings from HelloJob!");
+        }
+    }
+
+    public class ConsoleLogProvider : ILogProvider
+    {
+        public Logger GetLogger(string name)
+        {
+            return (level, func, exception, parameters) =>
+            {
+                if (level >= LogLevel.Info && func != null)
+                {
+                    Console.WriteLine("[" + DateTime.Now.ToLongTimeString() + "] [" + level + "] " + func(), parameters);
+                }
+                return true;
+            };
+        }
+
+        public IDisposable OpenNestedContext(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable OpenMappedContext(string key, string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
