d1ca4afdb17bb9e066acfe12866f8d1f45b2c096
桂素伟 <axzxs2001@163.com>
2018-02-22 10:38:12 +0800

    学习proto.actor

--- Asp.NetCoreExperiment/ProtoActorDemo/Program.cs
+++ Asp.NetCoreExperiment/ProtoActorDemo/Program.cs
@@ -8,31 +8,35 @@ namespace ProtoActorDemo
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("接收方");
+            Console.ReadKey();
             Test2();
+           
+            Console.ReadKey();
         }
         static void Test2()
         {
             var props = Actor.FromProducer(() => new HelloActor());
             var pid = Actor.Spawn(props);
+            Console.WriteLine(pid.Id);
             pid.Tell(new Hello
             {
                 Who = "Alex"
             });
         }
-        static void Test1()
-        {
-            var props = Actor.FromFunc(ctx =>
-            {
-                if (ctx.Message is string)
-                    ctx.Respond("hey");
-                return Actor.Done;
-            });
-            var pid = Actor.Spawn(props);
+        //static void Test1()
+        //{
+        //    var props = Actor.FromFunc(ctx =>
+        //    {
+        //        if (ctx.Message is string)
+        //            ctx.Respond("hey");
+        //        return Actor.Done;
+        //    });
+        //    var pid = Actor.Spawn(props);
 
-            var reply = pid.RequestAsync<object>("hello").Result;
-            Console.WriteLine(reply);
-        }
+        //    var reply = pid.RequestAsync<object>("hello").Result;
+        //    Console.WriteLine($"Test1:{reply}");
+        //}
     }
 
     internal class Hello
@@ -46,7 +50,7 @@ namespace ProtoActorDemo
             var msg = context.Message;
             if (msg is Hello r)
             {
-                Console.WriteLine($"Hello {r.Who}");
+                Console.WriteLine($"ReceiveAsync:Hello {r.Who}");
             }
             return Actor.Done;
         }
---
+++ Asp.NetCoreExperiment/ProtoActorDemo2/Program.cs
@@ -0,0 +1,29 @@
+﻿using Proto;
+using System;
+
+namespace ProtoActorDemo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请求方");
+            var content = Console.ReadLine();
+            Test1(content);
+            Console.ReadKey();
+        }
+        static void Test1(string hey)
+        {
+            var props = Actor.FromFunc(ctx =>
+            {
+                if (ctx.Message is string)
+                    ctx.Respond(hey);
+                return Actor.Done;
+            });
+            var pid = Actor.Spawn(props);
+            Console.WriteLine(pid.Id);
+            var reply = pid.RequestAsync<object>("hello").Result;
+            Console.WriteLine($"请求方:{reply}");
+        }
+    }
+}
