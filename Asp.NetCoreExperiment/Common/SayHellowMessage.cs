c6acbb4d6b794e8f2d12d496a5aa2252d7b0e956
桂素伟 <axzxs2001@163.com>
2018-02-24 08:26:34 +0800

    akka测试例子。

---
+++ Asp.NetCoreExperiment/Common/SayHellowMessage.cs
@@ -0,0 +1,15 @@
+﻿using Akka.Actor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class SayHellowMessage
+    {
+        public string Name { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Asp.NetCoreExperiment/Server/Program.cs
@@ -0,0 +1,59 @@
+﻿using Akka.Actor;
+using Akka.Configuration;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+            var config = ConfigurationFactory.ParseString(@"
+akka {  
+    actor {
+        provider = ""Akka.Remote.RemoteActorRefProvider, Akka.Remote""
+    }
+    remote {
+        helios.tcp {
+            transport-class = ""Akka.Remote.Transport.Helios.HeliosTcpTransport, Akka.Remote""
+            applied-adapters = []
+            transport-protocol = tcp
+            port = 8081
+            hostname = localhost
+        }
+    }
+}
+");
+
+            using (var system = ActorSystem.Create("MyServer", config))
+            {
+                system.ActorOf<GreetingActor>("Greeting");
+
+                Console.ReadLine();
+            }
+        }
+    }
+    public class GreetingActor :ActorBase
+    {     
+
+        protected override bool Receive(object message)
+        {
+            if (message is SayHellowMessage)
+            {
+                Console.WriteLine("收到信息");
+
+                Console.WriteLine((message as SayHellowMessage).Name);
+
+                Console.WriteLine((message as SayHellowMessage).Content);
+            }
+            return true;
+           
+        }
+    }
+}
