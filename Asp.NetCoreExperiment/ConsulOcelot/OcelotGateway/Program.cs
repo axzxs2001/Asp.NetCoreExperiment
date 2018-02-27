7c82a935fbf3c70b382d3f948c371e367899833b
桂素伟 <axzxs2001@163.com>
2018-02-27 14:43:20 +0800

    添加ConsulOcelot的OcelotGateway项目。

---
+++ Asp.NetCoreExperiment/ConsulOcelot/OcelotGateway/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+namespace OcelotGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Title = "OcelotGateway";
+            BuildWebHost(args).Run();
+        }
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            IWebHostBuilder builder = new WebHostBuilder();
+            //注入WebHostBuilder
+            return builder.ConfigureServices(service =>
+            {
+                service.AddSingleton(builder);
+            })
+                //加载configuration配置文人年
+                .ConfigureAppConfiguration(conbuilder =>
+                {
+                    conbuilder.AddJsonFile("appsettings.json");
+                    conbuilder.AddJsonFile("configuration.json");
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+                .UseUrls("http://*:5000")
+                .UseStartup<Startup>()
+                .Build();
+        }
+
+
+    }
+}
