6458da884492e35770debc1264f0f17809e6af5f
桂素伟 <axzxs2001@163.com>
2018-02-28 19:20:08 +0800

    配置Ocelot文件.

--- Asp.NetCoreExperiment/ConsulOcelot/OcelotGateway/Startup.cs
+++ Asp.NetCoreExperiment/ConsulOcelot/OcelotGateway/Startup.cs
@@ -30,13 +30,12 @@ namespace OcelotGateway
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public async Task Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
             await app.UseOcelot();
         }
     }
