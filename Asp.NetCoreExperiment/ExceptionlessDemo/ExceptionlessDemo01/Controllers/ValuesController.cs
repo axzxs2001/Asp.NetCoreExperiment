5c990bc44c848250d5746312a3514e81cb0398ed
桂素伟 <axzxs2001@163.com>
2018-03-02 09:23:28 +0800

    添加ExceptionlessDemo01项目。

---
+++ Asp.NetCoreExperiment/ExceptionlessDemo/ExceptionlessDemo01/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Exceptionless;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExceptionlessDemo01.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            try
+            {
+
+                throw new Exception("我的异常"+DateTime .Now);
+            }
+            catch(Exception exc)
+            {
+                exc.ToExceptionless().Submit();
+            }
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
