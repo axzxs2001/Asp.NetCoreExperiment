9e40a4b5e30bfba3edcd83927ec144a147ffb700
桂素伟 <axzxs2001@163.com>
2018-03-14 09:59:23 +0800

    RabbitMQ 测试。

---
+++ Asp.NetCoreExperiment/RabbitMQDemo/Demo001_WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo001_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        //[HttpGet("{id}")]
+        //public async Task<string> Get(int id)
+        //{
+        //    using(var con=new SqlConnection("server=.;database=testdb;uid=sa;pwd=1;"))
+        //    using (var cmd = new SqlCommand())
+        //    {
+        //        System.Threading.Thread.Sleep(5000);
+        //        cmd.Connection = con;
+        //        cmd.CommandText = "select count(1) from ttt";
+        //        con.Open();
+        //        var count = await cmd.ExecuteScalarAsync();
+        //        return count.ToString();
+        //    }
+         
+        //}
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            using (var con = new SqlConnection("server=.;database=testdb;uid=sa;pwd=1;"))
+            using (var cmd = new SqlCommand())
+            {
+                System.Threading.Thread.Sleep(5000);
+                cmd.Connection = con;
+                cmd.CommandText = "select count(1) from ttt";
+                con.Open();
+                var count = cmd.ExecuteScalar();
+                return count.ToString();
+            }
+
+        }
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/RabbitMQDemo/Demo002_WebAPITest/Controllers/ValuesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo002_WebAPITest.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<string> Get(int id)
+        {
+            var begin = DateTime.Now;
+            var client1 = new HttpClient();
+            var result1 =  client1.GetStringAsync("http://localhost:5000/api/values/1");
+
+            var client2 = new HttpClient();
+            var result2 =  client2.GetStringAsync("http://localhost:5000/api/values/1");
+            var result = (Convert.ToInt32(await result1) + Convert.ToInt32(await result2)).ToString();
+            return (DateTime .Now-begin).TotalSeconds+"秒   "+ result;
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
