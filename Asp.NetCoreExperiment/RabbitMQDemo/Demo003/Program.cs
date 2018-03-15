52332336f0ee2547c6211d29a5be1dc7e09862ae
桂素伟 <axzxs2001@163.com>
2018-03-15 13:59:42 +0800

    RabbitMQ Demo

---
+++ Asp.NetCoreExperiment/RabbitMQDemo/Demo003/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Demo003
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.Title = "Client";
+            Console.WriteLine("按任意键开始！");
+            Console.ReadKey();
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
