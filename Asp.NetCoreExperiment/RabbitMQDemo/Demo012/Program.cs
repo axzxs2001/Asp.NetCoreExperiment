85dfe1590702af01e40801fc168f1a6ced27198e
桂素伟 <axzxs2001@163.com>
2018-03-17 09:31:43 +0800

    RabbitMQ PRC测试

---
+++ Asp.NetCoreExperiment/RabbitMQDemo/Demo012/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+public class RpcClient
+{
+    private readonly IConnection connection;
+    private readonly IModel channel;
+    private readonly string replyQueueName;
+    private readonly EventingBasicConsumer consumer;
+    private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
+    private readonly IBasicProperties props;
+
+    public RpcClient()
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        connection = factory.CreateConnection();
+        channel = connection.CreateModel();
+        replyQueueName = channel.QueueDeclare().QueueName;
+        consumer = new EventingBasicConsumer(channel);
+
+        props = channel.CreateBasicProperties();
+        var correlationId = Guid.NewGuid().ToString();
+        props.CorrelationId = correlationId;
+        props.ReplyTo = replyQueueName;
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body;
+            var response = Encoding.UTF8.GetString(body);
+            if (ea.BasicProperties.CorrelationId == correlationId)
+            {
+                respQueue.Add(response);
+            }
+        };
+    }
+
+    public string Call(string message)
+    {
+        var messageBytes = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(
+            exchange: "",
+            routingKey: "rpc_queue",
+            basicProperties: props,
+            body: messageBytes);
+
+        channel.BasicConsume(
+            consumer: consumer,
+            queue: replyQueueName,
+            autoAck: true);
+
+        return respQueue.Take(); ;
+    }
+
+    public void Close()
+    {
+        connection.Close();
+    }
+}
+
+public class Rpc
+{
+    public static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("请输入一个整数：");
+
+            var rpcClient = new RpcClient();
+            var response = rpcClient.Call(Console.ReadLine());
+            Console.WriteLine(" [.] Got '{0}'", response);
+            rpcClient.Close();
+      
+        }
+    }
+}
