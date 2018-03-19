3a35101f3004960effd3e5c09a924f6d0808de61
桂素伟 <axzxs2001@163.com>
2018-03-19 10:01:36 +0800

    Masstransit Send Command Pattern test.

---
+++ Asp.NetCoreExperiment/Masstransit/Demo0002/Program.cs
@@ -0,0 +1,48 @@
+﻿using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo0002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "客户端";
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost/"), hst =>
+                {
+                    hst.Username("guest");
+                    hst.Password("guest");
+                });
+                
+            });
+
+            var uri = new Uri("rabbitmq://localhost/gsw");
+            while (Console.ReadLine() != null)
+            {
+                Task.Run(() => SendCommand(bus, uri)).Wait();
+               
+            }
+
+            Console.ReadLine();
+        }
+        private static async void SendCommand(IBusControl bus, Uri sendToUri)
+        {
+            var endPoint = await bus.GetSendEndpoint(sendToUri);
+            var command = new Demo001.ABC()
+            {
+                Name = "张三",
+                Time = DateTime.Now
+            };
+
+            await endPoint.Send(command);
+
+            Console.WriteLine($"send command:id={command.Name},{command.Time}");
+        }
+
+    }
+  
+ 
+}
---
+++ Asp.NetCoreExperiment/Masstransit/Demo001/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using MassTransit;
+
+
+namespace Demo001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "服务端";
+            var bus= Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost/"), hst =>
+                {
+                    hst.Username("guest");
+                    hst.Password("guest");
+                });
+
+                cfg.ReceiveEndpoint(host, "gsw", e =>
+                {
+                    e.Consumer<ConsumerABC>();
+
+                });
+            });
+          
+            bus.Start();
+            Console.WriteLine("Listening for Greeting commands.. Press enter to exit");
+            Console.ReadLine();
+            bus.Stop();
+        }
+    }
+
+    public class ConsumerABC : IConsumer<ABC>
+    {
+        public async Task Consume(ConsumeContext<ABC> context)
+        {
+            await Console.Out.WriteLineAsync($"receive greeting commmandB: {context.Message.Name},{context.Message.Time}");
+        }
+    
+    }
+    public class ABC
+    {
+        public DateTime Time { get; set; }
+        public string Name { get; set; }
+    }
+
+}
