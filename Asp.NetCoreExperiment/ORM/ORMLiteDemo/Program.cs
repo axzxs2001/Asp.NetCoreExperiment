70448eeca2f4d66fca4367c40fb4ba71c1259d96
桂素伟 <axzxs2001@163.com>
2018-03-29 10:11:18 +0800

    OrmLite

---
+++ Asp.NetCoreExperiment/ORM/ORMLiteDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using ServiceStack.OrmLite;
+using System;
+
+namespace ORMLiteDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbFactory = new OrmLiteConnectionFactory(
+      "server=.;database=orderdb;uid=sa;pwd=1;",
+      SqlServerDialect.Provider);
+            using (var db = dbFactory.Open())
+            {
+                foreach (var order in db.Select<Orders>())
+                {
+                    Console.WriteLine(order);
+                }
+            }
+        }
+    }
+    public class Orders
+    {
+        public string ID { get; set; }
+        public DateTime OrderTime { get; set; }
+        public string OrderUserID { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID={ID},OrderTime={OrderTime},OrderUserID={OrderUserID}";
+        }
+    }
+}
