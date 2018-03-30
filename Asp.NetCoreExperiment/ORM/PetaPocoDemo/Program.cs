44e1451db566ccb55b7a9b94d3c8c2bf6ea3747e
桂素伟 <axzxs2001@163.com>
2018-03-30 11:01:11 +0800

    PetaPoco 测试。

---
+++ Asp.NetCoreExperiment/ORM/PetaPocoDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//using PetaPoco;
+namespace PetaPocoDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         //   var db = new PetaPoco.Database("connectionStringName");
+         //   var db = DatabaseConfiguration.Build()
+         //.UsingConnectionString("Host=127.0.0.1;Username=petapoco;Password=petapoco;Database=petapoco;Port=5001")
+         //.UsingProvider<PostgreSQLDatabaseProvider>()
+         //.UsingDefaultMapper<ConventionMapper>(m =>
+         //{
+         //    m.InflectTableName = (inflector, s) => inflector.Pluralise(inflector.Underscore(s));
+         //    m.InflectColumnName = (inflector, s) => inflector.Underscore(s);
+         //})
+         //.Create();
+        }
+    }
+
+    public class Orders
+    {
+        public string ID { get; set; }
+        public DateTime OrderTime { get; set; }
+        public string OrderUserID { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID={ID},OrderTime={OrderTime},OrderUserID={OrderUserID}";
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/PetaPocoDemo01/Program.cs
@@ -0,0 +1,33 @@
+﻿using PetaPoco;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetaPocoDemo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new PetaPoco.Database(connectionString: "server=.;database=orderdb;uid=sa;pwd=1;", providerName: "sqlservers");
+            foreach (var order in db.Query<Orders>("select * from orders"))
+            {
+                Console.WriteLine(order);
+            }
+
+        }
+    }
+    public class Orders
+    {
+        public string ID { get; set; }
+        public DateTime OrderTime { get; set; }
+        public string OrderUserID { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID={ID},OrderTime={OrderTime},OrderUserID={OrderUserID}";
+        }
+    }
+}
