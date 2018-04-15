afdae84c8cd9885740afab1e755ea6892531ec41
桂素伟 <axzxs2001@163.com>
2018-04-15 19:21:48 +0800

    IdentityServer4 Asp.net core

---
+++ Asp.NetCoreExperiment/IdentityServer4/Demo03/TestApp/Config.cs
@@ -0,0 +1,91 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    public class Config
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+   new Client
+{
+    ClientId = "mvc",
+    ClientName = "MVC Client",
+    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+
+    RequireConsent = false,
+
+    ClientSecrets =
+    {
+        new Secret("secret".Sha256())
+    },
+
+    RedirectUris           = { "http://localhost:5002/signin-oidc" },
+    PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+
+    AllowedScopes =
+    {
+        IdentityServerConstants.StandardScopes.OpenId,
+        IdentityServerConstants.StandardScopes.Profile,
+        "api1"
+    },
+    AllowOfflineAccess = true
+}
+        };
+        }
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+    {
+        new TestUser
+        {
+            SubjectId = "1",
+            Username = "gsw",
+            Password = "111111",
+
+            Claims = new []
+            {
+                new Claim("name", "gsw"),
+                new Claim("website", "https://alice.com")
+            }
+        },
+        new TestUser
+        {
+            SubjectId = "2",
+            Username = "ggg",
+            Password = "222222",
+
+            Claims = new []
+            {
+                new Claim("name", "Bob"),
+                new Claim("website", "https://bob.com")
+            }
+        }
+    };
+        }
+
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),    };
+
+        }
+    }
+}
