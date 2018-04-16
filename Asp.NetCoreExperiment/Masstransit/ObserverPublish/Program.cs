a250ded51911edc5b7cb1ad1b0c2b62bcbc35bbc
桂素伟 <axzxs2001@163.com>
2018-04-16 11:25:30 +0800

    MassTransit Observer Demo

---
+++ Asp.NetCoreExperiment/Masstransit/ObserverPublish/Program.cs
@@ -0,0 +1,107 @@
+﻿using MassTransit;
+using ObserverSubscriber;
+using System;
+using System.Threading.Tasks;
+
+namespace ObserverPublish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "发布方";
+
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost"), hst =>
+                {
+                    hst.Username("guest");
+                    hst.Password("guest");
+                });
+
+            });
+            var observer = new SendObserver();
+            var handle = bus.ConnectSendObserver(observer);
+
+            var observer1 = new PublishObserver();
+            var handle1 = bus.ConnectPublishObserver(observer1);
+            bus.Start();
+            do
+            {
+                Console.WriteLine("请出请按q,否则请按其他键！");
+
+                string value = Console.ReadLine();
+
+                if (value.ToLower() == "q")
+                {
+                    break;
+                }
+
+                bus.Publish(new Entity() { ID = 1, Age = 10, Name = "张三", Time = DateTime.Now });
+            }
+            while (true);
+
+            bus.Stop();
+        }
+    }
+
+    public class PublishObserver : IPublishObserver
+    {
+        public Task PrePublish<T>(PublishContext<T> context)
+            where T : class
+        {
+            Console.WriteLine("------------------PrePublish--------------------");
+            Console.WriteLine($"ID={ (context.Message as Entity).ID},Name={(context.Message as Entity).Name},Time={(context.Message as Entity).Time}");
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+
+        public Task PostPublish<T>(PublishContext<T> context)
+            where T : class
+        {
+            Console.WriteLine("------------------PostPublish--------------------");
+            Console.WriteLine($"ID={ (context.Message as Entity).ID},Name={(context.Message as Entity).Name},Time={(context.Message as Entity).Time}");
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+
+        public Task PublishFault<T>(PublishContext<T> context, Exception exception)
+            where T : class
+        {
+            Console.WriteLine("------------------PublishFault--------------------");
+            Console.WriteLine($"ID={ (context.Message as Entity).ID},Name={(context.Message as Entity).Name},Time={(context.Message as Entity).Time}");
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+    }
+
+    public class SendObserver : ISendObserver
+    {
+        public Task PreSend<T>(SendContext<T> context)
+            where T : class
+        {
+            Console.WriteLine("------------------PreSend--------------------");
+            Console.WriteLine($"ID={ (context.Message as Entity).ID},Name={(context.Message as Entity).Name},Time={(context.Message as Entity).Time}");
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+
+        public Task PostSend<T>(SendContext<T> context)
+            where T : class
+        {
+            Console.WriteLine("------------------PostSend--------------------");
+            Console.WriteLine($"ID={ (context.Message as Entity).ID},Name={(context.Message as Entity).Name},Time={(context.Message as Entity).Time}");
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+
+        public Task SendFault<T>(SendContext<T> context, Exception exception)
+            where T : class
+        {
+            Console.WriteLine("------------------SendFault--------------------");
+            Console.WriteLine($"ID={ (context.Message as Entity).ID},Name={(context.Message as Entity).Name},Time={(context.Message as Entity).Time}");
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/Masstransit/ObserverSubscriber/Program.cs
@@ -0,0 +1,101 @@
+﻿using MassTransit;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverSubscriber
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "订阅方";
+            var bus = Bus.Factory.CreateUsingRabbitMq(cfg =>
+            {
+                var host = cfg.Host(new Uri("rabbitmq://localhost/"), hst =>
+                {
+                    hst.Username("guest");
+                    hst.Password("guest");
+                });
+                cfg.ReceiveEndpoint(host, "ObserverTest", e =>
+                {
+                    e.Consumer<EntityConsumer>();
+                });
+            });
+            var observer = new ReceiveObserver();
+            var handle = bus.ConnectReceiveObserver(observer);
+            bus.Start();
+            Console.ReadLine();
+            bus.Stop();
+        }
+    }
+    public class ReceiveObserver : IReceiveObserver
+    {
+        public Task PreReceive(ReceiveContext context)
+        {
+    
+            Console.WriteLine("------------------PreReceive--------------------");
+            Console.WriteLine(Encoding.Default.GetString(context.GetBody()));
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+
+        public Task PostReceive(ReceiveContext context)
+        {
+        
+            Console.WriteLine("-----------------PostReceive---------------------");
+            Console.WriteLine(Encoding.Default.GetString(context.GetBody()));
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+
+        public Task PostConsume<T>(ConsumeContext<T> context, TimeSpan duration, string consumerType)
+            where T : class
+        {
+       
+            Console.WriteLine("------------------PostConsume--------------------");
+            Console.WriteLine($"ID={ (context.Message as Entity).ID},Name={(context.Message as Entity).Name},Time={(context.Message as Entity).Time}");
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+
+        public Task ConsumeFault<T>(ConsumeContext<T> context, TimeSpan elapsed, string consumerType, Exception exception) where T : class
+        {
+         
+            Console.WriteLine("-----------------ConsumeFault---------------------");
+            Console.WriteLine($"ID={ (context.Message as Entity).ID},Name={(context.Message as Entity).Name},Time={(context.Message as Entity).Time}");
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+
+        public Task ReceiveFault(ReceiveContext context, Exception exception)
+        {            
+            Console.WriteLine("----------------ReceiveFault----------------------");
+            Console.WriteLine(Encoding.Default.GetString(context.GetBody()));
+            Console.WriteLine("--------------------------------------");
+            return Task.CompletedTask;
+        }
+    }
+
+
+    public class EntityConsumer : IConsumer<Entity>
+    {
+        public async Task Consume(ConsumeContext<Entity> context)
+        {
+            await Console.Out.WriteLineAsync($"IEntityConsumer 类型 {context.Message.GetType()} {context.Message.ID} {context.Message.Age} {context.Message.Name} {context.Message.Time}");
+
+        }
+    }
+
+    public class Entity
+    {
+        public int ID { get; set; }
+
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+
+    }
+
+}
