332ee0def21a393899bfcd2d304dd041d608e0a3
桂素伟 <axzxs2001@163.com>
2018-04-17 10:01:21 +0800

    优化QuartzNet注释。

--- Asp.NetCoreExperiment/QuartzNet/QuartzNetDemo2/Program.cs
+++ Asp.NetCoreExperiment/QuartzNet/QuartzNetDemo2/Program.cs
@@ -28,38 +28,39 @@ namespace QuartzNetDemo2
         {
             try
             {
-                // Grab the Scheduler instance from the Factory
+                //定义配置参数集合
                 var props = new NameValueCollection
                 {
                     { "quartz.serializer.type", "binary" }
                 };
                 var factory = new StdSchedulerFactory(props);
+                //生成调度器
                 var scheduler = await factory.GetScheduler();
 
-                // and start it off
+                //开始调度器
                 await scheduler.Start();
 
-                // define the job and tie it to our HelloJob class
+                //创建作业任务
                 var job = JobBuilder.Create<HelloJob>()
                     .WithIdentity("job1", "group1")
                     .Build();
 
-                // Trigger the job to run now, and then repeat every 10 seconds
+                //创建触发器
                 var trigger = TriggerBuilder.Create()
                     .WithIdentity("trigger1", "group1")
                     .StartNow()
                     .WithSimpleSchedule(x => x
-                        .WithIntervalInSeconds(10)
-                        .RepeatForever())
+                    .WithIntervalInSeconds(10)
+                    .RepeatForever())
                     .Build();
 
-                // Tell quartz to schedule the job using our trigger
+                //用调度器关联触发器和作业任务
                 await scheduler.ScheduleJob(job, trigger);
 
-                // some sleep to show what's happening
+                // 延迟60秒
                 await Task.Delay(TimeSpan.FromSeconds(60));
 
-                // and last shut down the scheduler when you are ready to close your program
+                // 关闭调度器
                 await scheduler.Shutdown();
             }
             catch (SchedulerException se)
@@ -108,7 +109,8 @@ namespace QuartzNetDemo2
     {
         public async Task Execute(IJobExecutionContext context)
         {
-            await Console.Out.WriteLineAsync($"{DateTime .Now.ToString()}:Greetings from HelloJob!");
+        
+            await Console.Out.WriteLineAsync($"{DateTime.Now.ToString()}:Greetings from HelloJob!");
         }
     }
 
