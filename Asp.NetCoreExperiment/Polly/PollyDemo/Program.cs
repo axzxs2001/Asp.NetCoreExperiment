bfe10b2a0fe04fd7fcbf262fb2a0577cdd6f049c
桂素伟 <axzxs2001@163.com>
2018-04-18 18:07:31 +0800

    整理Polly例子.

--- Asp.NetCoreExperiment/Polly/PollyDemo/Program.cs
+++ Asp.NetCoreExperiment/Polly/PollyDemo/Program.cs
@@ -1,5 +1,9 @@
-﻿using Polly;
+﻿
+using Microsoft.Extensions.Caching.Memory;
+using Polly;
 using System;
+
+using System.Threading;
 
 namespace PollyDemo
 {
@@ -8,137 +12,298 @@ namespace PollyDemo
         static void Main(string[] args)
         {
 
-            Demo4();
+            TimeOut();
         }
-        #region 默认策略
 
-        static void Demo1()
+
+
+
+
+        #region Retry
+
+        public static void Retry()
         {
-            #region demo1
             try
             {
-                var retryTwoTimesPolicy = Policy
+                //定义捕捉到的异常类型后重试
+                var policy = Policy
                     .Handle<DivideByZeroException>()
-                    .Retry(3, (ex, count) =>
+                    .Or<Exception>()
+                    //重试三次
+                    .Retry(3, (excetpion, index, context) =>
                     {
-                        Console.WriteLine("执行失败! 重试次数 {0}", count);
-                        Console.WriteLine("异常来自 {0}", ex.GetType().Name);
-                    });
-                retryTwoTimesPolicy.Execute(() =>
+                        Console.WriteLine(excetpion.Message);
+                        Console.WriteLine(index);
+                        Console.WriteLine(context);
+                    });    //故障的处理方法
+                //开始执行
+                policy.Execute(() =>
                 {
-                    Compute();
+                    Console.WriteLine("开始");
+                    throw new IndexOutOfRangeException("除零");
                 });
             }
-            catch (DivideByZeroException e)
+            catch (Exception exc)
             {
-                Console.WriteLine($"异常,Message: ({e.Message})");
-
+                Console.WriteLine($"Excepiton:{exc.Message}");
             }
-            #endregion
+            Console.ReadLine();
         }
 
-        static int Compute()
+        #endregion
+
+        #region Fallback
+        static void Fallback()
         {
-            var a = 0;
-            return 1 / a;
+            //在遇到故障时指定一个默认的返回值，
+            var policy =
+                Policy<string>
+                    .Handle<Exception>()
+                    .Fallback("执行失败，返回Fallback");
+
+            var fallBack = policy.Execute(() =>
+            {
+                throw new Exception("我和异常！");
+            });
+            Console.WriteLine(fallBack);
         }
         #endregion
 
 
-        #region 重试策略
 
-        static void Demo2()
+
+        #region CircuitBreaker
+
+        static void CircuitBreaker1()
         {
-          
+
+            Action<Exception, TimeSpan, Context> onBreak = (exception, timespan, context) =>
+            {
+                Console.WriteLine($"onBreak:{exception.Message} timespan:{timespan.TotalSeconds}s, context:{context.Count}");
+            };
+            Action<Context> onReset = (context) =>
+            {
+                Console.WriteLine($"onReset, context:{context.Count}");
+            };
+            var breaker = Policy
+                .Handle<DivideByZeroException>()
+                //测试三次，熔断后5秒后再重试
+                .CircuitBreaker(5, TimeSpan.FromSeconds(3), onBreak, onReset);
+
+            var times = 0;
+            while (true)
+            {
+                try
+                {
+                    times++;
+                    breaker.Execute(() =>
+                    {
+                        Thread.Sleep(500);
+                        Console.WriteLine($"开始执行,次数：{times}");
+                        throw new DivideByZeroException($"除以0,次数：{times}");
+                    });
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"异常,Message:({e.Message})");
+                }
+                Thread.Sleep(500);
+            }
+
+        }
+        static void CircuitBreaker2()
+        {
+            Action<Exception, TimeSpan, Context> onBreak = (exception, timespan, context) =>
+            {
+                Console.WriteLine($"onBreak:{exception.Message} timespan:{timespan.TotalSeconds}s, context:{context.Count}");
+            };
+            Action<Context> onReset = (context) =>
+            {
+                Console.WriteLine($"onReset, context:{context.Count}");
+            };
+            var breaker = Policy
+                .Handle<Exception>()
+                .AdvancedCircuitBreaker(
+                  failureThreshold: 0.5, //0.5为>=50%, 取值0~1，熔断的比例
+                  samplingDuration: TimeSpan.FromSeconds(10), //基础收集时间为10s
+                  minimumThroughput: 8, //10s超过8次触发熔断
+                  durationOfBreak: TimeSpan.FromSeconds(5) //熔断间隔
+                );
+            var times = 0;
+            while (true)
+            {
+                try
+                {
+                    times++;
+                    breaker.Execute(() =>
+                    {
+                        Thread.Sleep(500);
+                        if (times % 2 == 0)
+                        {
+                            Console.WriteLine($"次数：{times}，有异常！");
+                            throw new Exception("除以0");
+                        }
+                        Console.WriteLine($"次数：{times}，无异常！");
+                    });
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"异常,Message:({e.Message})");
+                }
+                Thread.Sleep(500);
+            }
+
+        }
+        #endregion
+
+
+
+
+
+        #region WaitAndRetry
+
+        static void WaitAndRetry()
+        {
+
             try
             {
+                //时间段数组
+                //var politicaWaitAndRetry = Policy
+                //  .Handle<Exception>()
+                //  .WaitAndRetry(new[]
+                //  {
+                //        TimeSpan.FromSeconds(1),
+                //        TimeSpan.FromSeconds(3),
+                //        TimeSpan.FromSeconds(5),
+                //        TimeSpan.FromSeconds(7)
+                //  }, (exception,timespan,count,context)=> {
+                //      Console.WriteLine($"异常:{exception.Message}, {count:00} (调用秒数: {timespan.Seconds} 秒)\t执行时间: {DateTime.Now}");
+                //  });
+                //次数+时间间隔
                 var politicaWaitAndRetry = Policy
-                  .Handle<DivideByZeroException>()
-                  .WaitAndRetry(new[]
-                  {
-                        TimeSpan.FromSeconds(1),
-                        TimeSpan.FromSeconds(3),
-                        TimeSpan.FromSeconds(5),
-                        TimeSpan.FromSeconds(7)
-                  }, ReportaError);
+                    .Handle<Exception>()
+                    //重试5次
+                    .WaitAndRetry(5
+                    //第次间隔3s,count为重试的索引
+                    , sleepDurationProvider: (count) =>
+                    {
+                        Console.WriteLine($"count={count}");
+                        return TimeSpan.FromSeconds(3);
+                    }
+                    , onRetry: (exception, timespan, count, context) =>
+                    {
+                        Console.WriteLine($"异常:{exception.Message}, {count:00} (调用秒数: {timespan.Seconds} 秒)\t执行时间: {DateTime.Now}");
+                    });
                 politicaWaitAndRetry.Execute(() =>
                 {
-                    ZeroExcepcion();
+                    throw new Exception("我的异常");
                 });
             }
             catch (Exception e)
             {
                 Console.WriteLine($"异常,Message:({e.Message})");
             }
-        
+
         }
-        /// <summary>
-        /// 抛出异常        /// </summary>
-        static void ZeroExcepcion()
+
+
+        static void WaitAndRetryForever()
         {
-            throw new DivideByZeroException();
+            try
+            {
+                var politicaWaitAndRetry = Policy
+                  .Handle<Exception>()
+                  .WaitAndRetryForever(retryAttempt =>
+                  {
+                      Console.WriteLine($"retryAttempt={retryAttempt}");
+                      return TimeSpan.FromSeconds(3);
+                  }, (exception, timespan) =>
+                  {
+                      Console.WriteLine($"异常:{exception.Message}, (调用秒数: {timespan.Seconds} 秒)\t执行时间: {DateTime.Now}");
+                  });
+                politicaWaitAndRetry.Execute(() =>
+                {
+                    throw new Exception("我的异常");
+                });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"异常,Message:({e.Message})");
+            }
+
         }
-
-
-        /// <summary>
-        /// 异常信息        /// </summary>
-        /// <param name="e"></param>
-        /// <param name="tiempo"></param>
-        /// <param name="intento"></param>
-        /// <param name="contexto"></param>
-        static void ReportaError(Exception e, TimeSpan tiempo, int intento, Context contexto)
-        {
-            Console.WriteLine($"异常: {intento:00} (调用秒数: {tiempo.Seconds} 秒)\t执行时间: {DateTime.Now}");
-        }
-
         #endregion
 
-        #region 反馈策略
-        static string ThrowException()
-        {
-            throw new Exception();
-        }
-        static void Demo3()
+
+
+        #region 组合
+        static void Group()
         {
             var fallBackPolicy =
                 Policy<string>
                     .Handle<Exception>()
                     .Fallback("执行失败，返回Fallback");
 
-            var fallBack = fallBackPolicy.Execute(() =>
-            {
-                return ThrowException();
-            });
-            Console.WriteLine(fallBack);
-        }
-        #endregion
-
-        #region 包裹策略
-        static void Demo4()
-        {
-            var fallBackPolicy =
+            var waitAetryPolicy =
                 Policy<string>
                     .Handle<Exception>()
-                    .Fallback("执行失败，返回Fallback");
-
-            var fallBack = fallBackPolicy.Execute(() =>
-            {
-                return ThrowException();
-            });
-            Console.WriteLine(fallBack);
-
-            var politicaWaitAndRetry =
-                Policy<string>
-                    .Handle<Exception>()
-                    .Retry(3, (ex, count) =>
+                    .WaitAndRetry(3, (count) =>
                     {
-                        Console.WriteLine("执行失败! 重试次数 {0}", count);
-                        Console.WriteLine("异常来自 {0}", ex.GetType().Name);
+                        Console.WriteLine($"执行失败! 重试次数 {count}");
+                        return TimeSpan.FromSeconds(3);
                     });
 
-            var mixedPolicy = Policy.Wrap(fallBackPolicy, politicaWaitAndRetry);
-            var mixedResult = mixedPolicy.Execute(ThrowException);
+            var mixedPolicy = Policy.Wrap(fallBackPolicy, waitAetryPolicy);
+            var mixedResult = mixedPolicy.Execute(() =>
+            {
+                throw new Exception("组合");
+            });
             Console.WriteLine($"执行结果: {mixedResult}");
+        }
+        #endregion
+
+        #region cache
+        public static void Cache()
+        {
+            var mc = new MemoryCache(new MemoryCacheOptions() { });
+            var memoryCacheProvider = new Polly.Caching.MemoryCache.MemoryCacheProvider(mc);
+            var policy = Policy.Cache(memoryCacheProvider, TimeSpan.FromSeconds(5));
+            var context = new Context("key");
+            for (int i = 0; i < 10; i++)
+            {
+                var cache = policy.Execute(cxt =>
+                {
+                    Console.WriteLine("执行" + cxt.CorrelationId);
+                    return DateTime.Now;
+                }, context);
+                Console.WriteLine(cache);
+                Thread.Sleep(1000);
+            }
+        }
+        #endregion
+
+
+        #region cache
+        public static void TimeOut()
+        {
+            try
+            {
+                var policy = Policy.Timeout(TimeSpan.FromSeconds(3));
+                var cts = new CancellationTokenSource();
+                policy.Execute(ct =>
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        Thread.Sleep(1000);
+                        Console.WriteLine(i);
+                        ct.ThrowIfCancellationRequested();
+                    }
+                }, cts.Token);
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
         }
         #endregion
 
