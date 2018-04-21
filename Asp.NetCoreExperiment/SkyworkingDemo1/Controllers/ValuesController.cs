52207fcb1e7ec40d2ab8ca72e5c742282843f9af
桂素伟 <axzxs2001@163.com>
2018-04-21 09:51:31 +0800

    Skywalking测试。

---
+++ Asp.NetCoreExperiment/SkyworkingDemo1/Controllers/ValuesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkyworkingDemo1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+     
+        [HttpGet("{id}")]
+        public async Task<string> Get(int id, [FromServices] IHttpClientFactory httpClientFactory)
+        {
+            var httpClient = httpClientFactory.CreateClient("sw-tracing");
+            return await httpClient.GetStringAsync("http://localhost:5002/values");
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
