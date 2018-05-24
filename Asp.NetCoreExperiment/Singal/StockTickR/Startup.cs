cca9abfe2e5cd8bf568891b2882292e97ec03299
桂素伟 <axzxs2001@163.com>
2018-05-25 08:25:43 +0900

    signalR test

---
+++ Asp.NetCoreExperiment/Singal/StockTickR/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using StockTickR.Hubs;
+
+namespace StockTickR
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR()
+                    .AddMessagePackProtocol();
+
+            services.AddSingleton<StockTicker>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<StockTickerHub>("/stocks");
+            });
+        }
+    }
+}
