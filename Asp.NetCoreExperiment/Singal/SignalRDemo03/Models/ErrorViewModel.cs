e91b0e167629a88202cbca22cbc0541fee293ba4
桂素伟 <axzxs2001@163.com>
2018-06-01 14:49:20 +0900

    创建SignalR项目。

---
+++ Asp.NetCoreExperiment/Singal/SignalRDemo03/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SignalRDemo03.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
