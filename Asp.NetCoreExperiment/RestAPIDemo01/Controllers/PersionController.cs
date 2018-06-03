5b0da10f026bcee3030aa927e983d07b3d74d631
桂素伟 <axzxs2001@163.com>
2018-06-04 08:28:09 +0900

    rest api test

---
+++ Asp.NetCoreExperiment/RestAPIDemo01/Controllers/PersionController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestAPIDemo01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersionController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}",Name ="GetPerson")]
+        public ActionResult GetPersion(int id)
+        {
+            var list = new List<Person>() {
+                new Person {ID=1,Name="1111"},
+                new Person {ID=2,Name="2222"},
+                new Person {ID=3,Name="3333"},
+                new Person {ID=4,Name="4444"},
+            };
+            return Ok(list.SingleOrDefault(s=>s.ID==id));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody] Person person)
+        {
+            person.ID = 4;
+            return  CreatedAtRoute("GetPersion", new { id = 4 },person);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Asp.NetCoreExperiment/RestAPIDemo01/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace RestAPIDemo01
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options =>
+            options.ReturnHttpNotAcceptable = true
+            ).SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
