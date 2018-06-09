d3e583dab4bc645dc992495537d7348791078f11
桂素伟 <axzxs2001@163.com>
2018-06-09 19:24:51 +0900

    FluentValidation验证。

--- Asp.NetCoreExperiment/MediatRSample/FluentValidation/Program.cs
+++ Asp.NetCoreExperiment/MediatRSample/FluentValidation/Program.cs
@@ -7,7 +7,7 @@ namespace FluentValidationDemo
     {
         static void Main(string[] args)
         {
-            var customer = new Customer();
+            var customer = new Customer() { AginPassword = "111" };
             var validator = new CustomerValidator();
             var results = validator.Validate(customer, ruleSet: "Names");
             foreach (var error in results.Errors)
@@ -22,8 +22,15 @@ namespace FluentValidationDemo
         {
             RuleSet("Names", () =>
             {
-                RuleFor(x => x.Surname).NotNull().WithName(x=>"ddd");
+                RuleFor(x => x.Surname).NotNull().WithName(x => "ddd");
                 RuleFor(x => x.Forename).NotNull().WithMessage("surname empty");
+                RuleFor(x => x.Password).NotNull().NotEmpty().WithMessage("不能为空");
+                RuleFor(x => x.AginPassword).NotEmpty().NotNull().Equal(x => x.Password).WithMessage("要和Password相等");
+                RuleFor(x => x.Surname).Must(surname =>
+                {
+                    return surname.Length > 0;
+
+                });
             });
             //RuleFor(customer => customer.Surname).NotNull();
         }
@@ -35,5 +42,8 @@ namespace FluentValidationDemo
         public string Forename { get; set; }
         public decimal Discount { get; set; }
         public string Address { get; set; }
+
+        public string Password { get; set; }
+        public string AginPassword { get; set; }
     }
 }
