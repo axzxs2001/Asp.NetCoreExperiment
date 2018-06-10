f83e5433343b7e4fdecae1087b07471b6a9cf202
桂素伟 <axzxs2001@163.com>
2018-06-10 18:41:22 +0900

    FluentValidation Test

--- Asp.NetCoreExperiment/MediatRSample/FluentValidation/Program.cs
+++ Asp.NetCoreExperiment/MediatRSample/FluentValidation/Program.cs
@@ -8,16 +8,24 @@ namespace FluentValidationDemo
     {
         static void Main(string[] args)
         {
-            var customer = new Customer() {
-                Address =new Address ()
+            var customer = new Customer()
+            {
+                SurePassword = "1",
+                Address = new Address(),
+                Positions = new List<Position> {
+                    new Position{ NO="no001"},
+                    new Position{Name="name001"}
+                }
             };
             var validator = new CustomerValidator();
-            var results = validator.Validate(customer);
+            var results = validator.Validate(customer, ruleSet: "*");
             if (!results.IsValid)
             {
                 foreach (var error in results.Errors)
                 {
-                    Console.WriteLine(error.ErrorMessage);
+                    Console.WriteLine("Property name: " + error.PropertyName);
+                    Console.WriteLine("Error: " + error.ErrorMessage);
+                    Console.WriteLine("");
                 }
             }
         }
@@ -29,21 +37,31 @@ namespace FluentValidationDemo
     {
         public CustomerValidator()
         {
-            RuleFor(customer => customer.Name).NotEmpty().WithMessage(x => "姓名不能为空！");
-            RuleFor(customer => customer.ID).GreaterThan(0).WithName(x => "编号");
-            RuleFor(customer => customer.Password).NotEmpty().WithMessage("密码不能为空");
-            RuleFor(customer => customer.SurePassword).Equal(customer => customer.Password).WithMessage("确认密码要与密码相同");
-            RuleFor(customer => customer.Age).Custom((age, context) =>
+            RuleSet("sigle", () =>
             {
-                if(age<18||age>45)
+                RuleFor(customer => customer.Name).NotEmpty().WithMessage(x => "姓名不能为空！");
+                RuleFor(customer => customer.ID).GreaterThan(0).WithName(x => "编号");
+                RuleFor(customer => customer.Password).NotEmpty().WithMessage("密码不能为空");
+                RuleFor(customer => customer.SurePassword).Equal(customer => customer.Password).WithMessage("确认密码要与密码相同");
+                RuleFor(customer => customer.Age).Custom((age, context) =>
                 {
-                    context.AddFailure("年龄必需在18和45之间");
-                }
+                    if (age < 18 || age > 45)
+                    {
+                        context.AddFailure("年龄必需在18和45之间");
+                    }
+                });
+                RuleFor(customer => customer.Address).SetValidator(new AddressValidator());
             });
 
-            RuleForEach(customer => customer.Titles).NotEmpty().WithMessage("每个Title不能为空");
 
-            RuleFor(customer => customer.Address).SetValidator(new AddressValidator());
+            RuleSet("lists", () =>
+            {
+                RuleForEach(customer => customer.Titles).NotEmpty().WithMessage("每个Title不能为空");
+
+            });
+
+            //where是移除不作验证的
+            RuleFor(customer => customer.Positions).SetCollectionValidator(new PositionValidator()).Where(x => x.NO != null);
 
         }
     }
@@ -51,8 +69,18 @@ namespace FluentValidationDemo
     {
         public AddressValidator()
         {
-            RuleFor(address => address.Postcode).NotEmpty().WithMessage("地址邮编不能为空");
-       
+            RuleFor(address => address.Postcode).Length(7).WithMessage("邮编必需就7位");
+            RuleFor(address => address.County).NotEmpty().WithMessage("国家不能为空");
+
+        }
+    }
+    public class PositionValidator : AbstractValidator<Position>
+    {
+        public PositionValidator()
+        {
+            RuleFor(position => position.NO).NotEmpty().WithMessage("NO不能为空");
+            RuleFor(position => position.Name).NotEmpty().WithMessage("Name不能为空");
+
         }
     }
     /// <summary>
@@ -69,10 +97,17 @@ namespace FluentValidationDemo
 
         public int Age { get; set; }
 
-        public List<string> Titles { get; set; } = new List<string>() {"厂长",""};
+        public List<string> Titles { get; set; } = new List<string>() { "厂长", "" };
 
         public Address Address { get; set; }
 
+        public List<Position> Positions { get; set; }
+
+    }
+    public class Position
+    {
+        public string NO { get; set; }
+        public string Name { get; set; }
     }
 
     public class Address
