be26e7dcdc2a964550d11a42ddcabffbb7871398
桂素伟 <axzxs2001@163.com>
2018-06-21 17:46:55 +0900

    热更新事例。

---
+++ Asp.NetCoreExperiment/ConfigurationDemo/SnapshotConfig/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SnapshotConfig.Controllers;
+
+namespace SnapshotConfig
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+      
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<AppSetting>(Configuration.GetSection("AppSetting"));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+     
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
