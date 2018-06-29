32f58eb93898b74dd28f0c5509d7d8e994abb377
桂素伟 <axzxs2001@163.com>
2018-06-29 10:58:43 +0900

    异常处理。

---
+++ Asp.NetCoreExperiment/TaskAsync/TaskAsyncDemo04/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TaskAsyncDemo04
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+            //GetPageSizeAsync("http://www.baidu.com").Wait(); //等待
+            //GetPageSizeAsync("http://www.baidu.com").GetAwaiter().GetResult();  //等待
+            GetPageSizeAsync("http://www.baidu.com").GetAwaiter();  //不等待
+                                                                    //GetPageSizeAsync("http://www.baidu.com");  //不等待
+            Console.WriteLine($"Main: {DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff")}");
+            Console.ReadLine();
+
+        }
+
+        private static async Task GetPageSizeAsync(string url)
+        {
+            var client = new HttpClient();
+            var uri = new Uri(Uri.EscapeUriString(url));
+            byte[] urlContents = await client.GetByteArrayAsync(uri);
+            Console.WriteLine($"GetPageSizeAsync: {DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff")}");
+            Console.WriteLine($"{url}: {urlContents.Length / 2:N0} characters");
+        }
+    }
+}
