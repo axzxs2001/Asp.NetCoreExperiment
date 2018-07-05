52822f0be3a52785a3a6ec02d64275dcf6f638ff
桂素伟 <axzxs2001@163.com>
2018-07-05 18:28:32 +0900

    测试asp.net core的Raft同步问题。

---
+++ Asp.NetCoreExperiment/RaftDemo/RaftDemo02_Web/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace RaftDemo02_Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args == null || args.Length == 0)
+            {
+                args = new string[] { "5000" };
+            }
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var host = WebHost.CreateDefaultBuilder(args)
+                 .UseUrls($"http://*:{args[0]}")
+                 .UseStartup<Startup>();
+            return host;
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/RaftDemo/RaftDemo02_Web/Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Raft;
+using Raft.Transport;
+
+namespace RaftDemo02_Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSingleton<RaftOpt>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+
+    public class RaftOpt
+    {
+        TcpRaftNode _node;
+
+        public RaftOpt(IConfiguration configuration)
+        {
+            var port = Convert.ToInt32(configuration.GetSection("AppSetting").GetSection("Port").Value);
+            var name = configuration.GetSection("AppSetting").GetSection("Name").Value;
+            Console.Title = name + "   port:" + port;
+            var log = new Logger();
+            var config = System.IO.File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + @"\config.txt");
+            _node = TcpRaftNode.GetFromConfig(1, config,
+                           System.IO.Directory.GetCurrentDirectory() + $@"\DBreeze\{name}", port, log,
+                           (entityName, index, data) =>
+                           {
+                               Console.WriteLine($"{entityName}/{index} { System.Text.Encoding.UTF8.GetString(data)}");
+                               return true;
+                           });
+            _node.Start();
+        }
+
+        public void Add(string content)
+        {
+            Console.WriteLine(content);
+            _node.AddLogEntry(System.Text.Encoding.UTF8.GetBytes(content));
+        }
+    }
+
+    public class Logger : IWarningLog
+    {
+        public void Log(WarningLogEntry logEntry)
+        {
+            Console.WriteLine(logEntry.ToString());
+        }
+    }
+}
