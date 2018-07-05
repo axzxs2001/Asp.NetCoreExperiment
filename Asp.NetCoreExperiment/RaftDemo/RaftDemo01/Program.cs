bbbaf050b7e900be061f100af13d0bf0744e2290
桂素伟 <axzxs2001@163.com>
2018-07-05 14:27:54 +0900

    测试失败补全情况。

--- Asp.NetCoreExperiment/RaftDemo/RaftDemo01/Program.cs
+++ Asp.NetCoreExperiment/RaftDemo/RaftDemo01/Program.cs
@@ -12,13 +12,13 @@ namespace RaftDemo01
 
             log = new Logger();
 
-            var config = System.IO.File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + @"\a.txt");
+            var config = System.IO.File.ReadAllText(System.IO.Directory.GetCurrentDirectory() + @"\config.txt");
             TcpRaftNode rn1 = TcpRaftNode.GetFromConfig(1, config,
                            System.IO.Directory.GetCurrentDirectory() + @"\DBreeze\Node1", 4250, log,
                            (entityName, index, data) =>
                            {
                                Console.WriteLine($"Committed {entityName}/{index}");
-                               Console.WriteLine("4250数据：" + System.Text.Encoding.UTF8.GetString(data));
+                               Console.WriteLine("0000000000000      4250数据：" + System.Text.Encoding.UTF8.GetString(data));
                                return true;
                            });
 
@@ -27,8 +27,20 @@ namespace RaftDemo01
                            (entityName, index, data) =>
                            {
                                Console.WriteLine($"Committed {entityName}/{index}");
-                               Console.WriteLine("4251数据：" + System.Text.Encoding.UTF8.GetString(data));
-                               return true;
+                               Console.WriteLine("111111111111      4251数据：" + System.Text.Encoding.UTF8.GetString(data));
+                               var random = new Random();
+                               var num = random.Next(1, 3) % 2;
+                               Console.WriteLine(num);
+
+                               if (num == 1)
+                               {
+                                   return true;
+                               }
+                               else
+                               {
+                                   return false;
+                               }
+
                            });
 
             TcpRaftNode rn3 = TcpRaftNode.GetFromConfig(1, config,
@@ -36,7 +48,7 @@ namespace RaftDemo01
                            (entityName, index, data) =>
                            {
                                Console.WriteLine($"Committed {entityName}/{index}");
-                               Console.WriteLine("4252数据：" + System.Text.Encoding.UTF8.GetString(data));
+                               Console.WriteLine("222222222222       4252数据：" + System.Text.Encoding.UTF8.GetString(data));
                                return true;
                            });
 
@@ -47,21 +59,22 @@ namespace RaftDemo01
 
             while (true)
             {
+                Console.Clear();
                 Console.WriteLine("选择要发送的：1、4250     2、4251     3、4252");
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        rn1.AddLogEntry(System.Text.Encoding.UTF8.GetBytes(DateTime.Now.ToString()));
+                        rn1.AddLogEntry(System.Text.Encoding.UTF8.GetBytes(DateTime.Now.ToString()), "inMemory1");
                         break;
                     case "2":
-                        rn2.AddLogEntry(System.Text.Encoding.UTF8.GetBytes(DateTime.Now.ToString()));
+                        rn2.AddLogEntry(System.Text.Encoding.UTF8.GetBytes(DateTime.Now.ToString()), "inMemory1");
                         break;
                     case "3":
-                        rn3.AddLogEntry(System.Text.Encoding.UTF8.GetBytes(DateTime.Now.ToString()));
+                        rn3.AddLogEntry(System.Text.Encoding.UTF8.GetBytes(DateTime.Now.ToString()), "inMemory1");
                         break;
                 }
             }
-      
+
 
         }
         public class Logger : IWarningLog
