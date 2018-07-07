80b9494c9dc171ef6d06c12ce635f608dbf45cff
桂素伟 <axzxs2001@163.com>
2018-07-07 23:17:21 +0900

    测试项目。

---
+++ Asp.NetCoreExperiment/RaftDemo/RaftyDemo02/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Rafty.Concensus.Node;
+using Rafty.Concensus.Peers;
+using Rafty.FiniteStateMachine;
+using Rafty.Infrastructure;
+using Rafty.Log;
+
+namespace RaftyDemo02.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        ILoggerFactory _loggerFactory;
+        public ValuesController(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+
+      
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+
+
+
+            return "value";
+        }
+
+      
+    }
+}
---
+++ Asp.NetCoreExperiment/RaftDemo/RaftyDemo02/Startup.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Rafty.Concensus.Node;
+using Rafty.Concensus.Peers;
+using Rafty.FiniteStateMachine;
+using Rafty.Infrastructure;
+using Rafty.Log;
+
+namespace RaftyDemo02
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            var log = new InMemoryLog();
+            var fsm = new InMemoryStateMachine();
+            var settings = new InMemorySettings(1000, 3500, 50, 5000);
+
+            var _peers = new List<IPeer>();
+            var peer1 = new NodePeer();
+    
+            _peers.Add(new NodePeer() { });
+            var peersProvider = new InMemoryPeersProvider(_peers);
+            var node = new Node(fsm, log, settings, peersProvider, loggerFactory);
+            node.Start(new NodeId("gsw" + DateTime.Now.ToString("HHmmssfff")));
+            
+            app.UseMvc();
+        }
+    }
+}
