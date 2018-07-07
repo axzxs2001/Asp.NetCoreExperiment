f341f189dd548d88842cbb6c1f1f84c8e90fa52d
桂素伟 <axzxs2001@163.com>
2018-07-07 22:46:26 +0900

    Rafty测试

---
+++ Asp.NetCoreExperiment/RaftDemo/RaftyDemo01/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Rafty.Concensus.Node;
+using Rafty.Concensus.Peers;
+using Rafty.FiniteStateMachine;
+using Rafty.Infrastructure;
+using Rafty.Log;
+using System;
+using System.Collections.Generic;
+
+namespace RaftyDemo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new InMemoryLog();
+       
+            var fsm = new InMemoryStateMachine();
+            var settings = new InMemorySettings(1000, 3500, 50, 5000);
+
+            var _peers = new List<IPeer>();
+            var peersProvider = new InMemoryPeersProvider(_peers);
+            var node = new Node(fsm, log, settings, peersProvider,null);
+            node.Start(new NodeId("gsw"));
+        }
+    }
+}
