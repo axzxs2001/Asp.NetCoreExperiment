33278a361e85fb5587da75cd6e8e87cda9134712
桂素伟 <axzxs2001@163.com>
2018-07-17 14:55:10 +0900

    测试asp.net core日志处理。

---
+++ Asp.NetCoreExperiment/LogDemos/LogDemo01/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LogDemo01.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        readonly ILogger<ValuesController> _logger;
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _logger.LogTrace("values.Get方法-LogTrace跟踪");
+            _logger.LogDebug("values.Get方法-LogDebug调试");
+            _logger.LogInformation("values.Get方法-LogInformation信息 ");
+            _logger.LogWarning("values.Get方法-LogWarning警告 ");
+            _logger.LogError("values.Get方法-LogError错误 ");
+            _logger.LogCritical("values.Get方法-LogCritical严重 ");          
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/LogDemos/LogDemo01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace LogDemo01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // NLog: setup the logger first to catch all errors
+            var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("启动Main");
+                BuildWebHost(args).Run();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                logger.Error(ex, "异常停止进程");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                NLog.LogManager.Shutdown();
+            }
+
+        }
+
+        /// <summary>
+        /// 构建webhost
+        /// </summary>
+        /// <param name="args">启动参数</param>
+        /// <returns></returns>
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseUrls("http://*:5000")
+            .UseStartup<Startup>()          
+            .UseNLog() // NLog: setup NLog for Dependency injectio
+            .Build();
+    }
+}
