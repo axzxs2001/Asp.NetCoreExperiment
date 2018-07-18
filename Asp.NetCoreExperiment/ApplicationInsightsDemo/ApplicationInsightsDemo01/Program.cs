fb79b5ff15d343d7187683bc7c02deff696a4585
桂素伟 <axzxs2001@163.com>
2018-07-18 19:31:43 +0900

    Application Insights测试。

---
+++ Asp.NetCoreExperiment/ApplicationInsightsDemo/ApplicationInsightsDemo01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ApplicationInsightsDemo01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
+                .UseStartup<Startup>();
+    }
+}
