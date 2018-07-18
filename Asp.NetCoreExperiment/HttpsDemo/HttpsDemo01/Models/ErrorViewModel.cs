619a44347128c7caa53bb2c2895e3ff4bbd83d2d
桂素伟 <axzxs2001@163.com>
2018-07-18 16:59:35 +0900

    开启ssl，开启https访问，添加pfx证书方式。

---
+++ Asp.NetCoreExperiment/HttpsDemo/HttpsDemo01/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HttpsDemo01.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Asp.NetCoreExperiment/HttpsDemo/HttpsDemo01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace HttpsDemo01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                //添加.pfx证书方式，debian和win10测试成功
+                .UseKestrel(kestrelOptions =>
+                {
+                    kestrelOptions.Listen(IPAddress.Any, 5000);
+                    kestrelOptions.Listen(IPAddress.Any, 5001, listenOptions =>
+                    {
+                        listenOptions.UseHttps("证书名称.pfx", "证书密码");
+                    });
+                });
+    }
+}
