84402d46216c1061220fd2e343ea201753778e10
桂素伟 <axzxs2001@163.com>
2018-07-26 10:46:52 +0900

    Proto.Actor Remote测试

---
+++ Asp.NetCoreExperiment/ProtoActor/ProtoRemote01/Program.cs
@@ -0,0 +1,54 @@
+﻿using Messages;
+using Proto;
+using Proto.Remote;
+using System;
+using ProtosReflection = Messages.ProtosReflection;
+namespace ProtoRemote01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "服务端";
+            Console.WriteLine("回车开始");
+            Console.ReadLine();
+
+            Serialization.RegisterFileDescriptor(ProtosReflection.Descriptor);
+            var props = Actor.FromFunc(ctx =>
+            {
+                switch (ctx.Message)
+                {
+                    case HelloRequest msg:
+                        ctx.Respond(new HelloResponse
+                        {
+                            Message = "回应：我是服务端",
+                        });
+                        break;
+                    default:
+                        break;
+                }
+                return Actor.Done;
+            });
+           
+            Remote.RegisterKnownKind("hello", props);
+            Remote.Start("127.0.0.1", 12000);
+            Console.WriteLine("服务端开始……");
+            Console.ReadLine();
+        }
+    }
+
+
+    //public class HelloRequest
+    //{
+    //    public string Message
+    //    {
+    //        get; set;
+    //    }
+    //}
+
+    //public class HelloResponse
+    //{
+    //    public string Message
+    //    { get; set; }
+    //}
+}
---
+++ Asp.NetCoreExperiment/ProtoActor/ProtoRemote02/Program.cs
@@ -0,0 +1,24 @@
+﻿using Messages;
+using Proto.Remote;
+using ProtoRemote01;
+using System;
+using ProtosReflection = Messages.ProtosReflection;
+namespace ProtoRemote02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "客户端";
+            Console.WriteLine("回车开始");
+            Console.ReadLine();
+            Serialization.RegisterFileDescriptor(ProtosReflection.Descriptor);
+            Remote.Start("127.0.0.1", 12001);           
+      
+            var pid = Remote.SpawnNamedAsync("127.0.0.1:12000","remote","hello", TimeSpan.FromSeconds(5)).Result.Pid;
+            var res = pid.RequestAsync<HelloResponse>(new HelloRequest { }).Result;
+            Console.WriteLine(res.Message);
+            Console.ReadLine();
+        }
+    }
+}
