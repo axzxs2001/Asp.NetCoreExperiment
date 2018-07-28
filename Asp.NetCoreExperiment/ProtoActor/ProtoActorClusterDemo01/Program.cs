5f308d636080b6b82ebb0779920727c4e3b702ac
桂素伟 <axzxs2001@163.com>
2018-07-28 11:34:39 +0900

    利用Consul集群实现Proto.Actor集群。

--- Asp.NetCoreExperiment/ProtoActor/ProtoActorClusterDemo01/Program.cs
+++ Asp.NetCoreExperiment/ProtoActor/ProtoActorClusterDemo01/Program.cs
@@ -4,6 +4,7 @@ using Proto.Serialization.Wire;
 using System;
 using System.Diagnostics;
 using System.Threading;
+
 using Proto.Cluster.Consul;
 
 
@@ -13,42 +14,25 @@ namespace ProtoActorClusterDemo01
     {
         static void Main(string[] args)
         {
-            var context = new RootContext();
+            Console.Title = "客户端";
+        
             var wire = new WireSerializer(new[] { typeof(HelloRequest), typeof(HelloResponse) });
             Serialization.RegisterSerializer(wire, true);
-            var parsedArgs = parseArgs(args);
-            // SINGLE REMOTE INSTANCE
-            Cluster.Start("MyCluster", parsedArgs.ServerName, 12002, new SingleRemoteInstanceProvider("127.0.0.1", 12000));
-
-            // CONSUL 
-            //if(parsedArgs.StartConsul)
-            //{
-            //    StartConsulDevMode();
-            //}
-            //Cluster.Start("MyCluster", parsedArgs.ServerName, 12001, new ConsulProvider(new ConsulProviderOptions(), c => c.Address = new Uri("http://" + parsedArgs.ConsulUrl + ":8500/")));
+            var parsedArgs = parseArgs(args);         
+        
+            Cluster.Start("MyCluster", parsedArgs.ServerName, 12001, new ConsulProvider(new ConsulProviderOptions(), c => c.Address = new Uri("http://" + parsedArgs.ConsulUrl + ":8500/")));
 
 
             var (pid, sc) = Cluster.GetAsync("TheName", "HelloKind").Result;
             while (sc != ResponseStatusCode.OK)
                 (pid, sc) = Cluster.GetAsync("TheName", "HelloKind").Result;
-            var res = context.RequestAsync<HelloResponse>(pid, new HelloRequest()).Result;
-            Console.WriteLine(res.Message);
+            var res = pid.RequestAsync<HelloResponse>(new HelloRequest() { Message="请求信息" }).Result;
+            Console.WriteLine("客户端收到信息："+res.Message);
             Thread.Sleep(Timeout.Infinite);
             Console.WriteLine("Shutting Down...");
             Cluster.Shutdown();
         }
-        private static void StartConsulDevMode()
-        {
-            Console.WriteLine("Consul - Starting");
-            ProcessStartInfo psi =
-                new ProcessStartInfo(@"..\..\..\dependencies\consul",
-                    "agent -server -bootstrap -data-dir /tmp/consul -bind=127.0.0.1 -ui")
-                {
-                    CreateNoWindow = true,
-                };
-            Process.Start(psi);
-            Console.WriteLine("Consul - Started");
-        }
+    
 
         private static Node1Config parseArgs(string[] args)
         {
--- Asp.NetCoreExperiment/ProtoActor/ProtoActorClusterDemo02/Program.cs
+++ Asp.NetCoreExperiment/ProtoActor/ProtoActorClusterDemo02/Program.cs
@@ -1,12 +1,66 @@
 ﻿using System;
+using System.Threading;
+using Proto;
+using Proto.Cluster;
+using Proto.Cluster.Consul;
+using Proto.Remote;
+using Proto.Serialization.Wire;
+using ProtoActorClusterDemo01;
 
-namespace ProtoActorClusterDemo02
+namespace Node2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "服务端";
+          
+            var wire = new WireSerializer(new[] { typeof(HelloRequest), typeof(HelloResponse) });
+            Serialization.RegisterSerializer(wire, true);
+            var props = Actor.FromFunc(ctx =>
+            {
+                switch (ctx.Message)
+                {
+                    case HelloRequest helloRequest:
+                        Console.WriteLine("服务端接收到："+helloRequest.Message);
+                        ctx.Respond(new HelloResponse
+                        {
+                            Message = "Hello from node 1"
+                        });
+                        break;
+                }
+                return Actor.Done;
+            });
+
+            var parsedArgs = ParseArgs(args);
+            Remote.RegisterKnownKind("HelloKind", props);
+
+        
+            Cluster.Start("MyCluster", parsedArgs.ServerName, 12000, new ConsulProvider(new ConsulProviderOptions(), c => c.Address = new Uri("http://" + parsedArgs.ConsulUrl + ":8500/")));
+
+            Thread.Sleep(Timeout.Infinite);
+            Console.WriteLine("Shutting Down...");
+            Cluster.Shutdown();
+        }
+
+        private static Node2Config ParseArgs(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                return new Node2Config(args[0], args[1]);
+            }
+            return new Node2Config("127.0.0.1", "127.0.0.1");
+        }
+
+        class Node2Config
+        {
+            public string ServerName { get; }
+            public string ConsulUrl { get; }
+            public Node2Config(string serverName, string consulUrl)
+            {
+                ServerName = serverName;
+                ConsulUrl = consulUrl;
+            }
         }
     }
 }
