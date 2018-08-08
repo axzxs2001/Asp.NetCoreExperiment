6936d577dce0dd1ba8cd265cb3fba765336f3c83
桂素伟 <axzxs2001@163.com>
2018-08-08 09:32:23 +0900

    用sqlite持久化。

--- Asp.NetCoreExperiment/ProtoActor/ProtoPersistenceDemo01/Program.cs
+++ Asp.NetCoreExperiment/ProtoActor/ProtoPersistenceDemo01/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using Proto;
+using Proto.Persistence;
+using Proto.Persistence.Sqlite;
+using System;
+using System.Threading.Tasks;
 
 namespace ProtoPersistenceDemo01
 {
@@ -6,7 +11,55 @@ namespace ProtoPersistenceDemo01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var actorid = "this1234";
+            var dbfile = @"C:\MyFile\Source\Repos\Asp.NetCoreExperiment\Asp.NetCoreExperiment\ProtoActor\ProtoPersistenceDemo01\data.sqlite";
+            var sqliteProvider = new SqliteProvider(new SqliteConnectionStringBuilder() { DataSource= dbfile });            
+            var c = new Counter(sqliteProvider, actorid);
+            var props = Actor.FromProducer(() => c);
+            var pid = Actor.Spawn(props);
+            for (int i = 0; i < 2; i++)
+            {
+                var no = int.Parse(Console.ReadLine());
+                pid.Tell(new Added { Amount = no });
+            }          
+        }
+    }
+    public class Added
+    {
+        public int Amount { get; set; }
+    }
+    public class Counter : IActor
+    {
+        private int _value = 0;
+        private readonly Persistence _persistence;
+
+        public Counter(IEventStore eventStore, string actorId)
+        {
+            _persistence = Persistence.WithEventSourcing(eventStore, actorId, ApplyEvent);
+        }
+        private void ApplyEvent(Proto.Persistence.Event @event)
+        {
+            switch (@event.Data)
+            {
+                case Added msg:
+                    _value = _value + msg.Amount;
+                    break;
+            }
+        }
+        public async Task ReceiveAsync(IContext context)
+        {
+            switch (context.Message)
+            {
+                case Started _:
+                    await _persistence.RecoverStateAsync();
+                    break;
+                case Added msg:
+                    if (msg.Amount > 0)
+                    {
+                        await _persistence.PersistEventAsync(new Added { Amount = msg.Amount });
+                    }
+                    break;
+            }
         }
     }
 }
