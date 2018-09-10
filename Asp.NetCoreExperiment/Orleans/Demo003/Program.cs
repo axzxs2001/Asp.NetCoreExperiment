1af3f4013e0abf4d35a00815dee11b94af26392b
桂素伟 <axzxs2001@163.com>
2018-09-10 09:58:48 +0900

    Demo实例

---
+++ Asp.NetCoreExperiment/Orleans/Demo003/Program.cs
@@ -0,0 +1,88 @@
+﻿using Orleans;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo003
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+    }
+
+    public interface IGameGrain
+    {
+        Guid GetPrimaryKey();
+    }
+    public class GameGrain : IGameGrain
+    {
+        public Guid GetPrimaryKey()
+        {
+            throw new NotImplementedException();
+        }
+        //public Task JoinGame(IGameGrain gameGrain)
+        //{
+        //    IPlayerGrain player=null;
+        //    //Invoking a grain method asynchronously
+        //    Task joinGameTask = player.JoinGame(this);
+        //    //The await keyword effectively makes the remainder of the method execute asynchronously at a later point (upon completion of the Task being awaited) without blocking the thread.
+        //    await joinGameTask;
+        //    //The next line will execute later, after joinGameTask is completed.
+        //    players.Add(playerId);
+        //}
+    }
+    public interface IPlayerGrain : IGrainWithGuidKey
+    {
+        Task<IGameGrain> GetCurrentGame();
+        Task JoinGame(IGameGrain game);
+        Task LeaveGame(IGameGrain game);
+    }
+
+    public class PlayerGrain : Grain, IPlayerGrain
+    {
+        private IGameGrain currentGame;
+
+        public IPlayerGrain GetPlayerGrain()
+        {
+            IPlayerGrain player = GrainFactory.GetGrain<IPlayerGrain>(new Guid());
+            return player;
+        }
+
+        // Game the player is currently in. May be null.
+        public Task<IGameGrain> GetCurrentGame()
+        {           
+         
+            return Task.FromResult(currentGame);
+        }
+
+        // Game grain calls this method to notify that the player has joined the game.
+        public Task JoinGame(IGameGrain game)
+        {
+            currentGame = game;
+            Console.WriteLine(
+                "Player {0} joined game {1}",
+                this.GetPrimaryKey(),
+                game.GetPrimaryKey());
+
+            return Task.CompletedTask;
+        }
+
+        // Game grain calls this method to notify that the player has left the game.
+        public Task LeaveGame(IGameGrain game)
+        {
+            currentGame = null;
+            Console.WriteLine(
+                "Player {0} left game {1}",
+                this.GetPrimaryKey(),
+                game.GetPrimaryKey());
+
+            return Task.CompletedTask;
+        }
+    }
+}
