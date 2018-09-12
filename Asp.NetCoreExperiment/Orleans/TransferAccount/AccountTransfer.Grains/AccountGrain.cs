871b7ab07663eeecf56070c524928ae3a02083c7
桂素伟 <axzxs2001@163.com>
2018-09-12 16:24:29 +0900

    Orleans转帐测试。

---
+++ Asp.NetCoreExperiment/Orleans/TransferAccount/AccountTransfer.Grains/AccountGrain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading.Tasks;
+using Orleans;
+using Orleans.CodeGeneration;
+using Orleans.Transactions.Abstractions;
+using AccountTransfer.Interfaces;
+
+[assembly: GenerateSerializer(typeof(AccountTransfer.Grains.Balance))]
+
+namespace AccountTransfer.Grains
+{
+    [Serializable]
+    public class Balance
+    {
+        public uint Value { get; set; } = 1000;
+    }
+
+    public class AccountGrain : Grain, IAccountGrain
+    {
+        private readonly ITransactionalState<Balance> balance;
+
+        public AccountGrain(
+            [TransactionalState("balance")] ITransactionalState<Balance> balance)
+        {
+            this.balance = balance ?? throw new ArgumentNullException(nameof(balance));
+        }
+
+        Task IAccountGrain.Deposit(uint amount)
+        {
+            this.balance.State.Value += amount;    
+            var random = new Random();
+            if (random.Next(1, 10) % 2 == 0)
+            {
+                throw new Exception("11111");
+            }
+            else
+            {
+                this.balance.Save();
+                return Task.CompletedTask;
+            }
+        }
+
+        Task IAccountGrain.Withdraw(uint amount)
+        {
+            this.balance.State.Value -= amount;
+            this.balance.Save();
+            return Task.CompletedTask;
+        }
+
+        Task<uint> IAccountGrain.GetBalance()
+        {
+            return Task.FromResult(this.balance.State.Value);
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/Orleans/TransferAccount/OrleansClient/Program.cs
@@ -0,0 +1,103 @@
+﻿using AccountTransfer.Interfaces;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Runtime;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OrleansClient
+{
+    /// <summary>
+    /// Orleans test silo client
+    /// </summary>
+    public class Program
+    {
+        static int Main(string[] args)
+        {
+            return RunMainAsync().Result;
+        }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                using (var client = await StartClientWithRetries())
+                {
+                    await DoClientWork(client);
+                    Console.ReadKey();
+                }
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return 1;
+            }
+        }
+
+        private static async Task<IClusterClient> StartClientWithRetries(int initializeAttemptsBeforeFailing = 5)
+        {
+            int attempt = 0;
+            IClusterClient client;
+            while (true)
+            {
+                try
+                {
+                    int gatewayPort = 30000;
+                    var siloAddress = IPAddress.Loopback;
+                    var gateway = new IPEndPoint(siloAddress, gatewayPort);
+
+                    client = new ClientBuilder()
+                        .UseLocalhostClustering()
+                        .Configure<ClusterOptions>(options =>
+                        {
+                            options.ClusterId = "dev";
+                            options.ServiceId = "AccountTransferApp";
+                        })
+                        .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IAccountGrain).Assembly).WithReferences())
+                        .ConfigureLogging(logging => logging.AddConsole())
+                        .Build();
+
+                    await client.Connect();
+                    Console.WriteLine("Client successfully connect to silo host");
+                    break;
+                }
+                catch (SiloUnavailableException)
+                {
+                    attempt++;
+                    Console.WriteLine($"Attempt {attempt} of {initializeAttemptsBeforeFailing} failed to initialize the Orleans client.");
+                    if (attempt > initializeAttemptsBeforeFailing)
+                    {
+                        throw;
+                    }
+                    await Task.Delay(TimeSpan.FromSeconds(4));
+                }
+            }
+
+            return client;
+        }
+
+        private static async Task DoClientWork(IClusterClient client)
+        {
+
+            IATMGrain atm = client.GetGrain<IATMGrain>(0);
+            Guid from = Guid.NewGuid();
+            Guid to = Guid.NewGuid();
+            try
+            {
+                await atm.Transfer(from, to, 100);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            uint fromBalance = await client.GetGrain<IAccountGrain>(from).GetBalance();
+            uint toBalance = await client.GetGrain<IAccountGrain>(to).GetBalance();
+            Console.WriteLine($"\n\nWe transfered 100 credits from {from} to {to}.\n{from} balance: {fromBalance}\n{to} balance: {toBalance}\n\n");
+
+        }
+    }
+}
