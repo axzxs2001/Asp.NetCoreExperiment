e0cff556a6a2658b0b2d9842b32b3a9e9d3c5584
桂素伟 <axzxs2001@163.com>
2018-09-12 19:34:20 +0900

    Grain测试.

---
+++ Asp.NetCoreExperiment/Orleans/Demo004/Program.cs
@@ -0,0 +1,47 @@
+﻿using Orleans;
+using Orleans.Runtime;
+using System;
+using System.Threading.Tasks;
+
+namespace Demo004
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new TestGrain();
+            test.MyTest();
+            Console.ReadLine();
+
+             IGrainFactory
+        }
+    }
+
+    interface ITestGrain
+    {
+        Task MyTest();
+    }
+    class TestGrain : Grain, ITestGrain
+    {
+        public Task MyTest()
+        {
+            Console.WriteLine("mytest");
+            return Task.CompletedTask;
+        }
+        public override Task OnActivateAsync()
+        {
+            Console.WriteLine("OnActivateAsync");
+            return base.OnActivateAsync();
+        }
+        public override Task OnDeactivateAsync()
+        {
+            Console.WriteLine("OnDeactivateAsync");
+            return base.OnDeactivateAsync();
+        }
+        public override void Participate(IGrainLifecycle lifecycle)
+        {
+            Console.WriteLine("Participate");
+            base.Participate(lifecycle);
+        }
+    }
+}
