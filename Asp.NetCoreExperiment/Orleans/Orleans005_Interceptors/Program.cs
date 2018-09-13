3a9e6c87b825560779eb005a3a94611af6f87975
桂素伟 <axzxs2001@163.com>
2018-09-13 14:33:57 +0900

    Reentrancy

---
+++ Asp.NetCoreExperiment/Orleans/Orleans005_Interceptors/Program.cs
@@ -0,0 +1,44 @@
+﻿using Orleans;
+using System;
+using System.Threading.Tasks;
+
+namespace Orleans005_Interceptors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public interface IMyFilteredGrain
+    {
+
+    }
+    public class MyFilteredGrain : Grain, IMyFilteredGrain, IIncomingGrainCallFilter, IOutgoingGrainCallFilter
+    {
+        public async Task Invoke(IIncomingGrainCallContext context)
+        {
+            await context.Invoke();
+
+            // Change the result of the call from 7 to 38.
+            if (string.Equals(context.InterfaceMethod.Name, nameof(this.GetFavoriteNumber)))
+            {
+                context.Result = 38;
+            }
+        }
+
+        public Task<int> GetFavoriteNumber() => Task.FromResult(7);
+
+        public async Task Invoke(IOutgoingGrainCallContext context)
+        {
+            await context.Invoke();
+
+            // Change the result of the call from 7 to 38.
+            if (string.Equals(context.InterfaceMethod.Name, nameof(this.GetFavoriteNumber)))
+            {
+                context.Result = 58;
+            }
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/Orleans/Orleans006_Scheduler/Program.cs
@@ -0,0 +1,37 @@
+﻿using Orleans;
+using Orleans.Concurrency;
+using System;
+using System.Threading.Tasks;
+
+namespace Orleans006_Reentrancy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new ClientBuilder().Build();
+            var grain = client.GetGrain<IMyGrain>(new Guid());
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+    interface IMyGrain : IGrainWithGuidKey
+    {
+        Task A();
+
+        [AlwaysInterleave]
+        Task B();
+    }
+    class MyGrain : Grain, IMyGrain
+    {
+        public Task A()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task B()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
