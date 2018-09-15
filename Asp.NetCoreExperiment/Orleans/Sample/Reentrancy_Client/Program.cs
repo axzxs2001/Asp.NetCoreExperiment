fd939e28578a1cdba6d5eb5650e515427353ca3b
桂素伟 <axzxs2001@163.com>
2018-09-15 16:50:49 +0900

    添加Reentrancy项目

---
+++ Asp.NetCoreExperiment/Orleans/Sample/Reentrancy_Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reentrancy_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/Orleans/Sample/Reentrancy_SiloHost/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Reentrancy_SiloHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
