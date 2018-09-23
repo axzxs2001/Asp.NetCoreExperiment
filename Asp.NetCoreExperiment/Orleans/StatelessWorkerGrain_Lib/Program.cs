043d718970cfa7361f16fb15edb57c114fca322d
桂素伟 <axzxs2001@163.com>
2018-09-23 10:05:34 +0900

    升级包。

---
+++ Asp.NetCoreExperiment/Orleans/StatelessWorkerGrain_Lib/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StatelessWorkerGrain_Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
