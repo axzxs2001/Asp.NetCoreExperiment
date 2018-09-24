8341fb903af8cf851d7f554dfc6ffb44ed7711e6
桂素伟 <axzxs2001@163.com>
2018-09-24 11:03:18 +0900

    ExternalTasksAndGrains测试完成。

---
+++ Asp.NetCoreExperiment/Orleans/ExternalTasksAndGrains_Lib/IHelloGrain.cs
@@ -0,0 +1,55 @@
+﻿
+using Orleans;
+using Orleans.LogConsistency;
+using Orleans.Providers;
+using Orleans.Runtime;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExternalTasksAndGrains_Lib
+{
+    public interface IHelloGrain:IGrainWithGuidKey
+    {
+        Task Method1();
+    }
+
+    public class HelloGrain : Grain, IHelloGrain
+    {
+        public async Task Method1()
+        {
+            // Grab the Orleans task scheduler
+            var orleansTs = TaskScheduler.Current;
+            await Task.Delay(3000);
+            // Current task scheduler did not change, the code after await is still running in the same task scheduler.
+            Console.WriteLine($"1、外部1 orleansTs == TaskScheduler.Current结果:{ orleansTs == TaskScheduler.Current}");
+
+            Task t1 = Task.Run(() =>
+            {
+                // This code runs on the thread pool scheduler, not on Orleans task scheduler
+                Console.WriteLine($"1、t1内部 orleansTs == TaskScheduler.Current结果： {orleansTs == TaskScheduler.Current}");
+                Console.WriteLine($"1、t1内部 TaskScheduler.Default== TaskScheduler.Current结果：{TaskScheduler.Default == TaskScheduler.Current}");
+            });
+            await t1;
+            // We are back to the Orleans task scheduler. 
+            // Since await was executed in Orleans task scheduler context, we are now back to that context.
+
+            Console.WriteLine($"1、外部2 orleansTs == TaskScheduler.Current结果:{ orleansTs == TaskScheduler.Current}");
+
+            // Example of using ask.Factory.StartNew with a custom scheduler to escape from the Orleans scheduler
+            //Task t2 = Task.Factory.StartNew(() =>
+            //{
+            //    // This code runs on the MyCustomSchedulerThatIWroteMyself scheduler, not on the Orleans task scheduler
+            //    Console.WriteLine($"1、t2内部 orleansTs == TaskScheduler.Current结果： {orleansTs == TaskScheduler.Current}");
+
+            //    Assert.AreEqual(MyCustomSchedulerThatIWroteMyself, TaskScheduler.Current);
+            //},
+            //CancellationToken.None, TaskCreationOptions.None,
+            //scheduler: MyCustomSchedulerThatIWroteMyself);
+            //await t2;
+            // We are back to Orleans task scheduler.
+            //Assert.AreEqual(orleansTS, TaskScheduler.Current)
+        }
+    }
+
+}
---
+++ Asp.NetCoreExperiment/Orleans/ExternalTasksAndGrains_SiloHost/Program.cs
@@ -0,0 +1,70 @@
+﻿
+using ExternalTasksAndGrains_Lib;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace ExternalTasksAndGrains_SiloHost
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Title = "Server";
+            var result = RunMainAsync().Result;
+            Console.ReadLine();
+            return result;
+        }
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                var host = await StartSilo();
+                Console.WriteLine("回车结束...");
+                Console.ReadLine();
+                await host.StopAsync();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return 1;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            var assambly = typeof(IHelloGrain).Assembly;
+            var builder = new SiloHostBuilder()
+                   .UseLocalhostClustering()
+                   .Configure<ClusterOptions>(options =>
+                   {
+                       options.ClusterId = "dev";
+                       options.ServiceId = "TestApp";
+                   })                  
+                   .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                   .ConfigureApplicationParts(parts => parts.AddApplicationPart(assambly).WithReferences())
+                   .ConfigureLogging(logging => logging.AddConsole())
+                   .UseInMemoryReminderService()
+                   .UseLocalhostClustering();
+
+
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
+        }
+
+
+
+    }
+
+
+
+}
