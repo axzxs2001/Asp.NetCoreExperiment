f3997fd9a29eefda210cd6086525fde72a6f7b17
桂素伟 <axzxs2001@163.com>
2018-09-26 19:21:39 +0900

    数据保护项目测试。

---
+++ Asp.NetCoreExperiment/DataProtection/DataProtectionDemo002/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DataProtectionDemo002.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        IDataProtector _dataProtector;
+        public ValuesController(IDataProtectionProvider provider)
+        {
+            _dataProtector = provider.CreateProtector("abcdef");
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            var endstring=_dataProtector.Protect("桂素伟");
+            return new string[] { "value1", endstring };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
