d0324e421452bc377b50257754295d7db9b9bb83
桂素伟 <axzxs2001@163.com>
2018-10-02 12:36:15 +0900

    监控测试。

---
+++ Asp.NetCoreExperiment/Orleans/Monitoring_Lib/IHello.cs
@@ -0,0 +1,27 @@
+﻿
+
+using Orleans;
+using Orleans.Concurrency;
+using Orleans.Runtime;
+using System;
+using System.Threading.Tasks;
+
+namespace Monitoring_Lib
+{
+    public interface IHello : IGrainWithGuidKey
+    {
+     
+        Task Method1();
+    }
+
+    public class HelloGrain : Grain, IHello
+    {
+        public async Task Method1()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            throw new Exception("这里发生错误");
+            Console.WriteLine($"方法：Method1  执行,{DateTime.Now.ToString("HH:mm:ss.fff")}");
+        }
+        
+    }
+}
---
+++ Asp.NetCoreExperiment/Orleans/Monitoring_SiloHost/Program.cs
@@ -0,0 +1,81 @@
+﻿
+
+
+using Microsoft.Extensions.Logging;
+using Monitoring_Lib;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using Orleans.Runtime;
+using Orleans.TelemetryConsumers.AI;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+
+namespace Monitoring_SiloHost
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Title = "Server";
+            var result = RunMainAsync().Result;
+            Console.ReadLine();
+            return result;
+        }
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                var host = await StartSilo();
+                Console.WriteLine("回车结束...");
+                Console.ReadLine();
+                await host.StopAsync();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return 1;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            var assambly = typeof(IHello).Assembly;
+            var builder = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "TestApp";
+                })
+                //重入的配置参数
+                .Configure<SchedulingOptions>(options =>
+                {
+                    options.AllowCallChainReentrancy = false;
+
+                })
+                .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(assambly).WithReferences())
+                .ConfigureLogging(logging => logging.AddConsole())
+                .UseInMemoryReminderService()
+                .UseLocalhostClustering()
+                .Configure<TelemetryOptions>(options => options.AddConsumer<AITelemetryConsumer>());   
+
+            
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
+        }
+
+
+
+    }
+
+
+
+}
+
