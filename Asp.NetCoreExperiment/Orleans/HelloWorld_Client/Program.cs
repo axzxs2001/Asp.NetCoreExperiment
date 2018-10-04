a3aff48d70ed30db14d3b36cc22aa5d437530429
桂素伟 <axzxs2001@163.com>
2018-10-04 16:15:16 +0900

    HelloGrain测试。

---
+++ Asp.NetCoreExperiment/Orleans/HelloWorld_Client/Program.cs
@@ -0,0 +1,97 @@
+﻿
+using HelloWorld_Lib;
+using Microsoft.Extensions.Logging;
+using Orleans;
+using Orleans.Configuration;
+using Orleans.Runtime;
+using System;
+using System.Threading.Tasks;
+
+namespace HelloWorld_Client
+{
+    class Program
+    {
+        const int initializeAttemptsBeforeFailing = 5;
+        private static int attempt = 0;
+
+        static void Main(string[] args)
+        {
+            Console.Title = "Client，回车开始"; while (true)
+            {
+                RunMainAsync().Wait();
+            }
+        }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                using (var client = await StartClientWithRetries())
+                {
+
+                    await DoClientWork(client);
+                    Console.ReadKey();
+                }
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.ReadKey();
+                return 1;
+            }
+        }
+
+        private static async Task<IClusterClient> StartClientWithRetries()
+        {
+            attempt = 0;
+            IClusterClient client = new ClientBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "TestApp";
+                })
+                .ConfigureLogging(logging => logging.AddConsole())
+                .Build();
+
+            await client.Connect(RetryFilter);
+            Console.WriteLine("Client成功连接服务端");
+            return client;
+        }
+        /// <summary>
+        /// 重连
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        private static async Task<bool> RetryFilter(Exception exception)
+        {
+            if (exception.GetType() != typeof(SiloUnavailableException))
+            {
+                Console.WriteLine($"集群客户端失败连接，返回unexpected error.  Exception: {exception}");
+                return false;
+            }
+            attempt++;
+            Console.WriteLine($"集群客户端试图 {attempt}/{initializeAttemptsBeforeFailing} 失败连接.  Exception: {exception}");
+            if (attempt > initializeAttemptsBeforeFailing)
+            {
+                return false;
+            }
+            await Task.Delay(TimeSpan.FromSeconds(4));
+            return true;
+        }
+        /// <summary>
+        /// 发送
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        private static async Task DoClientWork(IClusterClient client)
+        {
+
+            var hello = client.GetGrain<IHelloWorldGrain>(new Guid());
+            await hello.Method1();
+
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/Orleans/HelloWorld_Lib/IHello.cs
@@ -0,0 +1,22 @@
+﻿using Orleans;
+using System;
+using System.Threading.Tasks;
+
+namespace HelloWorld_Lib
+{
+
+    public interface IHelloWorldGrain : IGrainWithGuidKey
+    {
+        Task Method1();
+    }
+
+    public class HelloWorldGrain : Grain, IHelloWorldGrain
+    {
+        public Task Method1()
+        {
+            Console.WriteLine("这里是Grain的一个类，IHelloWorld.Method1");
+            return Task.CompletedTask;
+        }
+
+    }
+}
