6a3bf32f38e322bdac4d45b3be3beeea56a5b75e
桂素伟 <axzxs2001@163.com>
2018-12-03 11:20:59 +0900

    两种中间件实现。

---
+++ Asp.NetCoreExperiment/Common/MiddlewareDemo/Startup.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace MiddlewareDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddTransient<FactoryActivatedMiddleware>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            //使用中间件
+            app.UseRequestCulture();
+
+            //使用工厂激活中间件
+            app.UseFactoryActivatedMiddleware();
+
+            app.UseMvc();
+        }
+    }
+    #region 第一种普通方式定义中间件
+    //定义中间件
+    public class RequestCultureMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestCultureMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var cultureQuery = context.Request.Query["culture"];
+            if (!string.IsNullOrWhiteSpace(cultureQuery))
+            {
+                var culture = new CultureInfo(cultureQuery);
+                CultureInfo.CurrentCulture = culture;
+                CultureInfo.CurrentUICulture = culture;
+            }
+            // Call the next delegate/middleware in the pipeline
+            await _next(context);
+        }
+    }
+    //扩展中间件
+    public static class RequestCultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseRequestCulture(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<RequestCultureMiddleware>();
+        }
+    }
+    #endregion
+
+    #region 第二种工厂方式定义中间件
+    public class FactoryActivatedMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var cultureQuery = context.Request.Query["culture"];
+            if (!string.IsNullOrWhiteSpace(cultureQuery))
+            {
+                var culture = new CultureInfo(cultureQuery);
+                CultureInfo.CurrentCulture = culture;
+                CultureInfo.CurrentUICulture = culture;
+            }
+            // Call the next delegate/middleware in the pipeline
+            await next(context);
+        }
+    }
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseFactoryActivatedMiddleware(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<FactoryActivatedMiddleware>();
+        }
+    }
+    #endregion
+}
