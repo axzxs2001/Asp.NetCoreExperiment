d9dc58dd0e771036d127b54cc923d65c36d9deed
桂素伟 <axzxs2001@163.com>
2018-12-04 10:23:42 +0900

    BowerDemo test

---
+++ Asp.NetCoreExperiment/ClientSide/BowerDemo/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BowerDemo
+{
+    public class Startup
+    {
+      
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseStaticFiles();
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
