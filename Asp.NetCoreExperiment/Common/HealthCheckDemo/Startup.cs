9897d56f0777f8ccf5fece6612752351de26315e
桂素伟 <axzxs2001@163.com>
2018-12-06 15:25:16 +0900

    自定义HealthCheck

---
+++ Asp.NetCoreExperiment/Common/HealthCheckDemo/Startup.cs
@@ -0,0 +1,186 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace HealthCheckDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddHealthChecks();
+            //services.AddHealthChecks().AddCheck<ExampleHealthCheck>("example_health_check");
+
+            //services.AddHealthChecks().AddCheck<ExampleHealthCheck>("example_health_check",failureStatus: HealthStatus.Degraded,tags: new[] { "example" });
+
+            //services.AddHealthChecks()
+            //    .AddCheck("Foo", () => HealthCheckResult.Healthy("Foo is OK!"), tags: new[] { "foo_tag" })
+            //    .AddCheck("Bar", () => HealthCheckResult.Unhealthy("Bar is unhealthy!"), tags: new[] { "bar_tag" })
+            //    .AddCheck("Baz", () => HealthCheckResult.Healthy("Baz is OK!"), tags: new[] { "baz_tag" });
+
+            //services.AddHealthChecks().AddSqlServer(Configuration["ConnectionStrings:DefaultConnection"]);
+            services.AddHealthChecks().AddMemoryHealthCheck("memory");
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // app.UseHealthChecks("/health",port:5000);
+
+            //app.UseHealthChecks("/health", new HealthCheckOptions()
+            //{
+            //    Predicate = (check) => check.Tags.Contains("foo_tag") || check.Tags.Contains("bar_tag")
+            //});
+
+            //app.UseHealthChecks("/health", new HealthCheckOptions()
+            //{
+            //    // WriteResponse is a delegate used to write the response.
+            //    ResponseWriter = WriteResponse
+            //});
+            //app.UseHealthChecks("/health");
+
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            app.UseHealthChecks("/health", new HealthCheckOptions()
+            {
+                // This custom writer formats the detailed status as JSON.
+                ResponseWriter = WriteResponse
+            });
+
+            app.UseMvc();
+        }
+        private static Task WriteResponse(HttpContext httpContext, HealthReport result)
+        {
+            httpContext.Response.ContentType = "application/json";
+
+            var json = new JObject(
+                new JProperty("status", result.Status.ToString()),
+                new JProperty("results", new JObject(result.Entries.Select(pair =>
+                    new JProperty(pair.Key, new JObject(
+                        new JProperty("status", pair.Value.Status.ToString()),
+                        new JProperty("description", pair.Value.Description),
+                        new JProperty("data", new JObject(pair.Value.Data.Select(
+                            p => new JProperty(p.Key, p.Value))))))))));
+            return httpContext.Response.WriteAsync(
+                json.ToString(Formatting.Indented));
+        }
+    }
+    public class MemoryHealthCheck : IHealthCheck
+    {
+        private readonly IOptionsMonitor<MemoryCheckOptions> _options;
+
+        public MemoryHealthCheck(IOptionsMonitor<MemoryCheckOptions> options)
+        {
+            _options = options;
+        }
+
+        public string Name => "memory_check";
+
+        public Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var options = _options.Get(context.Registration.Name);
+
+            // Include GC information in the reported diagnostics.
+            var allocated = GC.GetTotalMemory(forceFullCollection: false);
+            var data = new Dictionary<string, object>()
+        {
+            { "AllocatedBytes", allocated },
+            { "Gen0Collections", GC.CollectionCount(0) },
+            { "Gen1Collections", GC.CollectionCount(1) },
+            { "Gen2Collections", GC.CollectionCount(2) },
+        };
+
+            var status = (allocated < options.Threshold) ?
+                HealthStatus.Healthy : HealthStatus.Unhealthy;
+
+            return Task.FromResult(new HealthCheckResult(
+                status,
+                description: "Reports degraded status if allocated bytes " +
+                    $">= {options.Threshold} bytes.",
+                exception: null,
+                data: data));
+        }
+    }
+
+    public class MemoryCheckOptions
+    {
+        public long Threshold { get; set; } = 1024L * 1024L * 1024L;
+    }
+
+    public class ExampleHealthCheck : IHealthCheck
+    {
+        public ExampleHealthCheck()
+        {
+            // Use dependency injection (DI) to supply any required services to the
+            // health check.
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Execute health check logic here. This example sets a dummy
+            // variable to true.
+            var healthCheckResultHealthy = DateTime.Now.Second % 2 == 0;
+
+            if (healthCheckResultHealthy)
+            {
+                return Task.FromResult(
+                    HealthCheckResult.Healthy("The check indicates a healthy result."));
+            }
+
+            return Task.FromResult(
+                HealthCheckResult.Unhealthy("The check indicates an unhealthy result."));
+        }
+    }
+
+    public static class GCInfoHealthCheckBuilderExtensions
+    {
+        public static IHealthChecksBuilder AddMemoryHealthCheck(
+            this IHealthChecksBuilder builder,
+            string name,
+            HealthStatus? failureStatus = null,
+            IEnumerable<string> tags = null,
+            long? thresholdInBytes = null)
+        {
+            // Register a check of type GCInfo.
+            builder.AddCheck<MemoryHealthCheck>(
+                name, failureStatus ?? HealthStatus.Degraded, tags);
+
+            // Configure named options to pass the threshold into the check.
+            if (thresholdInBytes.HasValue)
+            {
+                builder.Services.Configure<MemoryCheckOptions>(name, options =>
+                {
+                    options.Threshold = thresholdInBytes.Value;
+                });
+            }
+
+            return builder;
+        }
+    }
+}
