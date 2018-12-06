a399acce8f41f91a5d0aaed0b0f926ea5c6a2064
桂素伟 <axzxs2001@163.com>
2018-12-06 16:11:28 +0900

    Polly HttpClient

---
+++ Asp.NetCoreExperiment/Common/InitiateHTTPClientDemo/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace InitiateHTTPClientDemo.Controllers
+{
+
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        readonly HttpClient _client;
+        public ValuesController(IHttpClientFactory clientFactory)
+        {
+            _client = clientFactory.CreateClient("polly");
+        }
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            _client.BaseAddress = new System.Uri("http://www.baidu.com");
+            var response = _client.GetAsync("").Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            return new string[] { "value1", content };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/Common/InitiateHTTPClientDemo/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Polly;
+
+namespace InitiateHTTPClientDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+      
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHttpClient("polly")
+       .AddTransientHttpErrorPolicy(p =>
+           p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)));
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
