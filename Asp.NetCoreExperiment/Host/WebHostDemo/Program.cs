e6c73501054ed69b771ca55a6505b6a1ef6650de
桂素伟 <axzxs2001@163.com>
2018-12-15 18:47:35 +0800

    web host

--- Asp.NetCoreExperiment/Host/WebHostDemo/Program.cs
+++ Asp.NetCoreExperiment/Host/WebHostDemo/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using System;
 
 namespace WebHostDemo
 {
@@ -20,6 +14,17 @@ namespace WebHostDemo
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            //调用 ConfigureKestrel 来重写 CreateDefaultBuilder 在配置 Kestrel 时建立的 30,000,000 字节默认 
+            .ConfigureKestrel((context, options) =>
+            {
+                options.Limits.MaxRequestBodySize = 20000000;
+            })
+            //捕获启动错误
+            .CaptureStartupErrors(true)
+            //确定是否应捕获详细错误
+            .UseSetting(WebHostDefaults.DetailedErrorsKey, "true")
+             //设置应用的环境。
+            .UseEnvironment(EnvironmentName.Development)
+            .UseStartup<Startup>();
     }
 }
