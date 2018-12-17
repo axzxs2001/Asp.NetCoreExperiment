ec95d4cd5787cc4cb912f545a521e61977aca17f
桂素伟 <axzxs2001@163.com>
2018-12-17 10:21:33 +0800

    host demo create

---
+++ Asp.NetCoreExperiment/Host/HostDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HostDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
