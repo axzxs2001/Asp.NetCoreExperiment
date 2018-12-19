ce1058b014f87781d2ee7022297be6653decf4b1
桂素伟 <axzxs2001@163.com>
2018-12-19 16:38:30 +0900

    通用主机测试。

--- Asp.NetCoreExperiment/Host/HostDemo/Program.cs
+++ Asp.NetCoreExperiment/Host/HostDemo/Program.cs
@@ -1,13 +1,13 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
+using Microsoft.Extensions.Logging;
 
 namespace HostDemo
 {
     class Program
     {
         static async System.Threading.Tasks.Task Main(string[] args)
-        {
+        {     
             var host = new HostBuilder()
                    .ConfigureServices((hostContext, services) =>
                    {
@@ -16,7 +16,14 @@ namespace HostDemo
                            option.ShutdownTimeout = System.TimeSpan.FromSeconds(20);
                        });
                    })
-                .Build();
+                   .ConfigureLogging((hostContext,configLogging) =>
+                   {                
+                       configLogging.AddConsole();
+                       configLogging.SetMinimumLevel(LogLevel.Debug);
+                   })
+                   .Build();
+
+
 
             await host.RunAsync();
         }
