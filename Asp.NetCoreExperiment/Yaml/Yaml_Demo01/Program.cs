21f6b839aeacf3b30b3f04a1fb74bb9fa2998154
桂素伟 <axzxs2001@163.com>
2019-01-24 09:58:36 +0900

    YmalDotNet测试

---
+++ Asp.NetCoreExperiment/Yaml/Yaml_Demo01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using YamlDotNet.Serialization;
+namespace Yaml_Demo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serializer = new Serializer();
+            var yaml = serializer.Serialize(new { id = 1, name = "abc",ttime=new { t=DateTime .Now} });
+            Console.WriteLine(yaml);
+        }
+    }
+}
