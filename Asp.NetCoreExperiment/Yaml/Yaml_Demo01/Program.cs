b79109ddc708589145ac149b1befb616bc37b1e0
桂素伟 <axzxs2001@163.com>
2019-01-25 09:37:26 +0900

    yaml test

--- Asp.NetCoreExperiment/Yaml/Yaml_Demo01/Program.cs
+++ Asp.NetCoreExperiment/Yaml/Yaml_Demo01/Program.cs
@@ -7,8 +7,23 @@ namespace Yaml_Demo01
         static void Main(string[] args)
         {
             var serializer = new Serializer();
-            var yaml = serializer.Serialize(new { id = 1, name = "abc",ttime=new { t=DateTime .Now} });
+            var yaml = serializer.Serialize(new ABC { ID = 1, Name = "abc", BCD = new BCD { Time = DateTime.Now } });
             Console.WriteLine(yaml);
+
+            var deserializer = new Deserializer();
+            var abc = deserializer.Deserialize<ABC>(yaml);
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(abc));
+            Console.ReadLine();
         }
+    }
+    class ABC
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public BCD BCD { get; set; }
+    }
+    class BCD
+    {
+        public DateTime Time { get; set; }
     }
 }
