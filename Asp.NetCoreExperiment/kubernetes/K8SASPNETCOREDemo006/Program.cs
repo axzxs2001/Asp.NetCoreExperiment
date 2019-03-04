4da3ec1c2724640cad6ecf575646ef127cdcd07c
桂素伟 <axzxs2001@163.com>
2019-03-04 16:59:47 +0900

    CronJob demo

---
+++ Asp.NetCoreExperiment/kubernetes/K8SASPNETCOREDemo006/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace K8SASPNETCOREDemo006
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine($"后台任务{DateTime.Now}");
+        }
+
+    }
+}
