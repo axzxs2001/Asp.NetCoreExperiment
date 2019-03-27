f0337c97dfc8e0e779ac4499b993907959a20d91
桂素伟 <axzxs2001@163.com>
2019-03-27 12:32:41 +0900

    环境部署配置

---
+++ Asp.NetCoreExperiment/ENVIRONMENT_Demo/ENVIRONMENT_Demo01/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ENVIRONMENT_Demo01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            Console.WriteLine($"-------------------Program中env={env}----------------");
+            var assemblyName = typeof(Startup).GetTypeInfo().Assembly.FullName;
+            switch (env)
+            {
+                //本地开发环境
+                case "Development":
+                    return WebHost.CreateDefaultBuilder(args)
+                        .UseUrls("http://*:5000")
+                        .UseStartup(assemblyName);
+                //正式环境
+                case "Production":
+                    return WebHost.CreateDefaultBuilder(args)
+                    .UseStartup(assemblyName)
+                    .UseKestrel(options =>
+                    {
+                        options.Listen(IPAddress.Any, 80);
+                        options.Listen(IPAddress.Any, 443, listenOptions =>
+                        {
+                            listenOptions.UseHttps("my.pfx", "password");
+                        });
+                    });
+                //测试环境
+                case "Staging":
+                    return WebHost.CreateDefaultBuilder(args)
+                      .UseUrls("http://*:5678")
+                      .UseStartup(assemblyName);
+                //默认
+                default:
+                    return WebHost.CreateDefaultBuilder(args)
+                           .UseStartup("");
+            }
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/ENVIRONMENT_Demo/ENVIRONMENT_Demo01/Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ENVIRONMENT_Demo01
+{
+    /*
+     * 如果Startup中的代码不一样，可以分别写Starup
+     */
+
+    //开发的Startup
+    public class StartupDevelopment
+    {
+        public StartupDevelopment(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var apptitle = Configuration.GetSection("apptitle").Value;
+            Console.WriteLine($"*************  StartupDevelopment中的apptitle={apptitle}");
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvc();
+        }
+    }
+    //测试的Startup
+    public class StartupStaging
+    {
+        public StartupStaging(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var apptitle = Configuration.GetSection("apptitle").Value;
+            Console.WriteLine($"**********  StartupStaging中的apptitle={apptitle}");
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvc();
+        }
+    }
+    //正式的Startup
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+  
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var apptitle = Configuration.GetSection("apptitle").Value;
+            Console.WriteLine($"**********  Startup中的apptitle={apptitle}");
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            } 
+            app.UseMvc();
+        }
+    }
+}
