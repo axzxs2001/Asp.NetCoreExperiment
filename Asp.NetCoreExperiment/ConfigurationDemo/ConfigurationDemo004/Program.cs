064411e88635e40485896c3048b2aaa9ba597e5f
桂素伟 <axzxs2001@163.com>
2019-03-28 11:41:41 +0900

    配置文件

---
+++ Asp.NetCoreExperiment/ConfigurationDemo/ConfigurationDemo004/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace ConfigurationDemo004
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+             {
+                 config.SetBasePath(Directory.GetCurrentDirectory());       
+                 config.AddJsonFile("abc.json", optional: false, reloadOnChange: false);               
+             })
+            .UseStartup<Startup>();
+    }
+}
---
+++ Asp.NetCoreExperiment/ConfigurationDemo/ConfigurationDemo004/Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ConfigurationDemo004
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var settings = Configuration.GetSection("settings");
+            var abc = Configuration.GetSection("abc");
+
+            var sets = new AppSetting();
+            Configuration.GetSection("settings").Bind(sets);
+            var abcs = new ABC();
+            Configuration.GetSection("abc").Bind(abcs);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+    public class AppSetting
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+
+    }
+
+    public class ABC
+    {
+        public string ABC1 { get; set; }
+        public string ABC2 { get; set; }
+    }
+}
