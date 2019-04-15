493c1c33bc9ba4b065d92bdc5009ceba07f3ec7a
桂素伟 <axzxs2001@163.com>
2019-04-15 17:16:50 +0900

    jis编码

---
+++ Asp.NetCoreExperiment/Common/Shift_JISDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.IO;
+namespace Shift_JISDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var ens = Encoding.GetEncodings();
+            var jis = System.Text.Encoding.GetEncoding("shift_jis");
+            var bytes = jis.GetBytes("こんにちは");
+            File.WriteAllText(Directory.GetCurrentDirectory() + "/a.txt", "こんにちは", jis);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
