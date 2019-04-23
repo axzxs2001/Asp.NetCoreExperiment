ae40b5a4cb2cd09d8b1b99b8605456751af034fa
桂素伟 <axzxs2001@163.com>
2019-04-23 14:29:08 +0900

    Blazor Demo

---
+++ Asp.NetCoreExperiment/Blazor/BlazorDemo01/Data/WeatherForecastService.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDemo01.Data
+{
+    public class WeatherForecastService
+    {
+        private static string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
+        {
+            var rng = new Random();
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            }).ToArray());
+        }
+
+        public Task SavaAsync(string userName, bool sex, string work)
+        {
+            Console.WriteLine($"--Name:{userName},sex:{sex},work:{work}");
+            return Task.CompletedTask;
+        }
+    }
+}
