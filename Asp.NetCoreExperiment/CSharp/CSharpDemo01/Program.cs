d5a4a8ad63a1a25b0a13dd60d880f1d1ff64d6f0
桂素伟 <axzxs2001@163.com>
2019-05-13 11:39:02 +0900

    语法Demo

---
+++ Asp.NetCoreExperiment/CSharp/CSharpDemo01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using B;
+using CSharpDemo01;
+using CSharpDemo01_Lib;
+
+namespace CSharpDemo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(nameof(System.String));
+            int j = 5;
+            Console.WriteLine(nameof(j));
+            List<string> names = new List<string>();
+            Console.WriteLine(nameof(names));
+        }
+    }
+
+    class TestA
+    {
+        private protected void A()
+        {
+            Console.WriteLine("TestA.A");
+        }
+        public void B()
+        {
+            A();
+        }
+    }
+}
+namespace B
+{
+    class TestB : TestA
+    {
+        public void BB()
+        {
+            A();
+        }
+    }
+    class TestB_Lib : TestA_Lib
+    {
+        public void BB()
+        {
+            //A();
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/CSharp/CSharpDemo01_Lib/TestA_Lib.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CSharpDemo01_Lib
+{
+    public class TestA_Lib
+    {
+        private protected void A()
+        {
+            Console.WriteLine("TestA.A");
+        }
+        public void B()
+        {
+            A();
+        }
+    }
+}
