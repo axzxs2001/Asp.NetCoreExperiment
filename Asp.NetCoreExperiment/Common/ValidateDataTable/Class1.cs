de524ccc44d499c3c284f469e146f76cfb083216
桂素伟 <axzxs2001@163.com>
2019-05-21 17:27:33 +0900

    ValidateDataTable案例

---
+++ Asp.NetCoreExperiment/Common/ValidateDataTable/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+
+namespace ValidateTableLib
+{
+    /// <summary>
+    /// 一个验证Table，设置每列的正则，当调用Validate时，给附加验证后的错误信息
+    /// </summary>
+    public class ValidateDataTable : DataTable
+    {
+
+        /// <summary>
+        /// 开始验证
+        /// </summary>
+        /// <param name="ValidateCollections">验证列集合，每条验证用ColumnName,和正则集合</param>
+        public (bool result, int error) Validate(params (string ColumnName, (string RegularExpression, string Error)[] RegularExpressions)[] ValidateCollections)
+        {
+            var result = true;
+            var errorCount = 0;
+            if (ValidateCollections != null && ValidateCollections.Length > 0)
+            {
+                foreach (var validateColumn in ValidateCollections)
+                {
+                    foreach (DataRow row in this.Rows)
+                    {
+                        var value = row[validateColumn.ColumnName]?.ToString();
+                        var errors = new StringBuilder();
+                        var sn = 1;
+                        foreach (var regularItem in validateColumn.RegularExpressions)
+                        {
+                            var regex = new Regex(regularItem.RegularExpression);
+                            if (!regex.IsMatch(value))
+                            {
+                                errors.AppendLine($"{sn++}、{regularItem.Error}");
+                                errorCount++;
+                            }
+                        }
+                        result = false;
+                        row.SetColumnError(validateColumn.ColumnName, errors.ToString());
+                    }
+                }
+            }
+            return (result, errorCount);
+        }
+
+    }
+}
