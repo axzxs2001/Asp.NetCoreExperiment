3f0f941cbbdacad4042041b25b9db9755cdb8b12
桂素伟 <axzxs2001@163.com>
2019-06-02 20:51:54 +0800

    加载Excel模版，并导出excel文件

---
+++ Asp.NetCoreExperiment/Excel/LoadExcelTemplate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using OfficeOpenXml;
+namespace LoadExcelTemplate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var file = Directory.GetCurrentDirectory() + "\\Demo.xlsx";
+            using (var source = System.IO.File.OpenRead(file))
+            {
+                using (var excel = new ExcelPackage(source))
+                {
+
+                    excel.Workbook.Worksheets[1].Cells[3, 1].Value = 123;
+                    excel.Workbook.Worksheets[1].Cells[3, 2].Value = "张三丰";
+                    excel.Workbook.Worksheets[1].Cells[3, 3].Value = 29;
+                    excel.Workbook.Worksheets[1].Cells[3, 4].Value = true;
+                    var newfile = Directory.GetCurrentDirectory() + "\\new.xlsx";
+                    var newfileinfo = new FileInfo(newfile);
+                    excel.SaveAs(newfileinfo);
+                }
+            }
+        }
+    }
+}
