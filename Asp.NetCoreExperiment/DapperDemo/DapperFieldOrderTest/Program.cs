6618662efd22e67282847183d9cdcf2a3e05c1a0
桂素伟 <axzxs2001@163.com>
2019-06-03 14:44:22 +0800

    dapper，select 字段顺诹与数据序顺一致

---
+++ Asp.NetCoreExperiment/DapperDemo/DapperFieldOrderTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+using System.Linq;
+
+namespace DapperFieldOrderTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var con = new SqlConnection("server=.;database=testdb;uid=sa;pwd=sa;"))
+            {
+                var sql = "select   name,age,page,id  from test1";
+                var ents = con.Query<dynamic>(sql);
+                Console.WriteLine($"一共数据：{ents.Count()}");
+                //字段顺序与查询顺序一致
+                foreach(var ent in ents)
+                {                
+                    foreach(var col in ent)
+                    {
+                        Console.Write(col.Key+":"+col.Value+",");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
