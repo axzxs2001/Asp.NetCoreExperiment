9a5615688ee2e0be4f9cef80e3562cc230c040bf
桂素伟 <axzxs2001@163.com>
2019-06-20 09:56:48 +0800

    码码测试

---
+++ Asp.NetCoreExperiment/Common/EncodingDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncodingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "ズフーェ0ｱｲｳｴｵ";
+            Console.WriteLine("UTF8:" + System.Text.Encoding.UTF8.GetByteCount(s));
+            Console.WriteLine("UTF7:" + System.Text.Encoding.UTF7.GetByteCount(s));
+            Console.WriteLine("UTF32:" + System.Text.Encoding.UTF32.GetByteCount(s));
+            Console.WriteLine("Default:" + System.Text.Encoding.Default.GetByteCount(s));
+            Console.WriteLine("Unicode:" + System.Text.Encoding.Unicode.GetByteCount(s));
+            Console.WriteLine("ASCII:" + System.Text.Encoding.ASCII.GetByteCount(s));
+            var encoding = Encoding.GetEncoding(932);
+            Console.WriteLine("shift-jis:" + encoding.GetByteCount(s));
+
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/Common/HalfFullWidthDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace HalfFullWidthDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("UTF8:"+System.Text.Encoding.UTF8.GetByteCount("ＡA你"));
+            Console.WriteLine("UTF7:" + System.Text.Encoding.UTF7.GetByteCount("ＡA你"));
+            Console.WriteLine("UTF32:" + System.Text.Encoding.UTF32.GetByteCount("ＡA你"));
+            Console.WriteLine("Default:" + System.Text.Encoding.Default.GetByteCount("ＡA你"));
+            Console.WriteLine("Unicode:" + System.Text.Encoding.Unicode.GetByteCount("ＡA你"));
+            Console.WriteLine("ASCII:" + System.Text.Encoding.ASCII.GetByteCount("ＡA你"));
+
+            var end = Encoding.GetEncoding(50220); 
+            Console.WriteLine("shift-jis:" + end.GetByteCount("ＡA你"));
+
+            var regex = new Regex(@"^\s*\S((.){0,4}\S)?\s*$");
+            Console.WriteLine(regex.IsMatch("ＡAAA"));
+        }
+    }
+}
