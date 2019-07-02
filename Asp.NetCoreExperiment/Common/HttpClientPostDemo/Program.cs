f811dc9b775ea064d28d62cec05bff03747f7d8e
桂素伟 <axzxs2001@163.com>
2019-07-02 17:39:47 +0900

    测试HttpClient Post参数

---
+++ Asp.NetCoreExperiment/Common/HttpClientPostDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace HttpClientPostDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var request = "http://localhost:5000/login";
+            var values = new List<KeyValuePair<string, string>>();
+            values.Add(new KeyValuePair<string, string>("userName","gsw"));
+            values.Add(new KeyValuePair<string, string>("password","gsw"));
+            values.Add(new KeyValuePair<string, string>("returnUrl", "/"));
+            var content = new FormUrlEncodedContent(values);           
+            var _client = new HttpClient();
+           
+            var response = _client.PostAsync(request, content).Result;
+            response.Headers.TryGetValues("Set-Cookie", out IEnumerable<string> list);
+
+            Console.WriteLine(list);
+        }
+    }
+}
