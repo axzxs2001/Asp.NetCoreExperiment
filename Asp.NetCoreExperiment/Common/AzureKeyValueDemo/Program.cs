76a2341e97387d88b177f2638bc36bbcc2cbe3e9
桂素伟 <axzxs2001@163.com>
2019-07-10 18:37:46 +0900

    azure key value测试

---
+++ Asp.NetCoreExperiment/Common/AzureKeyValueDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.KeyVault; // Install-Package Microsoft.Azure.KeyVault
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.IdentityModel.Clients.ActiveDirectory; // Install-Package Microsoft.IdentityModel.Clients.ActiveDirectory
+
+namespace KeyVaultSample
+{
+    class Program
+    {
+        public static void Main()
+        {
+            OnGetAsync().Wait();
+        }
+        public static async Task OnGetAsync()
+        {
+            var Message = "Your application description page.";
+            int retries = 0;
+            bool retry = false;
+            try
+            {
+                /* The next four lines of code show you how to use AppAuthentication library to fetch secrets from your key vault */
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                var secret = await keyVaultClient.GetSecretAsync("https://gswsec.vault.azure.net/secrets/pgpwd")
+                        .ConfigureAwait(false);
+                Message = secret.Value;
+            }
+            /* If you have throttling errors see this tutorial https://docs.microsoft.com/azure/key-vault/tutorial-net-create-vault-azure-web-app */
+            /// <exception cref="KeyVaultErrorException">
+            /// Thrown when the operation returned an invalid status code
+            /// </exception>
+            catch (KeyVaultErrorException keyVaultException)
+            {
+                Message = keyVaultException.Message;
+            }
+            Console.WriteLine(Message);
+        }
+
+
+        // This method implements exponential backoff if there are 429 errors from Azure Key Vault
+        private static long getWaitTime(int retryCount)
+        {
+            long waitTime = ((long)Math.Pow(2, retryCount) * 100L);
+            return waitTime;
+        }
+
+        // This method fetches a token from Azure Active Directory, which can then be provided to Azure Key Vault to authenticate
+        public async Task<string> GetAccessTokenAsync()
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            string accessToken = await azureServiceTokenProvider.GetAccessTokenAsync("https://vault.azure.net");
+            return accessToken;
+        }
+    }
+
+
+}
