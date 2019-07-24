e675df916fdbbe58945918272e9f4317f58c9caa
桂素伟 <axzxs2001@163.com>
2019-07-24 19:38:29 +0900

    修改starup pg实现路库查询

--- Asp.NetCoreExperiment/PostgreSqlDemo/PostgresqlDemo02/Program.cs
+++ Asp.NetCoreExperiment/PostgreSqlDemo/PostgresqlDemo02/Program.cs
@@ -8,6 +8,12 @@ namespace PostgresqlDemo02
     {
         static void Main(string[] args)
         {
+
+            CrossDataBaseQuery();
+            return;
+
+
+
             var connString = "Server=127.0.0.1;Port=5432;UserId=postgres;Password=postgres2018;Database=TestDB;";
             using (var conn = new NpgsqlConnection(connString))
             {
@@ -24,11 +30,32 @@ namespace PostgresqlDemo02
                     s = paySetting.Setting;
                 }
                 //插入
-                conn.Execute("insert into paysettings(paytype,setting) values(@paytype,cast(@setting as jsonb))", new { paytype = "AliPay", setting = s});
+                conn.Execute("insert into paysettings(paytype,setting) values(@paytype,cast(@setting as jsonb))", new { paytype = "AliPay", setting = s });
             }
             Console.WriteLine("Hello World!");
         }
+        /// <summary>
+        /// postgre实现跨库查询
+        /// </summary>
+        static void CrossDataBaseQuery()
+        {
+            var connString = "Server=127.0.0.1;Port=5432;UserId=postgres;Password=postgres2018;database=TestDB";
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                //查询
+                var sql = @"select b.id,a.pid,a.name,b.name as bname from dblink('dbname=TestDB1 password=postgres2018 user=postgres','select id,pid,name from testdb')
+as a(id integer,pid integer,name  character varying(128)) join testdb b on a.pid=b.id";
+                foreach (var s in conn.Query<dynamic>(sql))
+                {
+                    Console.WriteLine($"pid:{s.pid} id:{s.id}  name:{s.name}  pname:{s.bname}");
+                }
+            }
+
+        }
     }
+
+
+
     public class PaySetting
     {
         public int ID { get; set; }
