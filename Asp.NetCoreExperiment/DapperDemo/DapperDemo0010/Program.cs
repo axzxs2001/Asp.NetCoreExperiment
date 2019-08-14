c24ed44aa8c3570571212bd2d36609ce0367a23d
桂素伟 <axzxs2001@163.com>
2019-08-14 14:53:47 +0900

    判断相等

--- Asp.NetCoreExperiment/DapperDemo/DapperDemo0010/Program.cs
+++ Asp.NetCoreExperiment/DapperDemo/DapperDemo0010/Program.cs
@@ -2,6 +2,7 @@
 using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DapperDemo0010
 {
@@ -10,19 +11,41 @@ namespace DapperDemo0010
         static void Main(string[] args)
         {
             var connString = "Server=127.0.0.1;Port=5432;UserId=postgres;Password=postgres2018;Database=TestDB;";
-            var sql = @"select content from jsontb";
+            var sql = @"select id,content from jsontb";
             using (var db = new NpgsqlConnection(connString))
             {
-                var list = db.Query<string>(sql);
-                var abcs = new List<dynamic>();
-                foreach (var item in list)
-                {
-                    abcs.Add(Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(item));
-                }
+                var a = db.Query<AA>(sql).First();
+                var b = db.Query<AA>(sql).First();
+                var c = a.Equals(b);
+                Console.WriteLine(c);
+                Console.WriteLine(a==b);
+                //var abcs = new List<dynamic>();
+                //foreach (var item in list)
+                //{
+                //    abcs.Add(Newtonsoft.Json.JsonConvert.DeserializeObject<dynamic>(item));
+                //}
             }
         }
     }
+    public class AA
+    {
+        public int ID { get; set; }
+        public string Content { get; set; }
+        public bool Equals(AA aa)
+        {
+            return (ID == aa.ID) && (Content == aa.Content);
+        }
 
+        public  static bool operator == (AA a,AA b)
+        {
+            return (a.ID ==b.ID) && (a.Content ==b.Content);
+        }
+        public static bool operator !=(AA a, AA b)
+        {
+            return (a.ID != b.ID) || (a.Content != b.Content);
+        }
+
+    }
     class ABC
     {
         public int ID { get; set; }
