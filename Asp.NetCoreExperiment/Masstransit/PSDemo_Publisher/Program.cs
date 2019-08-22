c203c1bfe3985f67335c7581a63815fb2acddab3
桂素伟 <axzxs2001@163.com>
2019-08-22 15:50:06 +0900

    MassTransit日志和docker中的RabbitMQ

--- Asp.NetCoreExperiment/Masstransit/PSDemo_Publisher/Program.cs
+++ Asp.NetCoreExperiment/Masstransit/PSDemo_Publisher/Program.cs
@@ -1,15 +1,21 @@
 ﻿using MassTransit;
+using MassTransit.Logging;
+using MassTransit.NLogIntegration;
 using System;
 
 namespace PSDemo_Publisher
 {
     class Program
     {
+        //启动Docker 中的RabbitMQ
+        //docker run -d --name rabbitmq --publish 5671:5671  --publish 5672:5672 --publish 4369:4369 --publish 25672:25672 --publish 15671:15671 --publish 15672:15672 rabbitmq:management
+        //docker rabbitmq启动UI http://localhost:15672  用户名：guest   密码：guest
         static void Main(string[] args)
         {
            var bus= Bus.Factory.CreateUsingRabbitMq(cfg =>
-            {
-                var host = cfg.Host(new Uri("rabbitmq://localhost/"), hst =>
+           {
+               cfg.UseNLog();
+               var host = cfg.Host(new Uri("rabbitmq://localhost/"), hst =>
                 {
                     hst.Username("gsw");
                     hst.Password("gsw790622");
@@ -25,6 +31,11 @@ namespace PSDemo_Publisher
                 }
 
                 bus.Publish(new PSDemo_Entity.Entity() { Name="张三", Time = DateTime.Now });
+
+                var log = Logger.Get("log");
+                log.Info("publish gsw");
+                
+
                 bus.Publish(new PSDemo_Entity.ChildEntity() { Name = "李四", Time = DateTime.Now,Age=22 });
             }
             while (true);        
