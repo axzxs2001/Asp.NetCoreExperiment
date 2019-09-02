3542dd0357c00f2f453761750a78116cd50e1582
桂素伟 <axzxs2001@163.com>
2019-09-02 15:01:20 +0900

    Rosly编译三方类型案例

--- Asp.NetCoreExperiment/Roslyn/RoslynDemo001/Program.cs
+++ Asp.NetCoreExperiment/Roslyn/RoslynDemo001/Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.IO;
 using System.Reflection;
-using System.Web.Http.Dependencies;
+using System.Runtime;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-
+using Microsoft.Dnx.Compilation;
+using Newtonsoft.Json;
 
 namespace RoslynDemo001
 {
@@ -30,20 +32,24 @@ namespace RoslynDynamicGenerate
     public class DynamicGenerateClass
     {
         public string Generate()
-        {
-          return DateTime.Now.AddDays(-1).ToString(""yyyyMMdd"");
+        {    
+          return Newtonsoft.Json.JsonConvert.SerializeObject(new TestClass{A=""abcde"",B=1.234d});
         }       
+    }
+    public class TestClass
+    {
+        public string A { get; set; }
+        public double B { get; set; }
     }
 }
 ";
 
-            var systemReference = MetadataReference.CreateFromFile(typeof(object).Assembly.Location);
             var syntaxTree = CSharpSyntaxTree.ParseText(sourceCodeText, new CSharpParseOptions(LanguageVersion.Latest)); // 获取代码分析得到的语法树
             var assemblyName = $"RoslynDynamicGenerate";
             // 创建编译任务
             var compilation = CSharpCompilation.Create(assemblyName) //指定程序集名称
                 .WithOptions(new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary))//输出为 dll 程序集
-                .AddReferences(systemReference) //添加程序集引用
+                .AddReferences(GetMetadataReference(typeof(JsonConvert), typeof(object), typeof(AssemblyTargetedPatchBandAttribute))) //添加程序集引用   
                 .AddSyntaxTrees(syntaxTree) // 添加上面代码分析得到的语法树     
                 ;
             var memory = new MemoryStream();
@@ -65,8 +71,34 @@ namespace RoslynDynamicGenerate
             }
             else
             {
+                foreach (var diagnositic in compilationResult.Diagnostics)
+                {
+                    Console.WriteLine(diagnositic);
+                }
                 throw new ApplicationException($"下面C#语句有语法错误{sourceCodeText}");
             }
+        }
+        /// <summary>
+        /// 从类型获取原数据引用
+        /// </summary>
+        /// <param name="types">类型集合</param>
+        /// <returns></returns>
+        static List<MetadataReference> GetMetadataReference(params Type[] types)
+        {
+            var list = new List<MetadataReference>();
+            foreach (var type in types)
+            {
+                var metadateRef = MetadataReference.CreateFromFile(type.Assembly.Location);
+                list.Add(metadateRef);
+                foreach (var assembly in type.Assembly.GetReferencedAssemblies())
+                {
+                    //Console.WriteLine("-------------");
+                    //Console.WriteLine(assembly.FullName);
+                    list.Add(MetadataReference.CreateFromFile(Assembly.Load(assembly).Location));
+                    //Console.WriteLine("-------------");
+                }
+            }
+            return list;
         }
 
 
