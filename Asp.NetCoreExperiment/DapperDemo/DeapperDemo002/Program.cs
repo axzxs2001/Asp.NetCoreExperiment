500416530b366afab44b689735b27f774143afe7
桂素伟 <axzxs2001@163.com>
2019-09-03 10:05:47 +0900

    时区格式下的UTC

--- Asp.NetCoreExperiment/DapperDemo/DeapperDemo002/Program.cs
+++ Asp.NetCoreExperiment/DapperDemo/DeapperDemo002/Program.cs
@@ -1,11 +1,48 @@
-﻿using Npgsql;
+﻿using Dapper;
+using Npgsql;
 using System;
+using System.Globalization;
+using System.Text;
+using System.Threading;
 
 namespace DeapperDemo002
 {
     class Program
     {
         static void Main(string[] args)
+        {
+            var currentCulture = Thread.CurrentThread.CurrentCulture;
+            
+            Test2();
+
+
+        }
+
+        static void Test2()
+        {
+            var connString = "Server=127.0.0.1;Port=5432;UserId=postgres;Password=postgres2018;Database=abc;";
+            using (var conn = new NpgsqlConnection(connString))
+            {
+
+                #region 错误做法
+                //var sql = @"select * from ""T_Test"" where  date(createtime)=date(@date)";
+                //var date = DateTimeOffset.Parse("2019-09-03 09:27:00 +09:00");
+                //var list = conn.Query<dynamic>(sql, new { date });
+                #endregion
+
+                #region 正确做法
+                var sql = @"select * from ""T_Test"" where  createtime>=@beginTime and createtime<=@endtime";
+                var beginTime = DateTimeOffset.Parse("2019-09-03 00:00:00");
+                var endTime = DateTimeOffset.Parse("2019-09-03 23:59:59.9999994");
+                var list = conn.Query<dynamic>(sql, new { beginTime, endTime });
+                #endregion
+                foreach (var item in list)
+                {
+                    Console.WriteLine($"ID:{item.id}  Name:{item.name}  Time:{item.createtime} ");
+                }
+            }
+        }
+        static void Test1()
         {
             //var file = System.IO.Directory.GetCurrentDirectory() + "/sql.txt";
 
@@ -31,9 +68,6 @@ namespace DeapperDemo002
                     while (reader.Read())
                         Console.WriteLine(reader.GetString(0));
             }
-
-
-
         }
     }
 }
