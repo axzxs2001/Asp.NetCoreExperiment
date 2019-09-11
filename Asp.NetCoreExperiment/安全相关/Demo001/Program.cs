f3ea7bbb07debc71aafca3a1da2d37b67ad896d9
桂素伟 <axzxs2001@163.com>
2019-09-11 10:58:41 +0900

    增加安全相关配置信息

---
+++ Asp.NetCoreExperiment/安全相关/Demo001/Program.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Demo001
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.ConfigureKestrel(opt =>
+                    {
+                        //不显示Host相关信息
+                        opt.AddServerHeader = false;
+                    });
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
