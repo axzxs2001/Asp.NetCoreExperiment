11429ce64ab52e62ed6b6b506fc61ba3dc10fe81
桂素伟 <axzxs2001@163.com>
2019-09-27 16:31:34 +0900

    pgp解密

---
+++ Asp.NetCoreExperiment/Common/PgpDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using Cinchoo.PGP;
+using PgpCore;
+using System;
+
+namespace PgpDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var txt = @"C:\MyFile\NETSTARS\pgp\data.txt";
+            var pgpf = @"C:\MyFile\NETSTARS\pgp\20190927.pgp";
+            var key = @"C:\MyFile\NETSTARS\pgp\FilesKey\secret-key.asc";
+            var pubkey = @"C:\MyFile\NETSTARS\pgp\FilesKey\GLNgpgPub";
+            var password = "netstars";
+            //using (ChoPGPEncryptDecrypt pgp = new ChoPGPEncryptDecrypt())
+            //{
+            //    pgp.DecryptFileAndVerify(pgpf,txt, pubkey, key,password);
+            //}
+
+            using (var pgp2 = new PGP())
+            {
+                pgp2.DecryptFile(pgpf, txt, key, password);
+                Console.WriteLine("Hello World!");
+            }
+        }
+    }
+}
