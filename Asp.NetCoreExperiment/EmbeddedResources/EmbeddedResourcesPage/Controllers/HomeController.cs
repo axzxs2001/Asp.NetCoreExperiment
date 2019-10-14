af0771f9570d1c888376ab3f19b5ce579de5006f
桂素伟 <axzxs2001@163.com>
2019-10-14 20:01:20 +0900

    .net core 3.0嵌入式资源文件

---
+++ Asp.NetCoreExperiment/EmbeddedResources/EmbeddedResourcesPage/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace EmbeddedResourcesPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet("page")]
+        public IActionResult Index()
+        {
+            _logger.LogInformation("----------------home/index");
+            return View();
+        }
+
+    }
+}
