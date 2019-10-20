1d5c75c0f59bb9d1ace64984045177afd5856757
桂素伟 <axzxs2001@163.com>
2019-10-20 20:13:34 +0900

    HttpClient Demo

---
+++ Asp.NetCoreExperiment/HttpClientDemo/HttpClientDemo01/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+
+namespace HttpClientDemo01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "http://localhost:4800";
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(url);
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "goodses");
+            {
+                request.Content = new StringContent(JsonConvert.SerializeObject(new QueryGoods { GoodsName = "aaaaa", Limit = 10, Offset = 0 }), Encoding.UTF8, "application/json");
+                //request.Content = new StringContent(JsonConvert.SerializeObject(new Person { Name = "桂素伟", Age = 99 }), Encoding.UTF8, "application/json");
+                //var values = new List<KeyValuePair<string, string>>();
+                //values.Add(new KeyValuePair<string, string>("goodsname", goodsName));
+                //values.Add(new KeyValuePair<string, string>("limit", _pagecount.ToString()));
+                //values.Add(new KeyValuePair<string, string>("offset", (pageIndex * _pagecount).ToString()));
+                //request.Content = new FormUrlEncodedContent(values);
+                var response = client.SendAsync(request).Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine(content);
+
+                }
+                else
+                {
+                    var content = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine(content);
+                }
+
+
+            }
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+    public class QueryGoods
+    {
+        public int Limit { get; set; }
+
+        public int Offset { get; set; }
+        /// <summary>
+        /// 商品名称
+        /// </summary>
+        public string GoodsName { get; set; }
+    }
+}
---
+++ Asp.NetCoreExperiment/HttpClientDemo/ServerDemo/Controllers/PersonController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ServerDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(ILogger<PersonController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/person")]
+        public IActionResult GetPerson(Person person)
+        {
+            _logger.LogInformation(System.Text.Json.JsonSerializer.Serialize(person));
+            return new JsonResult(person);
+        }
+    }
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+ 
+}
