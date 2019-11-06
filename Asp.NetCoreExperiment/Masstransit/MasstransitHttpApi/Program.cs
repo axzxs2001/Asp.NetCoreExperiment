788d2212440324d8ee4c87f11fe2b6750bbc70ea
桂素伟 <axzxs2001@163.com>
2019-11-06 15:35:52 +0900

    访问RabbitMQ http api，访问队列数

---
+++ Asp.NetCoreExperiment/Masstransit/MasstransitHttpApi/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace MasstransitHttpApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, "http://guest:guest@localhost:15672/api/queues");
+                var byteArray = Encoding.ASCII.GetBytes("guest:guest");
+                request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+                var result = client.SendAsync(request).Result;
+                var list = System.Text.Json.JsonSerializer.Deserialize<dynamic>(result.Content.ReadAsStringAsync().Result);
+
+                Console.WriteLine("------------");
+                for (var i = 0; i < list.GetArrayLength(); i++)
+                {
+                    System.Text.Json.JsonElement item = list[i];
+                    Console.WriteLine(item.GetProperty("backing_queue_status").GetProperty("len").GetInt32());
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
