b96c8fb9eaa89535050ba9f16c1e0bfc9a29f27f
桂素伟 <axzxs2001@163.com>
2019-11-13 20:30:54 +0800

    移除无用代码。

--- Asp.NetCoreExperiment/Common/LogEntity/StarPayLogger.cs
+++ Asp.NetCoreExperiment/Common/LogEntity/StarPayLogger.cs
@@ -29,35 +29,6 @@ namespace LogEntity
         }
     }
 
-
-    public class StarPayLogger<T> : ILogger<T>
-    {
-
-        public IDisposable BeginScope<TState>(TState state)
-        {
-            return null;
-        }
-
-        public bool IsEnabled(Microsoft.Extensions.Logging.LogLevel logLevel)
-        {
-            return true;
-        }
-
-        public void Log<TState>(Microsoft.Extensions.Logging.LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
-        {
-            var log = new Log { Level = logLevel.ToString(), Message = state.ToString() };
-            var logstr = JsonSerializer.Serialize(log);
-
-
-            var v = state as IReadOnlyList<KeyValuePair<string, object>>;
-            var key = v[0].Key;
-            var value = v[0].Value;
-
-            var message = formatter((TState)v, exception);
-
-        }
-    }
-
     public class StarPayProvider : ILoggerProvider
     {
 
@@ -68,6 +39,7 @@ namespace LogEntity
         public ILogger CreateLogger(string categoryName)
         {
             return _loggers.GetOrAdd(categoryName, name => new StarPayLogger());
+            //return _loggers.GetOrAdd(categoryName, name => new StarPayLogger());
         }
 
         public void Dispose()
