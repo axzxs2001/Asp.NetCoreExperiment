8cbfd4838bd4a84dd4cb521e8d985236f8149b00
桂素伟 <axzxs2001@163.com>
2019-11-23 11:14:26 +0900

    grpc测试

---
+++ Asp.NetCoreExperiment/GRPC/GRPCDemo01Test/Program.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace GRPCDemo01Test
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");      
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                              new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
