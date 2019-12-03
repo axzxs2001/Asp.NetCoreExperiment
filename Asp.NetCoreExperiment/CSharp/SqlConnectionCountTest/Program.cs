48117e08ae32dc38dacf970d941d0e0e87ddcfeb
素伟 桂 <guisuwei@outlook.com>
2019-12-03 15:59:46 +0900

    测试连接

---
+++ Asp.NetCoreExperiment/CSharp/SqlConnectionCountTest/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SqlConnectionCountTest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            for (var i = 0; i < 1000000; i++)
+            {
+                new Thread(Client).Start();
+               // F();
+            }
+        }
+        static void Client()
+        {
+            var client = new HttpClient();
+            var response = client.GetAsync("http://localhost:5000/test").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var content = response.Content.ReadAsStringAsync().Result;         
+                Console.WriteLine("返回值：" + content);
+            }
+            else
+            {
+                var content = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine("返回值：" + content);
+            }
+        }
+        static void F()
+        {
+            try
+            {
+                using (var conn = new SqlConnection("Data Source=.;Initial Catalog=StarPay;Persist Security Info=True;User ID=sa;Password=sa;Max Pool Size=500"))
+                {
+                    conn.Open();
+                    var sql = "select 1;WAITFOR DELAY '00:00:03';";
+                    var cmd = new SqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch(Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+        }
+
+    }
+}
---
+++ Asp.NetCoreExperiment/CSharp/WebSqlConnectionTest/Controllers/TestController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebSqlConnectionTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+      
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("/test")]
+        public string Test()
+        {
+            try
+            {
+                using (var conn = new SqlConnection("Data Source=.;Initial Catalog=StarPay;Persist Security Info=True;User ID=sa;Password=sa;Max Pool Size=100"))
+                {
+                    conn.Open();
+                    var sql = "select 1;--WAITFOR DELAY '00:00:03';";
+                    var cmd = new SqlCommand(sql, conn);
+                    cmd.ExecuteNonQuery();
+                    return "SUCCESS";
+                }
+            }
+            catch (Exception exc)
+            {               
+                return exc.Message;
+            }
+        }
+    }
+}
