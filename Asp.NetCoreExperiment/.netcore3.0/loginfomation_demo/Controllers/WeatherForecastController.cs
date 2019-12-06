2ae4c5361290a09f41bb2b044c266d57e5d91cfe
素伟 桂 <guisuwei@outlook.com>
2019-12-06 15:08:36 +0900

    测试loginfomartion特性。

---
+++ Asp.NetCoreExperiment/.netcore3.0/loginfomation_demo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace loginfomation_demo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {        
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {           
+            _logger.LogInformation($"-----------------:{DateTime.Now}");
+            return Ok();
+        }
+    }
+}
