65c13d3a5a2c742be994f5ed9ee8b47488be1167
素伟 桂 <guisuwei@outlook.com>
2019-12-17 19:22:53 +0900

    Soga测试

---
+++ Asp.NetCoreExperiment/Masstransit/AutomatonymousDemo01/Class.cs
@@ -0,0 +1,41 @@
+﻿using Automatonymous;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutomatonymousDemo01
+{
+    public class OrderState : SagaStateMachineInstance
+    {
+        public Guid CorrelationId { get; set; }
+        //public string CurrentState { get; set; }
+        public int CurrentState { get; set; }
+    }
+
+    //public class OrderStateMachine : MassTransitStateMachine<OrderState>
+    //{
+    //    public State Submitted { get; private set; }
+    //    public State Accepted { get; private set; }
+    //    public OrderStateMachine()
+    //    {
+    //        InstanceState(x => x.CurrentState, Submitted, Accepted);
+    //        //InstanceState(x => x.CurrentState);
+    //    }
+    //}
+
+    public interface SubmitOrder
+    {
+        Guid OrderId { get; }
+    }
+
+    public class OrderStateMachine : MassTransitStateMachine<OrderState>
+    {
+        public OrderStateMachine()
+        {
+            Event(() => SubmitOrder, x => x.CorrelateById(context => context.Message.OrderId));
+        }
+
+        public Event<SubmitOrder> SubmitOrder { get; private set; }
+    }
+}
