8f1f0757bd2866c2c84533ba8e3cbe024c723ae8
桂素伟 <axzxs2001@163.com>
2020-02-08 16:58:25 +0900

    添加事件

---
+++ Asp.NetCoreExperiment/DDD/sample/Do.Domain/Events/CaseSubmitEnvent.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Do.Domain.Events
+{
+    public class CaseSubmitEnvent : INotification
+    {
+        public string CaseID { get; private set; }
+        public CaseSubmitEnvent(string caseID)
+        {
+            CaseID = CaseID;
+        }
+    }
+
+}
