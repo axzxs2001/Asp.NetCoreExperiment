74763a6e7e286b0093800e5c92e09df9ecc0b132
桂素伟 <axzxs2001@163.com>
2020-04-15 17:29:36 +0900

    功有中间件的使用

---
+++ Asp.NetCoreExperiment/.netcore5.x/webmvcdemo002/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.Mvc;
+using webmvcdemo002.Models;
+
+namespace webmvcdemo002.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IFeatureManager _featureManager;
+
+        public HomeController(ILogger<HomeController> logger, IFeatureManager featureManager)
+        {
+            _featureManager = featureManager;
+            _logger = logger;
+        }
+        public async Task<IActionResult> Index()
+        {
+            if (await _featureManager.IsEnabledAsync(nameof(MyFeatureFlags.Privacy)))
+            {
+                Console.WriteLine("Privacy 可用");
+            }
+            else
+            {
+                Console.WriteLine("Privacy 不可用");
+            }
+            if (await _featureManager.IsEnabledAsync(nameof(MyFeatureFlags.PublicPage)))
+            {
+                Console.WriteLine("PublicPage 可用");
+            }
+            else
+            {
+                Console.WriteLine("PublicPage 不可用");
+            }
+            return View();
+        }
+
+        [FeatureGate(MyFeatureFlags.Privacy)]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [FeatureGate(MyFeatureFlags.PublicPage)]
+        public IActionResult PublicPage()
+        {
+            return View();
+        }
+        [FeatureGate(MyFeatureFlags.RadomPage)]
+        public IActionResult RadomPage()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    public enum MyFeatureFlags
+    {
+        Privacy,
+        PublicPage,
+        RadomPage
+    }
+}
---
+++ Asp.NetCoreExperiment/.netcore5.x/webmvcdemo002/Startup.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.FeatureManagement;
+using Microsoft.FeatureManagement.FeatureFilters;
+
+namespace webmvcdemo002
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddFeatureManagement()
+                   .AddFeatureFilter<PercentageFilter>();
+
+            services.AddControllersWithViews();
+        }
+
+   
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
