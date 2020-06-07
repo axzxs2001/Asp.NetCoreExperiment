c51733f02d51a145076546e16dc2c9644d051be7
桂素伟 <axzxs2001@163.com>
2020-06-07 18:53:07 +0900

    调用规则

---
+++ Asp.NetCoreExperiment/Common/CustomerFuncations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CustomerFuncations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var model = "${Replace(@content,1,3)}";
+
+            var content = "1234babcd";
+
+
+
+        }
+
+
+    }
+
+    class Functions
+    {
+        public string Replace(string content, string oldString, string newString)
+        {
+            return content.Replace(oldString, newString);
+        }
+    }
+}
