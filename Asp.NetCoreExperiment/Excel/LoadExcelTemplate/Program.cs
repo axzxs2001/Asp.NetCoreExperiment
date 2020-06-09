63a3fa5855d20197d6b5abce35119a51bb084369
素伟 桂 <axzxs2001@163.com>
2020-06-09 16:36:09 +0900

    excel添加单元格下拉列表

--- Asp.NetCoreExperiment/Excel/LoadExcelTemplate/Program.cs
+++ Asp.NetCoreExperiment/Excel/LoadExcelTemplate/Program.cs
@@ -13,6 +13,31 @@ namespace LoadExcelTemplate
     {
         static void Main(string[] args)
         {
+            Demo03();
+        }
+
+        static void Demo03()
+        {
+            //添加单元格下拉列表
+            var file = Directory.GetCurrentDirectory() + "\\aaaaa.xlsx";
+            using (var source = System.IO.File.Create(file))
+            {
+                using (var excel = new ExcelPackage(source))
+                {
+                    var sheet = excel.Workbook.Worksheets.Add("sheet0001");
+                    //ExcelWorksheet ws = excel.CreateSheet(p, "sheetname", 1, true);
+                    var unitmeasure = sheet.DataValidations.AddListValidation("a1:a1000");
+                    unitmeasure.Formula.Values.Add("Sq Ft");
+                    unitmeasure.Formula.Values.Add("Meter");
+                    excel.SaveAs(source);
+
+
+
+                }
+            }
+        }
+        static void Demo02()
+        {
             using (var con = new SqlConnection("server=.;database=testdb;uid=sa;pwd=sa;"))
             {
                 var sql = "select name,age,sex,height from works where id=1";
@@ -28,10 +53,10 @@ namespace LoadExcelTemplate
                             var jsonFile = Directory.GetCurrentDirectory() + "\\excelmap.json";
                             var excelMaps = Newtonsoft.Json.JsonConvert.DeserializeObject<List<ExcelMap>>(File.ReadAllText(jsonFile));
                             var fields = excelMaps.Select(s => s.Field);
-                           
+
                             foreach (var excelMap in excelMaps)
                             {
-                                
+
                                 excel.Workbook.Worksheets[1].Cells[excelMap.Row, excelMap.Col].Value = work[excelMap.Field];
                             }
                             var newfile = Directory.GetCurrentDirectory() + "\\newmywork.xlsx";
