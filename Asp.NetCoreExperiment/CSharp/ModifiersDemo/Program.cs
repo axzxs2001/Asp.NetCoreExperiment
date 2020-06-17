40427ae8724bb39e4f905937e8efdc26df25b058
素伟 桂 <axzxs2001@163.com>
2020-06-17 10:04:06 +0900

    访问修饰符的讲解

---
+++ Asp.NetCoreExperiment/CSharp/ModifiersDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using ModifiersDemo;
+using System;
+
+namespace ModifiersDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new TestModifiers().Test();
+            Console.WriteLine("-------------");
+            var mTest = new ModifiersLibDemo.TestModifiers();
+            mTest.Test();
+            Console.WriteLine("-------------");
+            mTest.Test2();
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class TestModifiers : ModifiersLibDemo.ModifiersClass
+    {
+        public void Test()
+        {
+            PublicMethod();
+            ProtectedMethod();
+            ProtectedInternalMethod();
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/ModifiersLibDemo/ModifiersClass.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace ModifiersLibDemo
+{
+    public class ModifiersClass
+    {
+        /// <summary>
+        /// 能在本程序集内外继承子类或实例化后调用
+        /// </summary>
+        public void PublicMethod()
+        {
+            Console.WriteLine("PublicMethod");
+        }
+        /// <summary>
+        /// 只能在继承子类中调用
+        /// </summary>
+        protected void ProtectedMethod()
+        {
+            Console.WriteLine("ProtectedMethod");
+        }
+        /// <summary>
+        /// 只能本类调用
+        /// </summary>
+        private void PrivateMethod()
+        {
+            Console.WriteLine("PrivateMethod");
+        }
+        /// <summary>
+        /// 只能在本程序集(dll,exe)继承子类或实例化后调用
+        /// </summary>
+        internal void InternalMethod()
+        {
+            Console.WriteLine("InternalMethod");
+        }
+        /// <summary>
+        /// 只能在本程序集(dll,exe)内外继承子类中或本程序集(dll,exe)中实例化后调用
+        /// </summary>
+        protected internal void ProtectedInternalMethod()
+        {
+            Console.WriteLine("ProtectedInternalMethod");
+        }
+        /// <summary>
+        /// 只能在本程序集(dll,exe)内的继承子类中调用
+        /// </summary>
+        private protected void PrivateProtectedMethod()
+        {
+            Console.WriteLine("PrivateProtectedMethod");
+        }
+    }
+
+
+    public class TestModifiers : ModifiersLibDemo.ModifiersClass
+    {
+        public void Test()
+        {
+            PublicMethod();
+            InternalMethod();
+            ProtectedInternalMethod();
+            ProtectedMethod();
+            PrivateProtectedMethod();
+        }
+
+        public void Test2()
+        {
+            var test2 = new ModifiersClass();
+            test2.InternalMethod();
+            test2.ProtectedInternalMethod();
+            test2.PublicMethod();
+        }
+    }
+}
