97c73f01b0f6b41ec3e0c942bdcee8883c5b1725
素伟 桂 <axzxs2001@163.com>
2020-07-08 14:15:09 +0900

    GraphQL简单demo

---
+++ Asp.NetCoreExperiment/GraphQL/GraphQLDemo01/Controllers/TestController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotChocolate;
+using HotChocolate.Execution;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GraphQLDemo01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+        private readonly ISchema _schema;
+        public TestController(ISchema schema, ILogger<TestController> logger)
+        {
+            _schema = schema;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get(string cod=null)
+        {
+            cod = "{persons{id name}}";//"{person(id:1){id name}}";
+            var executor = _schema.MakeExecutable();
+            return executor.Execute(cod).ToJson();
+        }
+    }
+    public class Query
+    {     
+        public IEnumerable<Person> GetPerson(int id)
+        {
+            return (new Person[] { new Person { Id = 1, Name = "AAA" }, new Person { Id = 2, Name = "BBBB" } }).Where(s=>s.Id==id);
+        }
+        public IEnumerable<Person> GetPersons()
+        {
+            return new Person[] { new Person { Id = 1, Name = "AAA" }, new Person { Id = 2, Name = "BBBB" } };
+        }
+    }
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Asp.NetCoreExperiment/GraphQL/GraphQLDemo01/Startup.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GraphQLDemo01.Controllers;
+using HotChocolate;
+using HotChocolate.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace GraphQLDemo01
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddGraphQL(sp => SchemaBuilder.New()
+    .AddQueryType<Query>()
+    .AddServices(sp)
+    .Create());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseGraphQL();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+
+}
