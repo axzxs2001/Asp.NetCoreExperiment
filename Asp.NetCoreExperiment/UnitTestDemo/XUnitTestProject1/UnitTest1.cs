f78cbf09a8eb42b01220fc80e05d82bbf524395a
素伟 桂 <axzxs2001@163.com>
2020-07-17 15:09:26 +0900

    wpf unittest success

--- Asp.NetCoreExperiment/UnitTestDemo/XUnitTestProject1/UnitTest1.cs
+++ Asp.NetCoreExperiment/UnitTestDemo/XUnitTestProject1/UnitTest1.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Threading;
+using System.Windows.Threading;
 using WpfApp1;
 using Xunit;
 
@@ -9,15 +11,27 @@ namespace XUnitTestProject1
         [Fact]
         public void ABCD()
         {
-            System.Windows.Application.Current.Dispatcher.Invoke((Action)(() =>
-            {
-                var main = new MainWindow();
-                main.button_Click(null, null);
-                Assert.Equal(DateTime.Parse(main.text01.Text).Date, DateTime.Now.Date);
-            }));
-  
-        
-
+            var  thread = new Thread(F);
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.IsBackground = true;
+            var t = new T();
+            thread.Start(t);
+            thread.Join();
+            Assert.Equal(t.V, DateTime.Now.Date);      
         }
+
+        static void F(object obj)
+        {
+            Thread.CurrentThread.SetApartmentState(ApartmentState.STA);
+            var main = new MainWindow();
+            main.button_Click(null, null);
+            var t = obj as T;
+            t.V = DateTime.Parse(main.text01.Text).Date;
+        }
+    }
+    class T
+    {
+        public object V
+        { get; set; }
     }
 }
