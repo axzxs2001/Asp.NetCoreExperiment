b5d128a4156c7fd2d17ad309aec452ad7715c9c1
桂素伟 <axzxs2001@163.com>
2020-07-24 23:36:38 +0900

    添加枚举项目EnumDemo,本例是位枚举案例。

---
+++ Asp.NetCoreExperiment/CSharp/EnumDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EnumDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FlagsEnumTest();
+        }
+        /// <summary>
+        /// 测试位枚举
+        /// </summary>
+        static void FlagsEnumTest()
+        {
+            var operation = Operation.Add | Operation.QueryAll | Operation.Modify;
+            Console.WriteLine(operation);
+        }
+    }
+
+    /// <summary>
+    /// 位枚兴
+    /// </summary>
+    [Flags]
+    enum Operation
+    {
+        None = 0,
+        Add = 1,
+        Modify = 2,
+        Remove = 4,
+        Query = 8,
+        QueryAll = 16
+    }
+}
