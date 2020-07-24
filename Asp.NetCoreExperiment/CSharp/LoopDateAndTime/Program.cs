6f3431caaf36595c1f4de45f00e8202c6aadc849
桂素伟 <axzxs2001@163.com>
2020-07-24 23:19:42 +0900

    测试git对应

--- Asp.NetCoreExperiment/CSharp/LoopDateAndTime/Program.cs
+++ Asp.NetCoreExperiment/CSharp/LoopDateAndTime/Program.cs
@@ -6,11 +6,11 @@ namespace LoopDateAndTime
     {
         static void Main(string[] args)
         {
-            // 天循环
+            // 按天循环
             var beginDate = DateTime.Parse("2020-02-25");
             var endDate = DateTime.Parse("2020-03-10");
             LoopDate(beginDate, endDate);
-            // 小时循环
+            // 按小时循环
             var beginTime = DateTime.Parse("2020-02-25 19:00:00");
             var endTime = DateTime.Parse("2020-02-27 01:30:00");
             LoopTime(beginDate, endDate);
