690b2a2d01e6be049ce3b05750716d8db71e5018
桂素伟 <axzxs2001@163.com>
2020-07-25 11:54:04 +0900

    EnumDemo项目中增加枚举类型转换的方法

--- Asp.NetCoreExperiment/CSharp/EnumDemo/Program.cs
+++ Asp.NetCoreExperiment/CSharp/EnumDemo/Program.cs
@@ -6,8 +6,33 @@ namespace EnumDemo
     {
         static void Main(string[] args)
         {
+            EnumConvert();
+
             FlagsEnumTest();
         }
+        /// <summary>
+        /// 枚举类型转换
+        /// </summary>
+        static void EnumConvert()
+        {
+            //用枚举值转换
+            var sex01 = (Sex)0;
+            Console.WriteLine($"字符串male转换后的类型：{sex01},对应值：{(int)sex01}");
+            //用Enum.Parse转换
+            var sex02 = (Sex)Enum.Parse(typeof(Sex), "Male");
+            Console.WriteLine($"字符串male转换后的类型：{sex02},对应值：{(int)sex02}");
+            //列出所有sex的枚举名称
+            foreach (var item in Enum.GetNames(typeof(Sex)))
+            {
+                Console.WriteLine($"Sex:{item} value:{(int)Enum.Parse(typeof(Sex), item)}");
+            }
+            //列出所有sex的枚举名称
+            foreach (var item in Enum.GetValues(typeof(Sex)))
+            {
+                Console.WriteLine($"Sex:{item} value:{(int)item}");
+            }
+        }
+
         /// <summary>
         /// 测试位枚举
         /// </summary>
@@ -16,6 +41,13 @@ namespace EnumDemo
             var operation = Operation.Add | Operation.QueryAll | Operation.Modify;
             Console.WriteLine(operation);
         }
+    }
+
+    enum Sex
+    {
+        Famale = 0,
+        Male = 1,
+        Other = 2
     }
 
     /// <summary>
