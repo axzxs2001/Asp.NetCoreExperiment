05915a7a8fa62a5d0080d826d0563e60340f8ca2
素伟 桂 <axzxs2001@163.com>
2020-08-05 18:33:17 +0900

    GraphQL demo完善。

--- Asp.NetCoreExperiment/GraphQL/GraphQLDemo01/Controllers/TestController.cs
+++ Asp.NetCoreExperiment/GraphQL/GraphQLDemo01/Controllers/TestController.cs
@@ -28,7 +28,7 @@ namespace GraphQLDemo01.Controllers
         [HttpGet]
         public async Task<string> Get(string query)
         {
-            var executor = _schema.MakeExecutable(); 
+            var executor = _schema.MakeExecutable();
             var executionResult = await executor.ExecuteAsync(query);
             return Regex.Unescape(executionResult.ToJson()); ;
         }
@@ -39,29 +39,30 @@ namespace GraphQLDemo01.Controllers
         public Query()
         {
             _items = new List<Item>() {
-                new Item { Id=1,Name="一级A" },
-                new Item { Id=2,Name="一级B" },
-                new Item { Id=3,Name="一级C" },
-                new Item { Id=4,Name="一级A-二级A",Pid=1 },
-                new Item { Id=5,Name="一级A-一级B",Pid=1 },
-                new Item { Id=6,Name="一级B-二级A",Pid=2 }
+                new Item { Id=1,Name="一级A",Istrue=true },
+                new Item { Id=2,Name="一级B",Istrue=false },
+                new Item { Id=3,Name="一级C",Istrue=true },
+                new Item { Id=4,Name="一级A-二级A",Pid=1,Istrue=false  },
+                new Item { Id=5,Name="一级A-一级B",Pid=1,Istrue=true },
+                new Item { Id=6,Name="一级B-二级A",Pid=2 ,Istrue=false }
             };
         }
 
-        /// <summary>
-        /// 条件查询
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        public Item GetPerson(int id)
+        public Item GetItem(int id)
         {
             return _items.SingleOrDefault(s => s.Id == id);
         }
-        /// <summary>
-        /// 全部查询
-        /// </summary>
-        /// <returns></returns>
-        public IEnumerable<Item> GetPersons()
+
+        public IEnumerable<Item> Queryistrue(bool istrue)
+        {
+            return _items.Where(s => s.Istrue == istrue);
+        }
+    
+        public IEnumerable<Item> GetItems()
+        {
+            return _items;
+        }
+        public IEnumerable<Item> GetEntity()
         {
             return _items;
         }
@@ -69,6 +70,8 @@ namespace GraphQLDemo01.Controllers
         {
             return _items.Where(s => s.Pid == pid);
         }
+
+
     }
     /// <summary>
     /// 实体类
@@ -78,5 +81,6 @@ namespace GraphQLDemo01.Controllers
         public int Id { get; set; }
         public string Name { get; set; }
         public int Pid { get; set; }
+        public bool Istrue { get; set; }
     }
 }
--- Asp.NetCoreExperiment/GraphQL/GraphQLDemo_Client/Program.cs
+++ Asp.NetCoreExperiment/GraphQL/GraphQLDemo_Client/Program.cs
@@ -13,8 +13,33 @@ namespace GraphQLDemo_Client
     {
         async static Task Main(string[] args)
         {
-            await GetAll();
-            await GetByID(1);
+            while (true)
+            {
+                Console.WriteLine("1、字段  2、参数  3、别名  4、片段  5、变量片段  6、操作名");
+                var key = Console.ReadLine();
+                switch (key)
+                {
+                    case "1":
+                        await GetAll();
+                        break;
+                    case "2":
+                        await GetByID(1);
+                        break;
+                    case "3":
+                        await GetAllAliases();
+                        break;
+                    case "4":
+                        await GetByIDFragments(1);
+                        break;
+                    case "5":
+                        await GetByIDFragmentsVar();
+                        break;
+                    case "6":
+                        await GetItems();
+                        break;
+
+                }
+            }
         }
 
         async static Task<string> HttpGetAsync(string query)
@@ -26,21 +51,72 @@ namespace GraphQLDemo_Client
             var content = await response.Content.ReadAsStringAsync();
             return content;
         }
-
+        /// <summary>
+        /// Fields
+        /// </summary>
+        /// <returns></returns>
         async static Task GetAll()
         {
             //全部查询 字段 Fields
-            var query = "{persons{id name }}";
-            var content = await HttpGetAsync(query);          
+            var query = "{items{id name }}";
+            var content = await HttpGetAsync(query);
             Console.WriteLine(content);
         }
 
-
+        /// <summary>
+        /// Arguments
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         async static Task GetByID(int id)
         {
             //条件查询 参数 Arguments
-            var query = $"{{person(id:{id}){{id name }} childs(pid:{id}){{id name}}}}";
-            var content = await HttpGetAsync(query);           
+            var query = $"{{item(id:{id}){{id name }}}}";
+            var content = await HttpGetAsync(query);
+            Console.WriteLine(content);
+        }
+        /// <summary>
+        /// Aliases
+        /// </summary>
+        /// <returns></returns>
+
+        async static Task GetAllAliases()
+        {
+            //全部查询 字段 Fields
+            var query = "{is:items{id name }}";
+            var content = await HttpGetAsync(query);
+            Console.WriteLine(content);
+        }
+        /// <summary>
+        /// Fragments
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        async static Task GetByIDFragments(int id)
+        {
+            var query = $"{{item(id:{id}){{id name }} childs(pid:{id}){{id name}}}}";
+            var content = await HttpGetAsync(query);
+            Console.WriteLine(content);
+        }
+        /// <summary>
+        /// Fragments Var
+        /// </summary>
+        /// <returns></returns>
+        async static Task GetByIDFragmentsVar()
+        {
+            var query = $"{{yes:queryistrue(istrue:true){{id name istrue }} error:queryistrue(istrue:false){{id name istrue }}}}";
+            var content = await HttpGetAsync(query);
+            Console.WriteLine(content);
+        }
+        /// <summary>
+        /// Operation name
+        /// </summary>
+        /// <returns></returns>
+        async static Task GetItems()
+        {
+            //全部查询 字段 Fields
+            var query = "query GetItems{entity{id name}}";
+            var content = await HttpGetAsync(query);
             Console.WriteLine(content);
         }
     }
