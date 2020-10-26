938b4e0cfe6e46c9c74973384811986ba9dcab36
素伟 桂 <axzxs2001@163.com>
2020-10-26 19:16:56 +0900

    .NET架构小技巧（一）——枚举

--- Asp.NetCoreExperiment/Architecture/ArchitectureDemo01/Program.cs
+++ Asp.NetCoreExperiment/Architecture/ArchitectureDemo01/Program.cs
@@ -10,29 +10,44 @@ namespace ArchitectureDemo01
     {
         static void Main(string[] args)
         {
-            var person = new Person
-            {
-                //ID = 1,
-                Name = "桂素伟",
-                Sex = Sex.Male
-            };
-            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(person));
 
-            AddPerson(person);
-
-            foreach (var per in GetPersons())
+            while (true)
             {
-                Console.WriteLine($"Name:{per.Name},Sex:{per.Sex}");
+                Console.WriteLine("选择操作： 1、添加  2、查询  3、枚举返回集合");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        var person = new Person
+                        {                           
+                            Name = "桂素伟",
+                            Sex = Sex.Male,
+                            UserType = UserType.App | UserType.Web
+                        };
+                        Console.WriteLine("录入信息是：");
+                        Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(person));
+                        AddPerson(person);
+                        break;
+                    case "2":
+                        foreach (var per in GetPersons())
+                        {
+                            Console.WriteLine($"Name:{per.Name},Sex:{per.Sex},UserType:{per.UserType}");
+                        }
+                        break;
+                    case "3":
+                        Console.WriteLine("Sex集合：");
+                        Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(GetEnumList<Sex>()));
+                        Console.WriteLine("UserType集合：");
+                        Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(GetEnumList<UserType>()));
+                        break;
+                }
             }
-
-            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(GetSexs()));
 
         }
         static void AddPerson(Person person)
         {
             using (var con = new MySqlConnection("server=localhost;database=marsdb;uid=root;pwd=mars2020;"))
             {
-                var sql = @"INSERT INTO `marsdb`.`persons`(`id`,`name`,`sex`) VALUES(@id,@name,@sex);";
+                var sql = @"INSERT INTO `marsdb`.`persons`(`id`,`name`,`sex`,`usertype`) VALUES(@id,@name,@sex,@usertype);";
                 con.Execute(sql, person);
             }
         }
@@ -46,34 +61,36 @@ namespace ArchitectureDemo01
             }
         }
 
-        static List<dynamic> GetSexs()
+        static List<dynamic> GetEnumList<T>() where T : struct, Enum
         {
-            var sexs = Enum.GetValues<Sex>();
+            var enums = Enum.GetValues<T>();
             var list = new List<dynamic>();
-            foreach (var sex in sexs)
+            foreach (var item in enums)
             {
-                list.Add(new { Name = sex.ToString(), Value = (int)sex });
+                list.Add(new { Name = item.ToString(), Value = Convert.ToInt32(item) });
             }
             return list;
         }
     }
     /*mysql库
-   CREATE TABLE `persons` (
+CREATE TABLE `persons` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `sex` int DEFAULT NULL,
+  `usertype` int DEFAULT NULL,
   PRIMARY KEY (`id`)
-   ) 
+)
      */
     public class Person
     {
         public int ID { get; set; }
         public string Name { get; set; }
         /// <summary>
-        /// 语义明确
-        /// 
+        /// 性别，用枚举语义明确 
         /// </summary>
         public Sex Sex { get; set; }
+
+        public UserType UserType { get; set; }
 
     }
 
@@ -85,5 +102,28 @@ namespace ArchitectureDemo01
         None = 0,
         Male = 1,
         Female = 2,
+    }
+    /// <summary>
+    /// 位数组
+    /// </summary>
+    [Flags]
+    public enum UserType
+    {
+        /// <summary>
+        /// app用户
+        /// </summary>
+        App = 1,
+        /// <summary>
+        /// web用户
+        /// </summary>
+        Web = 2,
+        /// <summary>
+        /// 不程序用户
+        /// </summary>
+        MiniPro = 4,
+        /// <summary>
+        /// 公众号用户
+        /// </summary>
+        OfficialAccounts = 8
     }
 }
