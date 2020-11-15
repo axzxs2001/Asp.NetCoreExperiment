76cdc7704b402e72efb46627a9c7616ab2deedf6
桂素伟 <axzxs2001@163.com>
2020-11-15 17:51:22 +0900

    MyCICD完成。

--- Asp.NetCoreExperiment/Architecture/MyCICD/Program.cs
+++ Asp.NetCoreExperiment/Architecture/MyCICD/Program.cs
@@ -14,75 +14,91 @@ namespace MyCICD
     {
         static void Main(string[] args)
         {
+            var processIDs = new int[0];
             while (true)
             {
                 if (Clone())
                 {
-                    if (Publish())
+                    if (Publish(processIDs))
                     {
-                        Run();
+                        processIDs = Run();
                     }
                 }
-
                 Thread.Sleep(30000);
             }
         }
+
+        /// <summary>
+        /// git 克隆
+        /// </summary>
+        /// <returns></returns>
         static bool Clone()
         {
-
             var gitLib = ConfigurationManager.AppSettings["GitLib"];
             var sourcePath = ConfigurationManager.AppSettings["SourcePath"];
-
-            var SourceDir = $"{sourcePath.TrimEnd('/', '\\') }/{ Path.GetFileNameWithoutExtension(gitLib)} ";
-            if (Directory.Exists(SourceDir))
+            var sourceDir = $"{sourcePath.TrimEnd('/', '\\') }/{ Path.GetFileNameWithoutExtension(gitLib)} ";
+            //存在就拉取代码，不存在就克隆
+            if (Directory.Exists(sourceDir))
             {
-                return Fetch(SourceDir);
-
+                return Pull(sourceDir);
             }
             else
             {
-                Console.WriteLine("开始Clone");
-                var processStartInfo = new ProcessStartInfo("git", $"clone {gitLib} {SourceDir}") { RedirectStandardOutput = true };
-
-                var process = Process.Start(processStartInfo);
-                if (process == null)
-                {
-                    Console.WriteLine("请确认是否安装git");
-                    return false;
-                }
-                else
-                {
-                    using (var output = process.StandardOutput)
-                    {
-                        while (!output.EndOfStream)
-                        {
-                            Console.WriteLine(output.ReadLine());
-                        }
-
-                        if (!process.HasExited)
-                        {
-                            process.Kill();
-                        }
-                    }
-                    Console.WriteLine($"执行时间 :{(process.ExitTime - process.StartTime).TotalMilliseconds} ms");
-                    Console.WriteLine("结束Clone");
-                    return process.ExitCode == 0;
-
-                }
+                return Clone(gitLib, sourceDir);
             }
         }
-        static bool Fetch(string SourceDir)
+        /// <summary>
+        /// 克隆代码
+        /// </summary>
+        /// <param name="gitLib">git库</param>
+        /// <param name="sourceDir">本地保存路径</param>
+        /// <returns></returns>
+        static bool Clone(string gitLib, string sourceDir)
+        {
+            Console.WriteLine("开始Clone");
+            var processStartInfo = new ProcessStartInfo("git", $"clone {gitLib} {sourceDir}") { RedirectStandardOutput = true };
+
+            var process = Process.Start(processStartInfo);
+            if (process == null)
+            {
+                Console.WriteLine("请确认是否安装git");
+                return false;
+            }
+            else
+            {
+                using (var output = process.StandardOutput)
+                {
+                    while (!output.EndOfStream)
+                    {
+                        Console.WriteLine(output.ReadLine());
+                    }
+
+                    if (!process.HasExited)
+                    {
+                        process.Kill();
+                    }
+                }
+                Console.WriteLine($"执行时间 :{(process.ExitTime - process.StartTime).TotalMilliseconds} ms");
+                Console.WriteLine("结束Clone");
+                return process.ExitCode == 0;
+
+            }
+        }
+        /// <summary>
+        /// 拉取项目
+        /// </summary>
+        /// <param name="sourceDir"></param>
+        /// <returns></returns>
+        static bool Pull(string sourceDir)
         {
             Console.WriteLine("开始Fetch");
-            var process = new Process();
-            process.StartInfo.WorkingDirectory = SourceDir;
-            process.StartInfo.FileName = "git";
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.Arguments = $"fetch origin";
-            process.Start();
-         
+            var processStartInfo = new ProcessStartInfo("git", $"pull origin")
+            {
+                RedirectStandardOutput = true,
+                WorkingDirectory = sourceDir,
+            };
+
+            var process = Process.Start(processStartInfo);
             using (var output = process.StandardOutput)
             {
                 var resultBuilder = new StringBuilder();
@@ -95,7 +111,7 @@ namespace MyCICD
                 {
                     process.Kill();
                 }
-                if (resultBuilder.Length > 0)
+                if (resultBuilder.ToString() != "Already up to date.\r\n")
                 {
                     Console.WriteLine("结束Fetch");
                     return true;
@@ -106,37 +122,16 @@ namespace MyCICD
                     return false;
                 }
             }
-
-     
         }
 
-        static bool Run()
-        {
-            Console.WriteLine("开始运行");
-            var sourcePath = ConfigurationManager.AppSettings["SourcePath"];
-            var publishDir = $"{sourcePath}/publish";
-            var result = true;
-            foreach (var project in Directory.GetDirectories(publishDir))
-            {
-                new Thread(RunProject).Start(project);
-            }
-            Console.WriteLine("结束运行");
-            return result;
-        }
-        static void RunProject(object project)
-        {
-            var projectPath = project.ToString();
-            var process = new Process();
-            process.StartInfo.FileName = "cmd.exe ";
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardInput = true;
-            process.StartInfo.Arguments = $"/k cd /d {projectPath}";
-            process.Start();
-            process.StandardInput.WriteLine($"dotnet {Path.GetFileNameWithoutExtension(projectPath)}.dll");
-            Console.WriteLine($"{Path.GetFileNameWithoutExtension(projectPath)}运行成功");
-        }
 
-        static bool Publish()
+
+        #region 发布项目
+        /// <summary>
+        /// 发布项目
+        /// </summary>
+        /// <returns></returns>
+        static bool Publish(int[] processIDs)
         {
             Console.WriteLine("开始Publish");
             var sourcePath = ConfigurationManager.AppSettings["SourcePath"];
@@ -145,6 +140,12 @@ namespace MyCICD
             var projects = GetProjectsPath(sourcePath, projectPathLists);
             var publishDir = $"{sourcePath}/publish";
             var result = true;
+            //关闭之前进程
+            foreach (var processid in processIDs)
+            {
+                Process.GetProcessById(processid).Kill();
+            }
+            //发布项目
             foreach (var project in projects)
             {
                 var processStartInfo = new ProcessStartInfo("dotnet", $"publish {project} -o {publishDir}/{Path.GetFileNameWithoutExtension(project)}") { RedirectStandardOutput = true };
@@ -180,6 +181,12 @@ namespace MyCICD
             Console.WriteLine("结束Publish");
             return result;
         }
+        /// <summary>
+        /// 查找项目
+        /// </summary>
+        /// <param name="sourcePath">源码路径</param>
+        /// <param name="projects">项目集</param>
+        /// <returns></returns>
         static string[] GetProjectsPath(string sourcePath, string[] projects)
         {
             var paths = new List<string>();
@@ -197,5 +204,33 @@ namespace MyCICD
             }
             return paths.ToArray();
         }
+        #endregion
+
+        #region 运行项目
+        /// <summary>
+        /// 运行项目
+        /// </summary>
+        /// <returns></returns>
+        static int[] Run()
+        {
+            Console.WriteLine("开始运行");
+            var sourcePath = ConfigurationManager.AppSettings["SourcePath"];
+            var publishDir = $"{sourcePath}/publish";
+            var proceddIDs = new List<int>();
+            foreach (var projectPath in Directory.GetDirectories(publishDir))
+            {
+                var processStartInfo = new ProcessStartInfo("dotnet", $"{Path.GetFileNameWithoutExtension(projectPath)}.dll")
+                {
+                    RedirectStandardOutput = true,
+                    WorkingDirectory = projectPath,
+                };
+                var process = Process.Start(processStartInfo);
+                proceddIDs.Add(process.Id);
+            }
+            Console.WriteLine("结束运行");
+            return proceddIDs.ToArray();
+        }
+
+        #endregion
     }
 }
