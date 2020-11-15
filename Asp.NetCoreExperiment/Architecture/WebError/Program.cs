378b2fc24dce1d38f6cbba61d2ef31c3f6989e01
桂素伟 <axzxs2001@163.com>
2020-11-15 14:05:48 +0900

    修改CICD测试项目端口

--- Asp.NetCoreExperiment/Architecture/WebError/Program.cs
+++ Asp.NetCoreExperiment/Architecture/WebError/Program.cs
@@ -19,7 +19,7 @@ namespace WebError
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
+                {                   
                     webBuilder.UseStartup<Startup>();
                 });
     }
