f5ef6a18a38abc139acb14f48d98ab42e539cdac
素伟 桂 <axzxs2001@163.com>
2020-11-24 10:17:15 +0900

    添加GraphQL网关项目

--- Asp.NetCoreExperiment/GraphQL/GraphQLDemo02/DB/AdventureWorks2016Context.cs
+++ Asp.NetCoreExperiment/GraphQL/GraphQLDemo02/DB/AdventureWorks2016Context.cs
@@ -117,7 +117,6 @@ namespace GraphQLDemo02
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=AdventureWorks2016;Persist Security Info=True;User ID=sa;Password=sa");
             }
         }
