61106858fbcaba0a852a6093dd5134498e2436c4
桂素伟 <axzxs2001@163.com>
2021-01-09 11:44:09 +0900

    elsa demo01

---
+++ Asp.NetCoreExperiment/ELSA/APIDemo/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Person GetPerson()
+        {
+            _logger.LogInformation("请求person");
+            return new Person() { ID = 100, Name = "张三丰收", Age = 22, Sex = true };
+        }
+    }
+
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public bool Sex { get; set; }
+    }
+}
---
+++ Asp.NetCoreExperiment/ELSA/Elsa01/Program.cs
@@ -0,0 +1,61 @@
+﻿using Elsa.Activities.Console.Activities;
+using Elsa.Activities.Http.Activities;
+using Elsa.Activities.Http.Parsers;
+using Elsa.Activities.Http.Services;
+using Elsa.Expressions;
+using Elsa.Scripting.JavaScript;
+using Elsa.Services;
+using Elsa.Services.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace Elsa01
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("回车开始");
+            Console.ReadLine();
+            var services = new ServiceCollection()
+              .AddElsa()
+              //注入Response返回格式
+              .AddScoped<IHttpResponseBodyParser, JsonHttpResponseBodyParser>()
+              //注入httpclient工厂
+              .AddHttpClient()
+              .AddActivity<SendHttpRequest>()
+              .AddActivity<WriteLine>()
+              .BuildServiceProvider();
+            var invoker = services.GetService<IWorkflowInvoker>();
+            await invoker.StartAsync<PersonHandleWorkflow>();
+            Console.WriteLine("回车结束");
+            Console.ReadLine();
+        }
+    }
+    /// <summary>
+    /// 定义工作流两个activity,一个是请求webapi，一个是显示请求结果
+    /// </summary>
+    public class PersonHandleWorkflow : IWorkflow
+    {
+        public void Build(IWorkflowBuilder builder)
+        {
+            builder
+                .StartWith<SendHttpRequest>(RequestAPI, "http5001")
+                .Then<WriteLine>(ShowMessage);
+        }
+
+        void ShowMessage(WriteLine write)
+        {
+            //用javascript脚本来处理返回信息
+            write.TextExpression = new JavaScriptExpression<string>("'ID:'+lastResult('http5001').Content[\"id\"]+'  Name:'+lastResult('http5001').Content[\"name\"]+'  Age:'+lastResult('http5001').Content[\"age\"]");
+        }
+        void RequestAPI(SendHttpRequest http)
+        {
+            //定义http发送参数
+            http.ContentType = "application/json";
+            http.Method = "get";
+            http.Url = new WorkflowExpression<Uri>(LiteralEvaluator.SyntaxName, "https://localhost:5001/home");
+        }
+    }
+}
