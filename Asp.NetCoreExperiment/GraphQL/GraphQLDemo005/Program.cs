6d6a4dfafbbb7ddb1ecb5ec208832fb5a009eb8a
桂素伟 <axzxs2001@163.com>
2021-01-26 09:53:09 +0900

    Interface在GraphQL中

--- Asp.NetCoreExperiment/GraphQL/GraphQLDemo005/Program.cs
+++ Asp.NetCoreExperiment/GraphQL/GraphQLDemo005/Program.cs
@@ -5,11 +5,13 @@ using HotChocolate.Execution;
 using HotChocolate.Execution.Instrumentation;
 using HotChocolate.Fetching;
 using HotChocolate.Language;
+using HotChocolate.Types;
 using Microsoft.Extensions.DiagnosticAdapter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
+
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -20,48 +22,193 @@ namespace GraphQLDemo005
     {
         static void Main(string[] args)
         {
-            DataLoaderDemo.Run();
+            InterfaceDemo.Run();
         }
     }
-    public class DataLoaderDemo
+    public class InterfaceDemo
     {
 
         public static void Run()
         {
             var schema = SchemaBuilder.New()
+                .AddQueryType<Query>()
+                //.AddType<AFormatType>()
+                //.AddType<BFormatType>()
+                .AddType<AFormat>()             
+                .AddType<BFormat>()                
                 .AddProjections()
-                .AddQueryType<Query>()               
-                .Create();           
-          
+                .Create();
+
 
             var executor = schema.MakeExecutable();
-            Console.WriteLine(executor.Execute("{person(id:1){id name tel} }").ToJson());
+            Console.WriteLine(executor.Execute(@"
+{
+    formats
+    {
+        __typename,
+        name,        
+        ... on AFormat{
+            a1,
+            a2
+        },
+        ... on BFormat{
+            b1,
+            b2
+        }
+    } 
+}").ToJson());
 
         }
-        /// <summary>
-        /// 查询类
-        /// </summary>
-        public class Query
-        {
-            [UseProjection]
-            public Person GetPerson(int id)
-            {
-                return new Person { Id = id, Name = "ZhangSanFeng", Tel = "13453467114" };
-            }
-        }
-
-        /// <summary>
-        /// 用户
-        /// </summary>
-        public class Person
-        {
-            public int Id { get; set; }
-            public string Name { get; set; }
-            public string Tel { get; set; }
-        }
-
-       
     }
 
+    #region 抽象类1 通不过
+    public class Query
+    {
+        public Format[] GetFormats()
+        {
+            return new Format[]
+            {
+                    new AFormat{
+                         Name="A",
+                         A1="A1",
+                         A2="A2"
+                    },
+                    new BFormat{
+                       Name="B",
+                       B1="B1",
+                       B2="B2"
+                    }
+            };
+        }
+    }
+    [InterfaceType]
+    public abstract class Format
+    {
+        public string Name { get; set; }
+    }
+    public class AFormat : Format
+    {
+        public string A1 { get; set; }
+        public string A2 { get; set; }
+    }
+  
+    public class BFormat : Format
+    {
+        public string B1 { get; set; }
+        public string B2 { get; set; }
+    }
+    #endregion
+    #region 抽象类2
 
+    //public class Query
+    //{
+    //    public Format[] GetFormats()
+    //    {
+    //        return new Format[]
+    //        {
+    //                new AFormat{
+    //                     Name="A",
+    //                     A1="A1",
+    //                     A2="A2"
+    //                },
+    //                new BFormat{
+    //                   Name="B",
+    //                   B1="B1",
+    //                   B2="B2"
+    //                }
+    //        };
+    //    }
+    //}
+
+    //public abstract class Format
+    //{
+    //    public abstract string Name { get; set; }
+    //}
+    //public class AFormat : Format
+    //{
+    //    public override string Name { get; set; }
+    //    public string A1 { get; set; }
+    //    public string A2 { get; set; }
+    //}
+
+    //public class BFormat : Format
+    //{
+    //    public override string Name { get; set; }
+    //    public string B1 { get; set; }
+    //    public string B2 { get; set; }
+    //}
+
+
+
+    //public class FormatType : InterfaceType<Format>
+    //{
+    //    protected override void Configure(IInterfaceTypeDescriptor<Format> descriptor)
+    //    {
+    //        base.Configure(descriptor);
+    //    }
+    //}
+    //public class AFormatType : ObjectType<AFormat>
+    //{
+    //    protected override void Configure(IObjectTypeDescriptor<AFormat> descriptor)
+    //    {
+    //        descriptor.Implements<FormatType>();
+    //    }
+    //}
+
+    //public class BFormatType : ObjectType<BFormat>
+    //{
+    //    protected override void Configure(IObjectTypeDescriptor<BFormat> descriptor)
+    //    {
+    //        descriptor.Implements<FormatType>();
+    //    }
+    //}
+
+    #endregion
+    #region 接口
+    //public class Query
+    //{
+    //    public IFormat[] GetFormats()
+    //    {
+    //        return new IFormat[]
+    //        {
+    //                new AFormat{
+    //                     Name="A",
+    //                     A1="A1",
+    //                     A2="A2"
+    //                },
+    //                new BFormat{
+    //                   Name="B",
+    //                   B1="B1",
+    //                   B2="B2"
+    //                }
+    //        };
+    //    }
+    //}
+
+    //[GraphQLName("Format")]     
+    //public interface IFormat
+    //{
+    //   string Name { get; set; }
+
+    //}
+
+    //public class AFormat : IFormat
+    //{
+    //    public  string Name { get; set; }
+
+    //    public string A1 { get; set; }
+    //    public string A2 { get; set; }
+    //}
+
+    //public class BFormat : IFormat
+    //{
+    //    public  string Name { get; set; }
+
+    //    public string B1 { get; set; }
+    //    public string B2 { get; set; }
+    //}
+    #endregion
 }
+
+
+
