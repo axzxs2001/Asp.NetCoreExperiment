94089ec52f426fd9537e48665fa72888561752fa
素伟 桂 <axzxs2001@163.com>
2021-03-23 18:33:16 +0900

    null处理

---
+++ Asp.NetCoreExperiment/CSharp/KeyWordsDemo/NullDemo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Console;
+
+namespace KeyWordsDemo
+{
+    class NullDemo : Demo
+    {
+        public void Run()
+        {
+            var str = string.Empty;//和str=""等价
+            var result = str switch
+            {
+                var s when s is null => "string.Empty is null",
+                var s when s is "" => "string.Empty is \"\"",
+                var _ => "none"
+            };
+            WriteLine(result);
+
+            if (string.IsNullOrEmpty(str))
+            {
+                WriteLine("string is null or \"\"");
+            }
+
+            //------------------
+            Nullable<int> i = 10;
+            if (i.HasValue)
+            {
+                WriteLine(i.Value);
+            }
+            int? ii = null;
+            if (ii.HasValue)
+            {
+                WriteLine(ii.Value);
+            }
+            //-------
+            var c = new AppOrder();
+            if (c == null)
+            {
+                Console.WriteLine("c is null");
+            }
+
+
+        }
+    }
+    class Order
+    {
+        public static bool operator ==(Order left, Order right)
+        {
+            return true;
+        }
+        public static bool operator !=(Order left, Order right)
+        {
+            return true;
+        }
+
+        public string OrderNo
+        { get; set; }
+    }
+
+    class AppOrder : Order
+    {
+
+    }
+
+}
---
+++ Asp.NetCoreExperiment/CSharp/KeyWordsDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace KeyWordsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Demo demo = new NullDemo();
+            demo.Run();
+        }
+    }
+
+    interface Demo
+    {
+        void Run();
+    }
+}
