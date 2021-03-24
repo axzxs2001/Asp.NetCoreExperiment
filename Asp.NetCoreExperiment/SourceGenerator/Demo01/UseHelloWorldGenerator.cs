095033807d8e807411a7ce3252cdb938fc2a0c98
素伟 桂 <axzxs2001@163.com>
2021-03-24 17:33:25 +0900

    完成测试，有只能是特定版本。

--- Asp.NetCoreExperiment/SourceGenerator/Demo01/UseHelloWorldGenerator.cs
+++ Asp.NetCoreExperiment/SourceGenerator/Demo01/UseHelloWorldGenerator.cs
@@ -1,11 +1,14 @@
-﻿
+﻿using System;
+
 namespace Demo01
 {
     public static class UseHelloWorldGenerator
     {
+        //    <OutputType>Exe</OutputType>
+        //  <TargetFramework>net5.0</TargetFramework>
         public static void Run()
-        {         
-            HelloWorldGenerated.HelloWorld.SayHello();
+        {
+            HelloWorldGenerated.HelloWorld.SayHello2();
         }
     }
 }
--- Asp.NetCoreExperiment/SourceGenerator/MyGenerator/HelloWorldGenerator.cs
+++ Asp.NetCoreExperiment/SourceGenerator/MyGenerator/HelloWorldGenerator.cs
@@ -1,31 +1,33 @@
-﻿
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Text;
 
-namespace Demo01Generator
+namespace SourceGeneratorSamples
 {
     [Generator]
     public class HelloWorldGenerator : ISourceGenerator
-    {
+    {      
+        
+        //必需是.Net Stantard 2.0
         public void Execute(GeneratorExecutionContext context)
         {
             // begin creating the source we'll inject into the users compilation
-            StringBuilder sourceBuilder = new StringBuilder(@"
+            var sourceBuilder = new StringBuilder(@"
 using System;
 namespace HelloWorldGenerated
 {
     public static class HelloWorld
     {
-        public static void SayHello() 
+        public static void SayHello2() 
         {
             Console.WriteLine(""Hello from generated code!"");
             Console.WriteLine(""The following syntax trees existed in the compilation that created this program:"");
 ");
 
             // using the context, get a list of syntax trees in the users compilation
-            IEnumerable<SyntaxTree> syntaxTrees = context.Compilation.SyntaxTrees;
+            var syntaxTrees = context.Compilation.SyntaxTrees;
 
             // add the filepath of each tree to the class we're building
             foreach (SyntaxTree tree in syntaxTrees)
@@ -40,12 +42,14 @@ namespace HelloWorldGenerated
 }");
 
             // inject the created source into the users compilation
-            context.AddSource("helloWorldGenerated", sourceBuilder.ToString());
+            context.AddSource("HelloWorldGenerated", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
         }
+
+     
 
         public void Initialize(GeneratorInitializationContext context)
         {
-            // No initialization required
+            //
         }
     }
 }
