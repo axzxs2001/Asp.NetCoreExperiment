a954b70d9d1e712478e394b68f34a8e29fd8dfc5
素伟 桂 <axzxs2001@163.com>
2021-04-01 17:39:47 +0900

    模板

---
+++ Asp.NetCoreExperiment/ProjectTemplate/working/templates/GSWNuget/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GSWNuget.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+       
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+       
+    }
+}
