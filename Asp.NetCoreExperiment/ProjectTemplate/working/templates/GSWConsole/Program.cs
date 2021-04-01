2d83ac1397a6b6d3812c91f77ad3494187d8e597
素伟 桂 <axzxs2001@163.com>
2021-04-01 12:23:35 +0900

    添加项目模板。

---
+++ Asp.NetCoreExperiment/ProjectTemplate/working/templates/GSWConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Dapper;
+using NLog;
+
+namespace GSWCon
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine(Dapper.CommandFlags.Buffered);
+        Console.WriteLine(NLog.Config.ExceptionRenderingFormat.Data);
+    }
+}
+}
---
+++ Asp.NetCoreExperiment/ProjectTemplate/working/templates/GSWWebAPI/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GSWWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return await Task.FromResult("ok");
+        }
+    }
+}
