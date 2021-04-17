4d7518aa06e6a7bf58467136ed0a201ec88c01ce
桂素伟 <axzxs2001@163.com>
2021-04-17 23:14:29 +0900

    winform blazor

---
+++ Asp.NetCoreExperiment/Blazor/BlazorWinForm/frmMain.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace BlazorWinForm
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1720, 1226);
+            this.Name = "frmMain";
+            this.Text = "药品查询";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Asp.NetCoreExperiment/Blazor/BlazorWinForm/frmMain.cs
@@ -0,0 +1,58 @@
+﻿using BlazorWinForm.wwwroot;
+using Microsoft.AspNetCore.Components.WebView.WindowsForms;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Dapper;
+
+
+namespace BlazorWinForm
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+
+            InitializeComponent();
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddBlazorWebView();
+            var blazor = new BlazorWebView()
+            {
+                Width = this.Width - 5,
+                Height = this.Height - 5,
+                // Dock = DockStyle.Fill,
+                HostPage = "wwwroot/index.html",
+                Services = serviceCollection.BuildServiceProvider(),
+
+            };
+            blazor.RootComponents.Add<Query>("#app");
+            Controls.Add(blazor);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+    }
+
+    class Goods
+    {
+        public string spid { get; set; }
+        public string spmch { get; set; }
+        public string shpchd { get; set; }
+        public string shpgg { get; set; }
+    }
+}
