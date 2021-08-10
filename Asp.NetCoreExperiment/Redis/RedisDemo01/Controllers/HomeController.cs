8ffe4626a76c77dacf2fa5dfbb64c809bfc23237
桂素伟 <axzxs2001@163.com>
2021-08-10 09:08:08 +0900

    redis demo

---
+++ Asp.NetCoreExperiment/Redis/RedisDemo01/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisDemo01.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : ControllerBase
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IDatabase _db;
+
+        public HomeController(ILogger<HomeController> logger, IConnectionMultiplexer multiplexer)
+        {
+            _db = multiplexer.GetDatabase();
+            _logger = logger;
+        }
+
+        [HttpGet("/write")]
+        public async Task<string> Write()
+        {
+            var result = await _db.StringSetAsync("time", DateTime.Now.ToString());
+            return result.ToString();
+        }
+
+        [HttpGet("/read")]
+        public async Task<string> Read()
+        {
+            var result = await _db.StringGetAsync("time");
+            return result.ToString();
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/Redis/RedisDemo01/Startup.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisDemo01
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IConnectionMultiplexer, ConnectionMultiplexer>(pro => ConnectionMultiplexer.Connect("127.0.0.1:6379"));
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RedisDemo01", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RedisDemo01 v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
