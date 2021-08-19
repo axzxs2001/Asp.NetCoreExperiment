70ca597ba1a68db38ae2f8445f0adb87e0519c6d
桂素伟 <axzxs2001@163.com>
2021-08-19 17:52:24 +0900

    录制回放

---
+++ Asp.NetCoreExperiment/GoReplay/GoReplayDemo01/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoReplayDemo01.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet("/getvalue/{id}")]
+    public async Task<string> Get(int id)
+    {
+        _logger.LogInformation($"X-NSS-UUID:{Request.Headers["X-NSS-UUID"]}");
+        _logger.LogInformation($"{DateTime .Now}---{id.ToString()}");
+     
+        var client = new HttpClient();
+        var content = await client.GetStringAsync("https://www.google.com");
+        return $"{DateTime.Now.ToString()}-{id}-----------{content}";
+    }
+    [HttpPost("/pay")]
+    public IActionResult Post([FromBody] Pay pay)
+    {
+        _logger.LogInformation($"X-NSS-UUID:{Request.Headers["X-NSS-UUID"]}");
+        _logger.LogInformation(System.Text.Json.JsonSerializer.Serialize(pay));
+        pay.Status = "SUCCESS";
+        return new JsonResult(pay);
+    }
+
+
+}
+
+public class Pay
+{
+    public string Code { get; set; }
+    public int Amount { get; set; }
+
+    public string Status { get; set; }
+}
