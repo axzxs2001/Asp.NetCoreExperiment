93b4cb47967cda132e44886af20b9a98fc7337b3
桂素伟 <axzxs2001@163.com>
2021-08-26 08:38:23 +0900

    graphql

---
+++ Asp.NetCoreExperiment/GraphQL/graphql-dotnet/graphqldotnet_demo01/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "graphqldotnet_demo01", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "graphqldotnet_demo01 v1"));
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ Asp.NetCoreExperiment/GraphQL/graphql-dotnet/graphqldotnet_demo02/Program.cs
@@ -0,0 +1,93 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using GraphQL.SystemTextJson;
+
+
+//await F1();
+//await F2();
+await F2();
+
+
+static async Task F3()
+{
+    var schema = new Schema { Query = new StarWarsQuery() };
+    var json = await schema.ExecuteAsync(_ =>
+    {
+        _.Query = "{ hero { id name } }";
+    });
+
+    Console.WriteLine(json);
+}
+
+static async Task F2()
+{
+    var schema = Schema.For(@"
+type Droid {
+    id: String!
+    name: String!
+  }
+  type Query {
+    hero: Droid
+  }", _ =>
+    {
+        _.Types.Include<Query>();
+    });
+
+    var json = await schema.ExecuteAsync(_ =>
+    {
+        _.Query = "{ hero { id name } }";
+    });
+    Console.WriteLine(json);
+}
+
+static async Task F1()
+{
+    var schema = Schema.For(@"
+      type Query {
+        hello: String
+      }
+    ");
+
+    var json = await schema.ExecuteAsync(_ =>
+    {
+        _.Query = "{ hello }";
+        _.Root = new { Hello = "Hello World!" };
+    });
+
+    Console.WriteLine(json);
+}
+
+public class Droid
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Query
+{
+    [GraphQLMetadata("hero")]
+    public Droid GetHero()
+    {
+        return new Droid { Id = "1", Name = "R2-D2" };
+    }
+}
+
+public class DroidType : ObjectGraphType<Droid>
+{
+    public DroidType()
+    {
+        Field(x => x.Id).Description("The Id of the Droid.");
+        Field(x => x.Name).Description("The name of the Droid.");
+    }
+}
+
+public class StarWarsQuery : ObjectGraphType
+{
+    public StarWarsQuery()
+    {
+        Field<DroidType>(
+          "hero",
+          resolve: context => new Droid { Id = "1", Name = "R2-D2" }
+        );
+    }
+}
