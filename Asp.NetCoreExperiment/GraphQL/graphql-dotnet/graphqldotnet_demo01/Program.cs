c5f3c6042b1aaf5b15393264e9f6a5afed2be7f8
桂素伟 <axzxs2001@163.com>
2021-08-26 09:44:16 +0900

    add core

--- Asp.NetCoreExperiment/GraphQL/graphql-dotnet/graphqldotnet_demo01/Program.cs
+++ Asp.NetCoreExperiment/GraphQL/graphql-dotnet/graphqldotnet_demo01/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c =>
@@ -10,15 +10,21 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new() { Title = "graphqldotnet_demo01", Version = "v1" });
 });
 
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (builder.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "graphqldotnet_demo01 v1"));
 }
+//访问地址：http://localhost:5000/ui/graphiql  
+app.UseGraphQLGraphiQL();
+
+//访问地址：http://localhost:5000/ui/altair
+app.UseGraphQLAltair();
 
 app.UseAuthorization();
 
--- Asp.NetCoreExperiment/GraphQL/graphql-dotnet/graphqldotnet_demo02/Program.cs
+++ Asp.NetCoreExperiment/GraphQL/graphql-dotnet/graphqldotnet_demo02/Program.cs
@@ -2,11 +2,48 @@
 using GraphQL.Types;
 using GraphQL.SystemTextJson;
 
+Console.WriteLine("--------F1()--------");
+await F1();
 
-//await F1();
-//await F2();
+Console.WriteLine("--------F2()--------");
 await F2();
 
+Console.WriteLine("--------F3()--------");
+await F3();
+
+
+Console.WriteLine("--------F4()--------");
+await F4();
+
+static async Task F4()
+{
+    var schema = Schema.For(@"
+          type Droid {
+            id: String!
+            name: String!
+            friend: Character
+          }
+
+          type Character {
+            name: String!
+          }
+
+          type Query {
+            hero: Droid
+          }
+        ", _ =>
+    {
+        _.Types.Include<DroidType1>();
+        _.Types.Include<Query>();
+    });
+
+    var json = await schema.ExecuteAsync(_ =>
+    {
+        _.Query = "{ hero { id name friend { name } } }";
+    });
+
+    Console.WriteLine(json);
+}
 
 static async Task F3()
 {
@@ -81,6 +118,7 @@ public class DroidType : ObjectGraphType<Droid>
     }
 }
 
+
 public class StarWarsQuery : ObjectGraphType
 {
     public StarWarsQuery()
@@ -89,5 +127,23 @@ public class StarWarsQuery : ObjectGraphType
           "hero",
           resolve: context => new Droid { Id = "1", Name = "R2-D2" }
         );
+    }
+}
+
+public class Character
+{
+    public string Name { get; set; }
+}
+[GraphQLMetadata("Droid", IsTypeOf = typeof(Droid))]
+public class DroidType1
+{
+    public string Id(Droid droid) => droid.Id;
+    public string Name(Droid droid) => droid.Name;
+
+    // these two parameters are optional
+    // IResolveFieldContext provides contextual information about the field
+    public Character Friend(IResolveFieldContext context, Droid source)
+    {
+        return new Character { Name = "C3-PO" };
     }
 }
