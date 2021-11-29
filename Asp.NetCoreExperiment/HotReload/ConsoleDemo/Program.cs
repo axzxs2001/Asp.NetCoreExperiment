0ac19f158cad68f508251451a0e2cf5194da340a
素伟 桂 <axzxs2001@163.com>
2021-11-29 10:55:05 +0900

    执重载测试

---
+++ Asp.NetCoreExperiment/HotReload/ConsoleDemo/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+Console.WriteLine("回车继续：");
+Console.ReadLine();
+File.AppendAllLines(@"C:\NetStars\GSWA.txt", new string[] { $"0{ DateTime.Now.ToString()}" });
+
+Console.ReadLine();
+File.AppendAllLines(@"C:\NetStars\GSWA.txt", new string[] { $"1-{ DateTime.Now.ToString()}" });
+
---
+++ Asp.NetCoreExperiment/HotReload/WinFormsDemo/Program.cs
@@ -0,0 +1,15 @@
+namespace WinFormsDemo
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
