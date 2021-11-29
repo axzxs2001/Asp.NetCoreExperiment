03fe108cf849da5ca00833dda426a2668e57b4a2
桂素伟 <axzxs2001@163.com>
2021-11-29 22:14:53 +0900

    完成组装graphql

--- Asp.NetCoreExperiment/GraphQL/conf2021/GraphQLClient/Program.cs
+++ Asp.NetCoreExperiment/GraphQL/conf2021/GraphQLClient/Program.cs
@@ -1,16 +1,17 @@
 ﻿
-Console.WriteLine("Hello, World!");
-var client = new HttpClient();
-var content = new StringContent(@"{
-  examPaper{
-    title,
-    totalScore,
-    questionCount
-  }
-}");
+using System.Text.RegularExpressions;
 
-//content.Headers.Clear();
-//content.Headers.Add("Content-Type", "application/json");
+
+Console.WriteLine("Graphql数据：");
+var client = new HttpClient();
+var content = new StringContent(@"{""query"":""{examPaper{ title,totalScore,questionCount}}""}");
+
+content.Headers.Clear();
+content.Headers.Add("Content-Type", "application/json;charset=utf-8");
 var response = await client.PostAsync("http://localhost:5000/graphql/", content);
-var backContent = await response.Content.ReadAsStringAsync();
-Console.WriteLine(backContent);
+
+var backContent= await response.Content.ReadAsStringAsync();
+
+Console.WriteLine(Regex.Unescape(backContent));
+
+
