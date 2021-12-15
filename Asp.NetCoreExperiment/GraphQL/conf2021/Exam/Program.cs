05832a5969f25654027d29f557c0acd336ebdea3
素伟 桂 <axzxs2001@163.com>
2021-12-15 17:35:54 +0900

    升级版本

--- Asp.NetCoreExperiment/GraphQL/conf2021/Exam/Program.cs
+++ Asp.NetCoreExperiment/GraphQL/conf2021/Exam/Program.cs
@@ -3,15 +3,13 @@ using Exam.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 builder.Services.AddDbContext<ExamContext>(options =>
       options.UseSqlServer(builder.Configuration.GetConnectionString("ExamDatabase")));
 
+//==========
 builder.Services 
     .AddGraphQLServer()  
     .AddQueryType<Query>()  
@@ -26,18 +24,17 @@ builder.Services
     .AddProjections()
     .AddFiltering()
     .AddSorting();
+//==========
 
 var app = builder.Build();
-
-
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
 app.UseAuthorization();
-
 app.MapControllers();
+//==========
 app.MapGraphQL("/graphql");
+//==========
 app.Run();
