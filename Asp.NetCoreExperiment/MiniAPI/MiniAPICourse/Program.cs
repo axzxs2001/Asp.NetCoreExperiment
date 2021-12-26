af1dbfc89d91a64f9fa1757699954dc650991ff1
桂素伟 <axzxs2001@163.com>
2021-12-26 21:12:26 +0900

    options完民

--- Asp.NetCoreExperiment/MiniAPI/MiniAPICourse/Program.cs
+++ Asp.NetCoreExperiment/MiniAPI/MiniAPICourse/Program.cs
@@ -1,68 +1,128 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Options;
 using MiniAPICourse;
 using MiniAPICourse.Models;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
+using System.Text.RegularExpressions;
 
 var builder = WebApplication.CreateBuilder();
 
 
-builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
-{
-    config.AddIniFile("myconfig.ini",
-                       optional: true,
-                       reloadOnChange: true);
-});
-//ini取值
-Console.WriteLine(builder.Configuration.GetSection("iniconfig:key1").Value);
 
-//取值
-Console.WriteLine(builder.Configuration.GetSection("Setting:Url").Value);
+//builder.Services.AddDbContext<ExamContext>(options =>
+//  options.UseSqlServer(builder.Configuration.GetConnectionString("ExamDatabase")));
 
-//绑定
-var setting = new Setting1();
-builder.Configuration.GetSection("Setting").Bind(setting);
-Console.WriteLine(setting);
-
-//record不可以用在Snapshot中
-builder.Services.Configure<Setting>(builder.Configuration.GetSection("Setting"));
+//builder.Services.PostConfigure<AppInfo>(options =>
+//{
+//    options.Name = "新名称";
+//});
 
 
-builder.Services.AddDbContext<ExamContext>(options =>
-      options.UseSqlServer(builder.Configuration.GetConnectionString("ExamDatabase")));
+builder.Services.Configure<RedisSetting>(builder.Configuration.GetSection("RedisSetting"));
+builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IValidateOptions
+                            <RedisSetting>, RedisSettingValidation>());
+
+
+//builder.Services.AddOptions<RedisSetting>()
+//          .Bind(builder.Configuration.GetSection("RedisSetting"))
+//          .ValidateDataAnnotations()
+//          .Validate(config =>
+//          {
+//              if (config.Port < 1000)
+//              {
+//                  return false;
+//              }
+
+//              return true;
+//          }, "端口不能少于1000"); 
+
+
+//builder.Services.Configure<RedisSetting>(RedisSetting.Main,
+//                                   builder.Configuration.GetSection("RedisSettings:Main"));
+//builder.Services.Configure<RedisSetting>(RedisSetting.Prepare,
+//                                    builder.Configuration.GetSection("RedisSettings:Prepare"));
+
 
 var app = builder.Build();
 
-app.MapGet("/config", (IConfiguration config) =>
+app.MapGet("/redissetting", (IOptions<RedisSetting> options) =>
 {
-    return config.GetSection("Setting:Method").Value;
+    try
+    {
+        return options.Value.ToString();
+    }
+    catch (OptionsValidationException exc)
+    {
+        return exc.Message;
+    }
 });
 
-app.MapGet("/snapshot", (IOptionsSnapshot<Setting> options) =>
+app.MapGet("/snapshotredissetting", (IOptionsSnapshot<RedisSetting> options) =>
 {
     return options.Value;
 });
-app.MapGet("/monitorstart", (IOptionsMonitor<Setting> options) =>
+app.MapGet("/monitorstart", (IOptionsMonitor<RedisSetting> options) =>
 {
-    options.OnChange(seting =>
-    {
-        Console.WriteLine(seting.Url);
-    });
+    options.OnChange(redisSetting =>
+   {
+       app.Logger.LogInformation(options.CurrentValue.ToString());
+   });
     return options.CurrentValue;
 });
 
 app.Run();
 
-
-public record Setting
+public record RedisSetting
 {
-    public string? Url { get; set; }
-    public string? TimeOut { get; set; }
-    public string? Method { set; get; }
+    public string? Host { get; set; }
+    public int Port { get; set; }
+    public string? Password { get; set; }
+    [RegularExpression(@"^\d+ms$", ErrorMessage = "格式不正确，必须是ms")]
+    public string? ConnectionTimeOut { get; set; }
 }
 
-record Setting1(string? Url = null, string? TimeOut = null, string? Method = null);
+//public record RedisSetting
+//{
+//    public const string Main = "Main";
+//    public const string Prepare = "Prepare";
+//    public string? Host { get; set; }
+//    public int Port { get; set; }
+//    public string? Password { get; set; }
+//    public string? ConnectionTimeOut { get; set; }
+//}
+
+
+public class RedisSettingValidation : IValidateOptions<RedisSetting>
+{
+    public RedisSetting _config { get; init; }
+    public RedisSettingValidation(IConfiguration config)
+    {
+        _config = config.GetSection("RedisSetting")
+            .Get<RedisSetting>();
+    }
+    public ValidateOptionsResult Validate(string name, RedisSetting options)
+    {
+        string? vor=null;
+        var rx = new Regex(@"^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$");
+        if (options != null&&options.Host!=null)
+        {
+            var match = rx.Match(options.Host);
+            if (string.IsNullOrEmpty(match.Value))
+            {
+                vor = $"{options.Host} 格式不正确";
+            }
+
+            if (vor != null)
+            {
+                return ValidateOptionsResult.Fail(vor);
+            }
+        }
+        return ValidateOptionsResult.Success;
+    }
+}
