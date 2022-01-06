8d49575a2175366e96c37922afe6509a1aa9e308
素伟 桂 <axzxs2001@163.com>
2022-01-06 18:53:11 +0900

    DI demo

--- Asp.NetCoreExperiment/MiniAPI/MiniAPICourse/Program.cs
+++ Asp.NetCoreExperiment/MiniAPI/MiniAPICourse/Program.cs
@@ -1,128 +1,77 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.Options;
-using MiniAPICourse;
-using MiniAPICourse.Models;
-using System.ComponentModel.DataAnnotations;
-using System.Net.Mime;
-using System.Text;
-using System.Text.Json;
-using System.Text.RegularExpressions;
+﻿var builder = WebApplication.CreateBuilder();
 
-var builder = WebApplication.CreateBuilder();
-
-
-
-//builder.Services.AddDbContext<ExamContext>(options =>
-//  options.UseSqlServer(builder.Configuration.GetConnectionString("ExamDatabase")));
-
-//builder.Services.PostConfigure<AppInfo>(options =>
-//{
-//    options.Name = "新名称";
-//});
-
-
-builder.Services.Configure<RedisSetting>(builder.Configuration.GetSection("RedisSetting"));
-builder.Services.TryAddEnumerable(ServiceDescriptor.Singleton<IValidateOptions
-                            <RedisSetting>, RedisSettingValidation>());
-
-
-//builder.Services.AddOptions<RedisSetting>()
-//          .Bind(builder.Configuration.GetSection("RedisSetting"))
-//          .ValidateDataAnnotations()
-//          .Validate(config =>
-//          {
-//              if (config.Port < 1000)
-//              {
-//                  return false;
-//              }
-
-//              return true;
-//          }, "端口不能少于1000"); 
-
-
-//builder.Services.Configure<RedisSetting>(RedisSetting.Main,
-//                                   builder.Configuration.GetSection("RedisSettings:Main"));
-//builder.Services.Configure<RedisSetting>(RedisSetting.Prepare,
-//                                    builder.Configuration.GetSection("RedisSettings:Prepare"));
-
-
+builder.Services.AddScoped<IScopedService, ScopedService>();
+builder.Services.AddTransient<ITransientService, TransientService>();
+builder.Services.AddSingleton<ISingletonService, SingletonService>();
 var app = builder.Build();
 
-app.MapGet("/redissetting", (IOptions<RedisSetting> options) =>
+app.Use(async (context, next) =>
 {
-    try
+    if (context.Request.Path.HasValue)
     {
-        return options.Value.ToString();
+        switch (context.Request.Path.Value)
+        {
+            case string s when s.Contains("transient"):
+                var transientService = context.RequestServices.GetService<ITransientService>();
+                Console.WriteLine($"--------------{transientService?.Call()}");
+                break;
+            case string s when s.Contains("scoped"):
+                var scopedService = context.RequestServices.GetService<IScopedService>();
+                Console.WriteLine($"--------------{scopedService?.Call()}");
+                break;
+            case string s when s.Contains("singleton"):
+                var singletonService = context.RequestServices.GetService<ISingletonService>();
+                Console.WriteLine($"--------------{singletonService?.Call()}");
+                break;
+        }
     }
-    catch (OptionsValidationException exc)
-    {
-        return exc.Message;
-    }
+    await next.Invoke();
 });
 
-app.MapGet("/snapshotredissetting", (IOptionsSnapshot<RedisSetting> options) =>
-{
-    return options.Value;
-});
-app.MapGet("/monitorstart", (IOptionsMonitor<RedisSetting> options) =>
-{
-    options.OnChange(redisSetting =>
-   {
-       app.Logger.LogInformation(options.CurrentValue.ToString());
-   });
-    return options.CurrentValue;
-});
+app.MapGet("/transient", (ITransientService transientService) => transientService.Call());
+
+app.MapGet("/scoped", (IScopedService scopedService) => scopedService.Call());
+
+app.MapGet("/singleton", (ISingletonService singletonService) => singletonService.Call());
 
 app.Run();
 
-public record RedisSetting
+
+public interface ITransientService
 {
-    public string? Host { get; set; }
-    public int Port { get; set; }
-    public string? Password { get; set; }
-    [RegularExpression(@"^\d+ms$", ErrorMessage = "格式不正确，必须是ms")]
-    public string? ConnectionTimeOut { get; set; }
+    string Call();
+}
+public class TransientService : ITransientService
+{
+    public DateTime Time { get; init; } = DateTime.Now;
+    public string Call()
+    {
+        return $"TransientService {Time.ToString("yyyy-MM-dd HH:mm:ss.fffffff")} test……";
+    }
 }
 
-//public record RedisSetting
-//{
-//    public const string Main = "Main";
-//    public const string Prepare = "Prepare";
-//    public string? Host { get; set; }
-//    public int Port { get; set; }
-//    public string? Password { get; set; }
-//    public string? ConnectionTimeOut { get; set; }
-//}
-
-
-public class RedisSettingValidation : IValidateOptions<RedisSetting>
+public interface IScopedService
 {
-    public RedisSetting _config { get; init; }
-    public RedisSettingValidation(IConfiguration config)
+    string Call();
+}
+public class ScopedService : IScopedService
+{
+    public DateTime Time { get; init; } = DateTime.Now;
+    public string Call()
     {
-        _config = config.GetSection("RedisSetting")
-            .Get<RedisSetting>();
+        return $"ScopedService {Time.ToString("yyyy-MM-dd HH:mm:ss.fffffff")} test……";      
     }
-    public ValidateOptionsResult Validate(string name, RedisSetting options)
-    {
-        string? vor=null;
-        var rx = new Regex(@"^((25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)$");
-        if (options != null&&options.Host!=null)
-        {
-            var match = rx.Match(options.Host);
-            if (string.IsNullOrEmpty(match.Value))
-            {
-                vor = $"{options.Host} 格式不正确";
-            }
+}
 
-            if (vor != null)
-            {
-                return ValidateOptionsResult.Fail(vor);
-            }
-        }
-        return ValidateOptionsResult.Success;
+public interface ISingletonService
+{
+    string Call();
+}
+public class SingletonService : ISingletonService
+{
+    public DateTime Time { get; init; } = DateTime.Now;
+    public string Call()
+    {
+        return $"TSingletonService {Time.ToString("yyyy-MM-dd HH:mm:ss.fffffff")} test……";       
     }
 }
