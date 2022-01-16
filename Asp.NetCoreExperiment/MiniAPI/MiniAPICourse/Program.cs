617e2d43810b379b0e216e15eaffaf43045c1131
桂素伟 <axzxs2001@163.com>
2022-01-16 21:06:23 +0900

    身份认证和授权

--- Asp.NetCoreExperiment/MiniAPI/MiniAPICourse/Program.cs
+++ Asp.NetCoreExperiment/MiniAPI/MiniAPICourse/Program.cs
@@ -1,40 +1,145 @@
-﻿using MiniAPICourse.Services;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder();
-builder.Services.AddScoped<ILogDemoService, LogDemoService>();
-var app = builder.Build();
 
-app.MapGet("/test", (ILogger<Program> logger, ILogDemoService logDemo) =>
+var jwtConfig = new JWTConfig();
+builder.Configuration.GetSection("JWTConfig").Bind(jwtConfig);
+builder.Services.AddSingleton(jwtConfig);
+
+builder.Services
+    .AddAuthorization()
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, opt =>
+    {
+        opt.RequireHttpsMetadata = false;
+        opt.TokenValidationParameters = JwtToken.CreateTokenValidationParameters(jwtConfig);
+    });
+
+var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/hellosystem", (ILogger<Program> logger, HttpContext context) =>
 {
-    logger.LogInformation("test");
-    logDemo.Demo01();
-    return "ok";
+    var message = $"hello,system,{context.User?.Identity?.Name}";
+    logger.LogInformation(message);
+
+    return message;
+}).RequireAuthorization(new RoleData { Roles = "system" });
+
+app.MapGet("/helloadmin", (ILogger<Program> logger, HttpContext context) =>
+{
+    var message = $"hello,admin,{context.User?.Identity?.Name}";
+    logger.LogInformation(message);
+    return message;
+}).RequireAuthorization(new RoleData { Roles = "admin" });
+
+app.MapGet("/helloall", (ILogger<Program> logger, HttpContext context) =>
+{
+    var message = $"hello,all roles,{context.User?.Identity?.Name}";
+    logger.LogInformation(message);
+    return message;
+}).RequireAuthorization(new RoleData { Roles = "admin,system" });
+
+
+
+app.MapPost("/login", [AllowAnonymous] (ILogger<Program> logger, LoginModel login, JWTConfig jwtConfig) =>
+{
+    logger.LogInformation("login");
+    if (login.UserName == "gsw" && login.Password == "111111")
+    {
+        var now = DateTime.UtcNow;
+
+        var claims = new Claim[] {
+                new Claim(ClaimTypes.Role, "admin"),
+                new Claim(ClaimTypes.Name, "桂素伟"),
+                new Claim(ClaimTypes.Sid, login.UserName),
+                new Claim(ClaimTypes.Expiration, now.AddSeconds(jwtConfig.Expires).ToString())
+                };
+        var token = JwtToken.BuildJwtToken(claims, jwtConfig);
+        return token;
+    }
+    else
+    {
+        return "username or password is error";
+    }
 });
 
 app.Run();
 
-namespace MiniAPICourse.Services
+public class LoginModel
 {
-    public interface ILogDemoService
+    public string? UserName { get; set; }
+    public string? Password { get; set; }
+}
+public class JWTConfig
+{
+    public string? Secret { get; set; }
+    public string? Issuer { get; set; }
+    public string? Audience { get; set; }
+    public int Expires { get; set; }
+}
+public class JwtToken
+{
+    public static dynamic BuildJwtToken(Claim[] claims, JWTConfig jwtConfig)
     {
-        void Demo01();
-    }
-    public class LogDemoService : ILogDemoService
-    {
-        private readonly ILogger<LogDemoService> _logger;
-        public LogDemoService(ILogger<LogDemoService> logger)
+        var now = DateTime.UtcNow;
+        var jwt = new JwtSecurityToken(
+            issuer: jwtConfig.Issuer,
+            audience: jwtConfig.Audience,
+            claims: claims,
+            notBefore: now,
+            expires: now.AddSeconds(jwtConfig.Expires),
+            signingCredentials: GetSigningCredentials(jwtConfig)
+        );
+        var encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+        var response = new
         {
-            _logger = logger;
-        }
+            Status = true,
+            AccessToken = encodedJwt,
+            ExpiresIn = now.AddSeconds(jwtConfig.Expires),
+            TokenType = "Bearer"
+        };
+        return response;
+    }
 
-        public void Demo01()
-        {
-            _logger.LogTrace("Trace");
-            _logger.LogDebug("Debug");
-            _logger.LogInformation("Information");
-            _logger.LogWarning("Warning");
-            _logger.LogError("Error");
-            _logger.LogCritical("Critical");
-        }
+    static SigningCredentials GetSigningCredentials(JWTConfig jwtConfig)
+    {
+        var keyByteArray = Encoding.ASCII.GetBytes(jwtConfig?.Secret!);
+        var signingKey = new SymmetricSecurityKey(keyByteArray);
+        return new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256);
     }
+    public static TokenValidationParameters CreateTokenValidationParameters(JWTConfig jwtConfig)
+    {
+        var keyByteArray = Encoding.ASCII.GetBytes(jwtConfig?.Secret!);
+        var signingKey = new SymmetricSecurityKey(keyByteArray);
+        return new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = signingKey,
+            ValidateIssuer = true,
+            ValidIssuer = jwtConfig?.Issuer,
+            ValidateAudience = true,
+            ValidAudience = jwtConfig?.Audience,         
+            ClockSkew = TimeSpan.Zero,
+            RequireExpirationTime = true,
+        };
+    }
+}
+public class RoleData : IAuthorizeData
+{
+    public string? Policy { get; set; }
+    public string? Roles { get; set; }
+    public string? AuthenticationSchemes { get; set; }
+
 }
