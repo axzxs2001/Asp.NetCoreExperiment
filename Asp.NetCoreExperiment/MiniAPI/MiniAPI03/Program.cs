714deaae9e895d771b53a062d73553b7582ff2a3
素伟 桂 <axzxs2001@163.com>
2022-01-17 16:52:44 +0900

    多语言

---
+++ Asp.NetCoreExperiment/MiniAPI/MiniAPI03/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.Extensions.Localization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+
+var app = builder.Build();
+
+var supportedCultures = new[] { "zh-CN", "ja-JP", "en-US" };
+var localizationOptions = new RequestLocalizationOptions()
+    .SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures)
+    .AddSupportedUICultures(supportedCultures);
+localizationOptions.ApplyCurrentCultureToResponseHeaders = true;
+app.UseRequestLocalization(localizationOptions);
+
+
+app.MapGet("/demo", (IStringLocalizer<SharedResource> sharedLocalizer) =>
+{
+    return sharedLocalizer["ok"].Value;
+});
+
+app.Run();
+
+
+
+public class SharedResource
+{
+}
