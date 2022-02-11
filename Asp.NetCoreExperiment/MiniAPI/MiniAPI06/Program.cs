13654fbda355c692479a59c58982eeea9a58fb96
桂素伟 <axzxs2001@163.com>
2022-02-11 09:46:45 +0900

    数据保存完成

--- Asp.NetCoreExperiment/MiniAPI/MiniAPI06/Program.cs
+++ Asp.NetCoreExperiment/MiniAPI/MiniAPI06/Program.cs
@@ -1,24 +1,39 @@
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDataProtection();
+builder.Services.AddDbContext<DataProtContext>(options =>
+      options.UseSqlServer(builder.Configuration.GetConnectionString("DataProtDB")));
+//通过SetDefaultKeyLifetime更改默认值90天
+builder.Services.AddDataProtection().SetDefaultKeyLifetime(TimeSpan.FromDays(10)).PersistKeysToDbContext<DataProtContext>();
 var app = builder.Build();
 
 
-app.MapGet("/test", (IDataProtectionProvider provider,ILogger<Program> logger) =>
+app.MapGet("/encrypt/{str}", (IDataProtectionProvider provider, ILogger<Program> logger, string str) =>
 {
     var protector = provider.CreateProtector("a.b.c");
-    var protectString = protector.Protect("gsw");
-    logger.LogInformation(protectString);
-    logger.LogInformation(protector.Unprotect(protectString));
-    return "ok";
+    var sec = protector.Protect(str);
+    logger.LogInformation(sec);
+    return "加密：" + sec;
 });
-app.MapGet("/test/{sec}", (IDataProtectionProvider provider, ILogger<Program> logger,string  sec) =>
+app.MapGet("/decrypt/{sec}", (IDataProtectionProvider provider, ILogger<Program> logger, string sec) =>
 {
-    var protector = provider.CreateProtector("a.b.c");    
-    logger.LogInformation(protector.Unprotect(sec));
-    return "ok";
+    var protector = provider.CreateProtector("a.b.c");
+    var str = protector.Unprotect(sec);
+    logger.LogInformation(str);
+    return "解密：" + str;
 });
 
 app.Run();
+
+
+class DataProtContext : DbContext, IDataProtectionKeyContext
+{
+    public DbSet<DataProtectionKey> DataProtectionKeys { get; set; }
+
+    public DataProtContext(DbContextOptions<DataProtContext> options)
+          : base(options)
+    {}
+}
