417cc3748257dc753f76f032de2c079787189051
桂素伟 <axzxs2001@163.com>
2022-03-26 11:33:06 +0900

    Span<T>

---
+++ Asp.NetCoreExperiment/CSharp/SpanDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿
+var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+//方法二
+for (var i = 0; i < arr.Length; i++)
+{
+    arr[i] += 1;
+}
+Console .WriteLine(string.Join(',',arr));
+//方法三
+foreach (ref var i in arr.AsSpan())
+{
+    i++;
+}
+Console.WriteLine(string.Join(',', arr));
+//using BenchmarkDotNet.Attributes;
+//using BenchmarkDotNet.Running;
+
+//BenchmarkRunner.Run<TestSpan>();
+
+//public class TestSpan
+//{
+
+//    [Benchmark]
+//    public void Demo1()
+//    {
+//        var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//        foreach (ref var i in arr.AsSpan())
+//        {
+//            i++;
+//        }
+//    }
+
+//    [Benchmark]
+//    public void Demo2()
+//    {
+//        var arr = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//        for (var i = 0; i < arr.Length; i++)
+//        {
+//            arr[i] += 1;
+//        }
+//    }
+//}
