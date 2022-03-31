cba32ffce0d682d4a0eb7ba7fcb1d3a239f582e2
素伟 桂 <axzxs2001@163.com>
2022-03-31 10:45:28 +0900

    jeager完成demo

---
+++ Asp.NetCoreExperiment/MiniAPI/MiniAPI18_Jeager/Program.cs
@@ -0,0 +1,50 @@
+using Jaeger;
+using OpenTracing.Propagation;
+using OpenTracing;
+using OpenTracing.Util;
+using Jaeger.Samplers;
+using Jaeger.Senders.Thrift;
+using Jaeger.Senders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+
+builder.Services.AddOpenTracing();
+
+builder.Services.AddSingleton<ITracer>(serviceProvider =>
+{
+    var serviceName = serviceProvider.GetRequiredService<IWebHostEnvironment>().ApplicationName;
+    var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+
+    Jaeger.Configuration.SenderConfiguration.DefaultSenderResolver = new SenderResolver(loggerFactory)
+        .RegisterSenderFactory<ThriftSenderFactory>();
+
+
+    var tracer = new Tracer.Builder(serviceName)
+        .WithLoggerFactory(loggerFactory)
+        .WithSampler(new ConstSampler(true))
+        .Build();
+
+    GlobalTracer.Register(tracer);
+
+    return tracer;
+});
+
+var app = builder.Build();
+
+
+
+app.MapGet("/order", async (ILogger<Program> logger, IHttpClientFactory clientFactory, ITracer tracer) =>
+{
+    using (var scope = tracer.BuildSpan("订单系统").StartActive(true))
+    {
+        logger.LogInformation("订单查询库存");
+        var client = clientFactory.CreateClient();
+        var content = await client.GetStringAsync("http://localhost:5160/stock/D0001");
+        return $"stock调用结果：{content}";
+    }
+});
+
+app.Run();
+
---
+++ Asp.NetCoreExperiment/MiniAPI/MiniAPI18_Jeager_API/Program.cs
@@ -0,0 +1,45 @@
+using Jaeger;
+using Jaeger.Samplers;
+using Jaeger.Senders;
+using Jaeger.Senders.Thrift;
+using OpenTracing;
+using OpenTracing.Util;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddOpenTracing();
+
+builder.Services.AddSingleton<ITracer>(serviceProvider =>
+{
+    var serviceName = serviceProvider.GetRequiredService<IWebHostEnvironment>().ApplicationName;
+    var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+    Jaeger.Configuration.SenderConfiguration.DefaultSenderResolver = new SenderResolver(loggerFactory)
+        .RegisterSenderFactory<ThriftSenderFactory>();
+
+    var tracer = new Tracer.Builder(serviceName)
+        .WithLoggerFactory(loggerFactory)
+        .WithSampler(new ConstSampler(true))
+        .Build();
+
+    GlobalTracer.Register(tracer);
+    return tracer;
+});
+var app = builder.Build();
+
+app.MapGet("/stock/{no}", (string no,ITracer tracer) =>
+{
+    using (var scope = tracer.BuildSpan("¿â´æÏµÍ³").StartActive(true))
+    {
+        return new Product { No = no, Quantity = 1324, Name = "Surface Go 3" };
+    }
+});
+
+app.Run();
+
+public class Product
+{
+    public string? No { get; set; }
+    public string? Name { get; set; }
+    public int Quantity { get; set; }
+}
