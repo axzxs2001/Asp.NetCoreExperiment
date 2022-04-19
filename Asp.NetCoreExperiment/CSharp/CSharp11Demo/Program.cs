d3fffc11ec0e40ac238d18b1f7a90ffcb248d1bc
桂素伟 <axzxs2001@163.com>
2022-04-19 12:22:04 +0900

    CSharp11

---
+++ Asp.NetCoreExperiment/CSharp/CSharp11Demo/Program.cs
@@ -0,0 +1,26 @@
+﻿//原始字符串
+var firstName = "桂";
+var lastName = "素伟";
+var json0 =
+    $$"""
+    {
+         "firstName": "{{firstName}}",
+         "lastName": "{{lastName}}",
+         "sex": "male",
+         "age": 15         
+     }
+    """;
+Console.WriteLine(json0);
+
+
+var json1 =
+    $$"""
+    <?xml version="1.0"?>
+    <Person>
+      <FirstName>{{firstName}}</FirstName>
+      <LastName>{{lastName}}</LastName>
+      <Sex>male</Sex>
+      <Age>15</Age>
+    </Person>
+    """;
+Console.WriteLine(json1);
