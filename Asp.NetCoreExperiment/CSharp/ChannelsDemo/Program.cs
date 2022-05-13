9dceb4edda686164886dbfba83823dfabd5572d4
素伟 桂 <axzxs2001@163.com>
2022-05-13 16:15:59 +0900

    .NET的Channel Demo

---
+++ Asp.NetCoreExperiment/CSharp/ChannelsDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading.Channels;
+
+var chanel = Channel.CreateBounded<Data>(8);
+
+while (true)
+{
+    chanel.Writer.TryWrite(new Data() { ID = 1, Name = "abcde", CreateOn = DateTime.Now });
+    chanel.Writer.TryWrite(new Data() { ID = 2, Name = "ddddd", CreateOn = DateTime.Now });
+
+    var result = chanel.Reader.TryRead(out Data? data);
+    do
+    {
+        Console.WriteLine(data?.CreateOn.Second);
+        Console.WriteLine(data?.CreateOn.Millisecond);
+        Console.WriteLine(data?.CreateOn.Microsecond);
+        Console.WriteLine(data?.CreateOn.Nanosecond);
+        Console.WriteLine(data);
+        result = chanel.Reader.TryRead(out data);
+
+    } while (result);
+    Console.ReadLine();
+}
+
+record Data
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public DateTime CreateOn { get; set; }
+}
