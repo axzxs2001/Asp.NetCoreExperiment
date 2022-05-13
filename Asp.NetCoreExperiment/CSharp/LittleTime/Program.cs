c411fc46a64b9f07a5997a54965c3471414d74ce
素伟 桂 <axzxs2001@163.com>
2022-05-13 09:59:14 +0900

    时间demo

---
+++ Asp.NetCoreExperiment/CSharp/LittleTime/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LittleTime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {                
+                var time = DateTime.Now;
+                Console.WriteLine(time);
+                Console.WriteLine(time.Kind);
+                Console.WriteLine(time.Year);
+                Console.WriteLine(time.Month);
+                Console.WriteLine(time.Day);
+                Console.WriteLine(time.Hour);
+                Console.WriteLine(time.Minute);
+                Console.WriteLine(time.Second);
+                Console.WriteLine(time.Millisecond);
+                Console.WriteLine(time.Microsecond);
+                Console.WriteLine(time.Nanosecond);
+                Console.WriteLine(time.Ticks);
+                Console.ReadLine();
+            }
+        }
+    }
+}
