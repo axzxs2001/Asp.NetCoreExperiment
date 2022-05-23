00e13a9c241831494da904a90b641502768f2b88
素伟 桂 <axzxs2001@163.com>
2022-05-23 10:40:37 +0900

    CommandLine demo

---
+++ Asp.NetCoreExperiment/Command/CommandDemo01/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.CommandLine;
+
+
+await Command01Async(args);
+
+static async Task Command01Async(string[] args)
+{
+    var rootCommand = new RootCommand("这是一个命令行工具：旦猫");
+    rootCommand.SetHandler(() =>
+    {
+        Console.WriteLine("欢迎使用《旦猫》");
+    });
+
+    var sub1Command = new Command("show", "显示一些信息");
+    rootCommand.Add(sub1Command);
+    sub1Command.SetHandler(() =>
+    {
+        Console.WriteLine("这是《旦猫》的show命令");
+    });
+
+    await rootCommand.InvokeAsync(args);
+}
