e8801295d4ba71cdc6eb32dbc5178e8f8a928b00
素伟 桂 <axzxs2001@163.com>
2022-06-28 12:01:22 +0900

    demo

--- Asp.NetCoreExperiment/Command/CSRebot/Program.cs
+++ Asp.NetCoreExperiment/Command/CSRebot/Program.cs
@@ -4,10 +4,17 @@ using System.CommandLine.Binding;
 using System.Data;
 using System.Text;
 using System;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Net.WebRequestMethods;
+using System.Reflection.Metadata;
+using File = System.IO.File;
+using MySql.Data.MySqlClient;
+using Npgsql;
+using NpgsqlTypes;
 
 
-//dotnet run dbto -l C# -constr 'server=localhost;database=mqpay;uid=sa;pwd=sa;encrypt=true;trustservercertificate=true' -t mssql
-
+//dotnet run dbto -l C# -constr 'server=localhost;database=abcd;uid=sa;pwd=sa;encrypt=true;trustservercertificate=true' -t mssql
+//dotnet run dbto -l C# -constr 'server=localhost;database=abcd;uid=root;pwd=mars2020;' -t mysql
 //完成后，选中项目并打包，然后进入项目所有的文件夹执行工具安装命令
 //dotnet tool install -g --add-source ./nupkg CSRebot
 //创建根命令
@@ -91,7 +98,12 @@ static async Task DBToCSharpAsync(string connectionString, string dbType)
         case "mssql":
             await MSSQLToCSharpAsync(connectionString);
             break;
-
+        case "mysql":
+            await MySQLToCSharpAsync(connectionString);
+            break;
+        case "pgsql":
+            await PgSQLToCSharpAsync(connectionString);
+            break;
         default:
             break;
     }
@@ -117,13 +129,24 @@ static async Task MSSQLToCSharpAsync(string connectionString)
     }
     foreach (var tableName in tableNames)
     {
-        using var fileCmd = new SqlCommand("SELECT syscolumns.name,systypes.name as typename FROM syscolumns, systypes WHERE syscolumns.xusertype = systypes.xusertype AND syscolumns.id = object_id(@tablename)", con);
+        using var fileCmd = new SqlCommand(@"
+select c.name,col.DATA_TYPE as typename,com.value as comment,col.IS_NULLABLE as isnull
+from sys.tables t 
+join sys.columns c 
+on t.object_id=c.object_id
+join INFORMATION_SCHEMA.COLUMNS col 
+on col.column_name=c.name
+left join sys.extended_properties com
+on com.major_id=c.object_id and com.minor_id=c.column_id
+where t.name=@tablename and col.TABLE_NAME=@tablename 
+
+", con);
         fileCmd.Parameters.Add("tablename", SqlDbType.VarChar).Value = tableName;
         using var fileReader = await fileCmd.ExecuteReaderAsync();
         var tablefieldses = new List<dynamic>();
         while (fileReader.Read())
         {
-            tablefieldses.Add(new { name = fileReader.GetString(0), typename = fileReader.GetString(1) });
+            tablefieldses.Add(new { name = fileReader.GetString(0), typename = fileReader.GetString(1), comment = fileReader.IsDBNull(2) ? null : fileReader.GetString(2), isnull = fileReader.GetString(3) });
         }
         fileReader.Close();
         await fileReader.DisposeAsync();
@@ -135,7 +158,134 @@ static async Task MSSQLToCSharpAsync(string connectionString)
         {
             var typeName = TypeMap.MSSQLToCSharp[tablefields.typename];
             var name = tablefields.name;
-            csBuilder.AppendLine($"   public {typeName} {name}{{get;set;}}");
+            if (!string.IsNullOrWhiteSpace(tablefields.comment))
+            {
+                csBuilder.AppendLine(@$"
+   /// <summary>
+   /// {tablefields.comment}
+   /// </summary>");
+            }
+            csBuilder.AppendLine($"   public {(typeName == "string" && tablefields.isnull == "YES" ? "string?" : typeName)} {name}{{get;set;}}");
+        }
+        csBuilder.AppendLine("}");
+        File.WriteAllText($"{dbPath}\\{tableName}.cs", csBuilder.ToString(), Encoding.UTF8);
+    }
+}
+
+
+static async Task MySQLToCSharpAsync(string connectionString)
+{
+    using var con = new MySqlConnection(connectionString);
+    using var cmd = new MySqlCommand("SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA=@databasename;", con);
+    cmd.Parameters.Add("databasename", MySqlDbType.VarChar).Value = con.Database;
+    await con.OpenAsync();
+    using var reader = await cmd.ExecuteReaderAsync();
+    var tableNames = new List<string>();
+    while (reader.Read())
+    {
+        tableNames.Add(reader.GetString(0));
+    }
+    await reader.CloseAsync();
+
+    var dbPath = $"{Environment.CurrentDirectory}\\{con.Database}";
+    if (!Directory.Exists(dbPath))
+    {
+        Directory.CreateDirectory(dbPath);
+    }
+    foreach (var tableName in tableNames)
+    {
+        using var fileCmd = new MySqlCommand("select column_name as name,data_type as typename,column_comment as comment,is_nullable as isnull from information_schema.columns where table_name=@tablename", con);
+        fileCmd.Parameters.Add("tablename", MySqlDbType.VarChar).Value = tableName;
+        using var fileReader = await fileCmd.ExecuteReaderAsync();
+        var tablefieldses = new List<dynamic>();
+        while (fileReader.Read())
+        {
+            tablefieldses.Add(new { name = fileReader.GetString(0), typename = fileReader.GetString(1), comment = fileReader.IsDBNull(2) ? null : fileReader.GetString(2), isnull = fileReader.GetString(3) });
+        }
+        fileReader.Close();
+        await fileReader.DisposeAsync();
+
+        var csBuilder = new StringBuilder();
+        csBuilder.AppendLine($"public class {tableName}");
+        csBuilder.AppendLine("{");
+        foreach (var tablefields in tablefieldses)
+        {
+            var typeName = TypeMap.MySQLToCSharp[tablefields.typename];
+            var name = tablefields.name;
+            if (!string.IsNullOrWhiteSpace(tablefields.comment))
+            {
+                csBuilder.AppendLine(@$"
+   /// <summary>
+   /// {tablefields.comment}
+   /// </summary>");
+            }
+            csBuilder.AppendLine($"   public {(typeName == "string" && tablefields.isnull == "YES" ? "string?" : typeName)} {name}{{get;set;}}");
+        }
+        csBuilder.AppendLine("}");
+        File.WriteAllText($"{dbPath}\\{tableName}.cs", csBuilder.ToString(), Encoding.UTF8);
+    }
+}
+
+static async Task PgSQLToCSharpAsync(string connectionString)
+{
+    using var con = new NpgsqlConnection(connectionString);
+    using var cmd = new NpgsqlCommand("select tablename from pg_tables where schemaname='public'", con);
+
+    await con.OpenAsync();
+    using var reader = await cmd.ExecuteReaderAsync();
+    var tableNames = new List<string>();
+    while (reader.Read())
+    {
+        tableNames.Add(reader.GetString(0));
+    }
+    await reader.CloseAsync();
+
+    var dbPath = $"{Environment.CurrentDirectory}\\{con.Database}";
+    if (!Directory.Exists(dbPath))
+    {
+        Directory.CreateDirectory(dbPath);
+    }
+    foreach (var tableName in tableNames)
+    {
+        using var fileCmd = new NpgsqlCommand(@"SELECT	
+	A.attname AS name,
+    T.typname AS typename,
+	col_description (A.attrelid, A.attnum) AS comment,
+	A.attnotnull AS isnull	
+FROM
+	pg_class AS C,
+	pg_attribute AS A,
+	pg_type AS T
+WHERE
+	C.relname = @tablename
+AND A.attrelid = C.oid
+AND A.attnum > 0
+AND A.atttypid=T.oid;", con);
+        fileCmd.Parameters.Add("tablename", NpgsqlDbType.Varchar).Value = tableName;
+        using var fileReader = await fileCmd.ExecuteReaderAsync();
+        var tablefieldses = new List<dynamic>();
+        while (fileReader.Read())
+        {
+            tablefieldses.Add(new { name = fileReader.GetString(0), typename = fileReader.GetString(1), comment = fileReader.GetString(2), isnull = fileReader.GetBoolean(3) });
+        }
+        fileReader.Close();
+        await fileReader.DisposeAsync();
+
+        var csBuilder = new StringBuilder();
+        csBuilder.AppendLine($"public class {tableName}");
+        csBuilder.AppendLine("{");
+        foreach (var tablefields in tablefieldses)
+        {
+            var typeName = TypeMap.PgSQLToCSharp[tablefields.typename];
+            var name = tablefields.name;
+            if (!string.IsNullOrWhiteSpace(tablefields.comment))
+            {
+                csBuilder.AppendLine(@$"
+   /// <summary>
+   /// {tablefields.comment}
+   /// </summary>");
+            }
+            csBuilder.AppendLine($"   public {(typeName == "string" && tablefields.isnull ? "string?" : typeName)} {name}{{get;set;}}");
         }
         csBuilder.AppendLine("}");
         File.WriteAllText($"{dbPath}\\{tableName}.cs", csBuilder.ToString(), Encoding.UTF8);
@@ -180,10 +330,109 @@ static class TypeMap
 
     internal static Dictionary<string, string> MySQLToCSharp => new Dictionary<string, string>
     {
-        {"","" }
+
+        {"bool","bool"},
+        {"boolean","bool" },
+        {"bit","bool" },
+        {"tinyint","sbyte" },
+        {"tinyint unsigned","byte" },
+        {"smallint","short" },
+        {"smallint unsigned","ushort" },
+        {"year","short" },
+        {"int","int" },
+        {"int unsigned","uint" },
+        {"integer","int" },
+        {"integer unsigned","uint" },
+        {"mediumint","int" },
+        {"mediumint unsigned","int" },
+        {"bigint","long" },
+        {"bigint unsigned","ulong" },        
+        {"float","float" },
+        {"double","double" },
+        {"real","double" },
+        {"decimal","decimal" },
+        {"numeric","decimal" },
+        {"dec","decimal" },
+        {"fixed","decimal" },
+        {"float unsigned","decimal" },
+        {"double unsigned","decimal" },
+        {"serial","decimal" },
+        {"date","DateTime" },
+        {"timestamp","DateTime" },
+        {"datetime","DateTime" },
+        {"time","DateTime" },
+        {"datetimeoffset","DateTimeOffset" },
+        {"char","string" },
+        {"varchar","string" },
+        {"tinytext","string" },
+        {"text","string" },
+        {"mediumtext","string" },
+        {"longtext","string" },
+        {"set","string" },
+        {"enum","string" },
+        {"nchar","string" },
+        {"national char","string" },
+        {"nvarchar","string" },
+        {"national varchar","string" },
+        {"character varying","string" },
+        {"binary","byte[]" },
+        {"varbinary","byte[]" },
+        {"tinyblob","byte[]" },
+        {"blob","byte[]" },
+        {"mediumblob","byte[]" },
+        {"longblob","byte[]" },
+        {"char byte","byte[]" },
+        {"json","text" },
     };
     internal static Dictionary<string, string> PgSQLToCSharp => new Dictionary<string, string>
     {
-        {"","" }
+        {"boolean","bool" },
+        {"smallint","short" },
+        {"int2","short" },
+        {"integer","int" },
+        {"int4","int" },
+        {"bigint","long" },
+        {"int8","long" },
+        {"real","float" },
+        {"double precision","double" },
+        {"money","double" },
+        {"numeric","decimal" },
+        {"decimal","decimal" },
+        {"float","decimal" },
+        {"date","DateTime" },
+        {"timestamp","DateTime" },
+        {"timestamp with time zone","DateTime" },
+        {"timestamp without time zone","DateTime" },
+        {"time","DateTime" },
+        {"time with time zone","DateTime" },
+        {"time without time zone","DateTime" },
+        {"interval","DateTime" },
+        {"timestamp with time zone","DateTimeOffset" },
+        {"char","string" },
+        {"varchar","string" },
+        {"text","string" },
+        {"citext","string" },
+        {"enum","string" },
+        {"bit","string" },
+        {"varbit","string" },
+        {"cidr","string" },
+        {"inet","string" },
+        {"macaddr","string" },
+        {"point","string" },
+        {"line","string" },
+        {"lseg","string" },
+        {"box","string" },
+        {"path","string" },
+        {"polygon","string" },
+        {"circle","string" },
+        {"xml*","string" },
+        {"character","string" },
+        {"character varying","string" },
+        {"bit varying","string" },
+        {"json","string" },
+        {"jsonb","string" },
+        {"bytea","byte[]" },
+        {"uuid","Guid" },
     };
 }
+
