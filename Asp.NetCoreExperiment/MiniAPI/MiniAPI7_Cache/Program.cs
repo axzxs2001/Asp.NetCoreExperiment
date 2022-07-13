6ac5ac3d5a0c9075ccd76c82b4825de0d8eabc46
桂素伟 <axzxs2001@163.com>
2022-07-13 22:48:55 +0900

    完成限流demo

--- Asp.NetCoreExperiment/MiniAPI/MiniAPI7_Cache/Program.cs
+++ Asp.NetCoreExperiment/MiniAPI/MiniAPI7_Cache/Program.cs
@@ -10,18 +10,17 @@ var app = builder.Build();
 app.UseOutputCache();
 
 
-var coolEndpointName = "coolPolicy";
-
+var limiterName = "MyLimiterName";
 //并发限流，现在是如果有一个并发，另一个来了就会马上返回失败，0是设置可能有几个在等待,NewestFirst，会把等待的顶掉
-//var options = new RateLimiterOptions().AddConcurrencyLimiter(coolEndpointName, new ConcurrencyLimiterOptions(1, QueueProcessingOrder.NewestFirst,1));
+//var options = new RateLimiterOptions().AddConcurrencyLimiter(limiterName, new ConcurrencyLimiterOptions(1, QueueProcessingOrder.NewestFirst, 1));
 
 //间隔多少时间补发
-//var options = new RateLimiterOptions().AddTokenBucketLimiter(coolEndpointName, new TokenBucketRateLimiterOptions(1, QueueProcessingOrder.OldestFirst, 1, TimeSpan.FromSeconds(10), 1));
+var options = new RateLimiterOptions().AddTokenBucketLimiter(limiterName, new TokenBucketRateLimiterOptions(1, QueueProcessingOrder.OldestFirst, 1, TimeSpan.FromSeconds(8), 1));
 
-//var options = new RateLimiterOptions().AddFixedWindowLimiter(coolEndpointName, new FixedWindowRateLimiterOptions(1, QueueProcessingOrder.OldestFirst, 1, TimeSpan.FromSeconds(10)));
+//var options = new RateLimiterOptions().AddFixedWindowLimiter(limiterName, new FixedWindowRateLimiterOptions(1, QueueProcessingOrder.OldestFirst, 1, TimeSpan.FromSeconds(10)));
 
 
-var options = new RateLimiterOptions().AddSlidingWindowLimiter(coolEndpointName, new SlidingWindowRateLimiterOptions(1, QueueProcessingOrder.OldestFirst, 1, TimeSpan.FromSeconds(10),1));
+//var options = new RateLimiterOptions().AddSlidingWindowLimiter(limiterName, new SlidingWindowRateLimiterOptions(1, QueueProcessingOrder.OldestFirst, 1, TimeSpan.FromSeconds(10),1));
 
 //options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(context =>
 //    {
@@ -32,10 +31,17 @@ var options = new RateLimiterOptions().AddSlidingWindowLimiter(coolEndpointName,
 app.UseRateLimiter(options);
 
 
-app.MapGet("/nocached", () => DateTime.Now.ToString()).RequireRateLimiting(coolEndpointName);
-app.MapGet("/nocached2", () => {  return DateTime.Now.ToString(); }).RequireRateLimiting(coolEndpointName); ;
+app.MapGet("/limit", () =>
+{
+    app.Logger.LogInformation($"limit 开始 {DateTime.Now}");
+    Thread.Sleep(5000);
+    app.Logger.LogInformation($"limit 结束 {DateTime.Now}");
+    return DateTime.Now.ToString();
+}).RequireRateLimiting(limiterName);
+
+
 app.MapGet("/cached", () => DateTime.Now.ToString()).CacheOutput();
-app.MapGet("/query", () => DateTime.Now.ToString()).CacheOutput(p => p.VaryByQuery("key"));
+app.MapGet("/query", () => DateTime.Now.ToString()).CacheOutput(p => p.VaryByQuery("key").Expire(TimeSpan.FromMinutes(10)));
 
 app.Run();
 
