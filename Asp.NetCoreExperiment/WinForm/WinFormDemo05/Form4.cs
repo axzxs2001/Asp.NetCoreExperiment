67638dcb679690166c4380ccd2cf66a9aa0c7317
桂素伟 <axzxs2001@163.com>
2022-08-06 23:31:02 +0900

    完成

---
+++ Asp.NetCoreExperiment/WinForm/WinFormDemo05/Form4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormDemo05
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void Form4_MouseMove(object sender, MouseEventArgs e)
+        {
+            button1.Location = e.Location;
+        }
+    }
+}
