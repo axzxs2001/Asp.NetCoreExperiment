1753f9ca020d12d2ac75d26be79da394ade99f2a
桂素伟 <axzxs2001@163.com>
2022-08-10 21:48:29 +0900

    提交

--- Asp.NetCoreExperiment/WinForm/WinFormsDemo06/Form1.Designer.cs
+++ Asp.NetCoreExperiment/WinForm/WinFormsDemo06/Form1.Designer.cs
@@ -31,13 +31,15 @@
             this.startButton = new System.Windows.Forms.Button();
             this.messageLabel = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // startButton
             // 
-            this.startButton.Location = new System.Drawing.Point(257, 198);
+            this.startButton.Location = new System.Drawing.Point(630, 280);
+            this.startButton.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(100, 45);
+            this.startButton.Size = new System.Drawing.Size(157, 64);
             this.startButton.TabIndex = 0;
             this.startButton.Text = "开始";
             this.startButton.UseVisualStyleBackColor = true;
@@ -47,32 +49,48 @@
             // 
             this.messageLabel.AutoSize = true;
             this.messageLabel.Font = new System.Drawing.Font("Microsoft YaHei UI", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.messageLabel.Location = new System.Drawing.Point(35, 37);
+            this.messageLabel.Location = new System.Drawing.Point(55, 52);
+            this.messageLabel.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.messageLabel.Name = "messageLabel";
-            this.messageLabel.Size = new System.Drawing.Size(0, 35);
+            this.messageLabel.Size = new System.Drawing.Size(0, 52);
             this.messageLabel.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(124, 198);
+            this.button1.Location = new System.Drawing.Point(408, 283);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 45);
+            this.button1.Size = new System.Drawing.Size(197, 64);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Exception开始";
+            this.button1.Text = "可能Exception开始";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 283);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(197, 64);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exception开始";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(577, 255);
+            this.ClientSize = new System.Drawing.Size(907, 360);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.messageLabel);
             this.Controls.Add(this.startButton);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,5 +101,6 @@
         private Button startButton;
         private Label messageLabel;
         private Button button1;
+        private Button button2;
     }
 }
--- Asp.NetCoreExperiment/WinForm/WinFormsDemo06/Form1.cs
+++ Asp.NetCoreExperiment/WinForm/WinFormsDemo06/Form1.cs
@@ -6,21 +6,40 @@ namespace WinFormsDemo06
     {
         public Form1()
         {
-            InitializeComponent();         
+            InitializeComponent();
         }
         bool mark = true;
         private void startButton_Click(object sender, EventArgs e)
         {
             Task.Run(() =>
             {
-                var times = 0;
-                while (mark)
+                foreach (var item in list)
                 {
-                    this.Invoke(() =>
+                    if (mark == false)
                     {
-                        messageLabel.Text = $"正在上传第{times++}条！";
-                    });
-                    SpinWait.SpinUntil(() => false, 100);
+                        break;
+                    }
+                    var dotString = "";
+                    for (var i = 0; i < item.Time; i++)
+                    {
+                        if (mark == false)
+                        {
+                            break;
+                        }
+                        if (i % 6 == 0)
+                        {
+                            dotString = ".";
+                        }
+                        else
+                        {
+                            dotString += ".";
+                        }
+                        this.Invoke(() =>
+                        {
+                            messageLabel.Text = $"{item.Name}{dotString}";
+                        });
+                        SpinWait.SpinUntil(() => false, 500);
+                    }
                 }
                 this.Invoke(() =>
                 {
@@ -31,28 +50,79 @@ namespace WinFormsDemo06
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
-            if (mark)
-            {
-                e.Cancel = true;
-                mark = false;
-           
-            }
+            //if (mark)
+            //{
+            //    e.Cancel = true;
+            //    mark = false;
+
+            //}
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Task.Run(() =>
             {
-                var times = 0;
-                while (true)
+                foreach (var item in list)
                 {
-                    this.Invoke(() =>
+                    var dotString = "";
+                    for (var i = 0; i < item.Time; i++)
                     {
-                        messageLabel.Text = $"正在上传第{times++}条！";
-                    });
-                    SpinWait.SpinUntil(() => false, 100);
-                }              
+                        if (i % 6 == 0)
+                        {
+                            dotString = ".";
+                        }
+                        else
+                        {
+                            dotString += ".";
+                        }
+                        this.Invoke(() =>
+                        {
+                            messageLabel.Text = $"{item.Name}{dotString}";
+                        });
+                        SpinWait.SpinUntil(() => false, 300);
+                    }
+                }
             });
         }
+        static List<Item> list;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            list = new List<Item>
+            {
+                new Item{ Name="正在上传诊疗项目",Time=8 },
+                new Item{ Name="正在上传器材",Time=12 },
+                new Item{ Name="正在上传药品",Time=20 },
+                new Item{ Name="正在核对",Time=24 },
+            };
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Task.Run(() =>
+            {
+                foreach (var item in list)
+                {
+                    var dotString = "";
+                    for (var i = 0; i < item.Time; i++)
+                    {
+                        if (i % 6 == 0)
+                        {
+                            dotString = ".";
+                        }
+                        else
+                        {
+                            dotString += ".";
+                        }
+                        messageLabel.Text = $"{item.Name}{dotString}";
+                        Thread.Sleep(300);
+                    }
+                }
+            });
+        }
+    }
+    class Item
+    {
+        public string Name { get; set; }
+        public int Time { get; set; }
     }
 }
