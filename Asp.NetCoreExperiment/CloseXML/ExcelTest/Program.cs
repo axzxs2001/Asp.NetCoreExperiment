5d139ba783fb38fe78400147c5d703b8ce046e90
素伟 桂 <axzxs2001@163.com>
2022-09-27 09:35:27 +0900

    test pdf build memory leak

--- Asp.NetCoreExperiment/CloseXML/ExcelTest/Program.cs
+++ Asp.NetCoreExperiment/CloseXML/ExcelTest/Program.cs
@@ -1,41 +1,63 @@
 ﻿
 
+using Aspose.Cells;
 using ClosedXML.Excel;
 using DocumentFormat.OpenXml.Spreadsheet;
+using QRCoder;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Reflection.Emit;
 using System.Runtime.InteropServices;
+using Color = System.Drawing.Color;
+using Workbook = Aspose.Cells.Workbook;
 
 Console.WriteLine("回车开始");
 Console.ReadLine();
-
-using var workBook = new XLWorkbook();
-var ws = workBook.Worksheets.Add("test");
-for (var i = 1; i < 200; i++)
+var qrGenerator = new QRCodeGenerator();
+while (true)
 {
-    var image = ws.AddPicture("C:\\Users\\axzxs\\Pictures\\1.jpg").MoveTo(ws.Cell(i, 1), new Point(50, 10));
-    image.Width = 60;
-    image.Height = 60;
+    var workBook = new XLWorkbook();
+    var ws = workBook.Worksheets.Add("test");
+    for (var i = 1; i < 200; i++)
+    {
+
+
+        var qrCodeData = qrGenerator.CreateQrCode(DateTime.Now.ToString("yyMMddHHmmssfffffff") + "QRCodeGeneratoQRCodeGeneratoQRCodeGeneratoQRCodeGeneratoQRCodeGenerato", QRCodeGenerator.ECCLevel.L);
+        var qrCode = new QRCode(qrCodeData);
+        var qrCodeImage = qrCode.GetGraphic(10, Color.Black, Color.White, false);
+
+        var imgStream = new MemoryStream();
+        qrCodeImage.Save(imgStream, ImageFormat.Bmp);
+        imgStream.Seek(0, SeekOrigin.Begin);
+
+
+        var image = ws.AddPicture(imgStream).MoveTo(ws.Cell(i, 1), new Point(50, 10));
+        image.Width = 60;
+        image.Height = 60;
+    }
+
+
+    var tmpStream = new MemoryStream();
+    workBook.SaveAs(tmpStream);
+    tmpStream.Position = 0;
+    var wb = new Workbook(tmpStream);
+    var pdfSaveOptions = new PdfSaveOptions
+    {
+        OptimizationType = Aspose.Cells.Rendering.PdfOptimizationType.MinimumSize,
+        OnePagePerSheet = true
+
+    };
+    //var ms = new MemoryStream();
+    //wb.Save(ms, pdfSaveOptions);
+    //ms.Position = 0;
+
+    wb.Save($"C:\\Users\\axzxs\\Pictures\\{DateTime.Now.ToString("yyyyMMddHHmmss")}.pdf");
+
+    Console.WriteLine($"完了:{DateTime.Now}");
+    Console.ReadLine();
+
 }
-
-workBook.SaveAs($"C:\\Users\\axzxs\\Pictures\\{DateTime.Now.ToString("yyyyMMddHHmmss")}.xlsx");
-
-Console.WriteLine("完了");
-Console.ReadLine();
-foreach (var p in ws.Pictures)
-{
-    p.Dispose();
-}
-foreach (var r in ws.Rows())
-{
-    r.Delete();
-}
-workBook.Dispose();
-GC.Collect();
-
-var t = GC.GetTotalAllocatedBytes();
-var t1 = GC.GetAllocatedBytesForCurrentThread();
-
 
 
 Console.ReadLine();
