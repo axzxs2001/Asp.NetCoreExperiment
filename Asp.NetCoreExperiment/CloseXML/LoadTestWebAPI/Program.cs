553a8187baaf08ddf0be11e2edd34a33397a94a4
素伟 桂 <axzxs2001@163.com>
2022-09-29 10:10:14 +0900

    test

--- Asp.NetCoreExperiment/CloseXML/LoadTestWebAPI/Program.cs
+++ Asp.NetCoreExperiment/CloseXML/LoadTestWebAPI/Program.cs
@@ -6,17 +6,21 @@ using QuestPDF.Fluent;
 using QRCoder;
 using Microsoft.AspNetCore.Mvc;
 using QuestPDF.Drawing;
+using QuestPDF;
 
+using var stream = File.OpenRead(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "fonts", "MEIRYO.TTC"));
+FontManager.RegisterFont(stream);
 
+Settings.EnableCaching = true;
+Settings.EnableDebugging = false;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/test", () =>
 {
-    return "ok";
+    return "ok" + DateTime.Now;
 });
-
 
 app.MapGet("/getpdf", () =>
 {
@@ -29,29 +33,27 @@ app.MapGet("/getpdf", () =>
     {
         table.Rows.Add(row.ToString(), row.ToString(), row.ToString(), row.ToString(), row.ToString(), row.ToString(), row.ToString(), DateTime.Now + "wwewrwerewfdsfdswefwefewfwefwefew" + row, row.ToString(), row.ToString());
     }
-
+    GC.Collect();
     return TypedResults.File(GetPDF(table), contentType: "application/pdf", fileDownloadName: "a.pdf");
 
 });
 
 app.Run();
-
+static IContainer CellStyle(IContainer container)
+{
+    return container.DefaultTextStyle(x => x.SemiBold().FontSize(11)).PaddingVertical(5).BorderBottom(1).BorderColor(Colors.Black);
+}
 static byte[] GetPDF(DataTable dt)
 {
     var doc = QuestPDF.Fluent.Document.Create(container =>
     {
 
-        using var stream = File.OpenRead(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "fonts", "MEIRYO.TTC"));
-        FontManager.RegisterFont(stream);
         container.Page(page =>
         {
-
             page.Size(PageSizes.A4);
             page.Margin(2, Unit.Centimetre);
             page.PageColor(Colors.White);
             page.DefaultTextStyle(x => x.FontSize(14).FontFamily("Meiryo"));
-
-
             page.Content()
                 .PaddingVertical(1, Unit.Centimetre)
                 .Column(x =>
@@ -72,19 +74,12 @@ static byte[] GetPDF(DataTable dt)
                         table.Header(header =>
                         {
 
-
                             foreach (DataColumn col in dt.Columns)
                             {
                                 header.Cell().Element(CellStyle).Text(col.ColumnName);
                             }
 
-
-                            static IContainer CellStyle(IContainer container)
-                            {
-                                return container.DefaultTextStyle(x => x.SemiBold().FontSize(11)).PaddingVertical(5).BorderBottom(1).BorderColor(Colors.Black);
-                            }
                         });
-
                         foreach (DataRow row in dt.Rows)
                         {
                             for (var i = 0; i < dt.Columns.Count; i++)
@@ -99,11 +94,6 @@ static byte[] GetPDF(DataTable dt)
                                 {
                                     table.Cell().Element(CellStyle).Text(row[i].ToString());
                                 }
-                            }
-
-                            static IContainer CellStyle(IContainer container)
-                            {
-                                return container.DefaultTextStyle(x => x.SemiBold().FontSize(11)).PaddingVertical(5).BorderBottom(1).BorderColor(Colors.Black);
                             }
                         }
 
