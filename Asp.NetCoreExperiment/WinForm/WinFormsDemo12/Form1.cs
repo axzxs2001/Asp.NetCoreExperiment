dc081e5956ef54a7e52f49665ab8750c8ef3c38b
桂素伟 <axzxs2001@163.com>
2022-10-02 10:33:18 +0900

    画图

---
+++ Asp.NetCoreExperiment/WinForm/WinFormsDemo12/Form1.cs
@@ -0,0 +1,15 @@
+namespace WinFormsDemo12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var graphics = this.CreateGraphics();
+        }
+    }
+}
