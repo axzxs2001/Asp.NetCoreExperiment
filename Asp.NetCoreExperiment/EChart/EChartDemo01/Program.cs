475edafcd5fd933fb411dccb04d1a5e37d68825d
桂素伟 <axzxs2001@163.com>
2022-10-24 18:43:12 +0900

    动态加载EChart

---
+++ Asp.NetCoreExperiment/EChart/EChartDemo01/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
