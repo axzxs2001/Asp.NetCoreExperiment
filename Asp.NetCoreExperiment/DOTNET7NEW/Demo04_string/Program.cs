004d0e44dd637400b8447def42df416252057d87
桂素伟 <axzxs2001@163.com>
2022-12-20 18:40:03 +0900

    add string project

---
+++ Asp.NetCoreExperiment/DOTNET7NEW/Demo04_string/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+int X = 2;
+int Y = 3;
+
+var pointMessage = $"""
+                    The point "{X}, {Y}" is Math.Sqrt(X * X + Y * Y)={Math.Sqrt(X * X + Y * Y)} from the origin
+                    """;
+
+Console.WriteLine(pointMessage);
+
+
+Console.WriteLine($"|{"Left",-7}|{"Right",7}|");
+
+const int FieldWidthRightAligned = 20;
+Console.WriteLine($"{Math.PI,FieldWidthRightAligned} - default formatting of the pi number");
+Console.WriteLine($"{Math.PI,FieldWidthRightAligned:F3} - display only three decimal digits of the pi number");
