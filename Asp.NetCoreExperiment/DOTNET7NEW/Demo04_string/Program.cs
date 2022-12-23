38743259de69b8deb6ef88f075362f33970a677b
桂素伟 <axzxs2001@163.com>
2022-12-23 17:17:40 +0900

    fix

--- Asp.NetCoreExperiment/DOTNET7NEW/Demo04_string/Program.cs
+++ Asp.NetCoreExperiment/DOTNET7NEW/Demo04_string/Program.cs
@@ -16,3 +16,17 @@ Console.WriteLine($"|{"Left",-7}|{"Right",7}|");
 const int FieldWidthRightAligned = 20;
 Console.WriteLine($"{Math.PI,FieldWidthRightAligned} - default formatting of the pi number");
 Console.WriteLine($"{Math.PI,FieldWidthRightAligned:F3} - display only three decimal digits of the pi number");
+
+
+class ABC
+{
+    public void F()
+    {
+        var i = 10;
+        if(i>20)
+        {
+            Console.WriteLine("abcd");
+        }
+
+    }
+}
