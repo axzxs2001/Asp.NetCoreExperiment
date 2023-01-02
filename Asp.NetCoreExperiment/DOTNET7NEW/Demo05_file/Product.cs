f0eab452a29b210c482f1908bc67bb6f697d3988
桂素伟 <axzxs2001@163.com>
2023-01-02 19:54:27 +0900

    file complete

---
+++ Asp.NetCoreExperiment/DOTNET7NEW/Demo05_file/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo05_file
+{
+    public class Product
+    {
+        public string? Name { get; set; }
+        public decimal PurchasePrice { get; set; }
+        public void PrintSalesProduct()
+        {
+            var salesPrice = new SalesPrice
+            {
+                RetailPrice = PurchasePrice * 1.5m,
+                WholesalePrice = PurchasePrice * 1.2m
+            };
+            Console.WriteLine($"Name:{Name},{salesPrice}");
+        }
+    }
+    file record SalesPrice
+    {
+        public decimal RetailPrice { get; set; }
+        public decimal WholesalePrice { get; set; }
+    }   
+}
---
+++ Asp.NetCoreExperiment/DOTNET7NEW/Demo05_file/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using Demo05_file;
+
+var product = new Product();
+product.Name = "冰棍";
+product.PurchasePrice = 1;
+product.PrintSalesProduct();
+
+
+
+Console.ReadLine();
