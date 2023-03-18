9da52ba6637fcbf6663a82e4aef39ad6c2e09aa6
桂素伟 <axzxs2001@163.com>
2023-03-18 18:41:38 +0900

    完成

--- Asp.NetCoreExperiment/DOTNET8/HttpDemo03/Program.cs
+++ Asp.NetCoreExperiment/DOTNET8/HttpDemo03/Program.cs
@@ -1,30 +1,44 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 var app = builder.Build();
+var logger = app.Logger;
 
-// Configure the HTTP request pipeline.
-
-var summaries = new[]
+app.MapGet("item/{id:int}", (int id) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    logger.LogInformation("按 ID={0} 查询Item", id);
+    return new Item { ID = 1, Name = "PS5", Price = 2000, Describe = "White" };
+});
 
-app.MapGet("/weatherforecast", () => {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+app.MapPost("item", ([FromBody] Item item) =>
+{
+    logger.LogInformation("添加Item:{0}", item);
+    item.ID = 999;
+    return item;
+});
+
+app.MapDelete("item/{id:int}", (int id) =>
+{
+    logger.LogInformation("按 ID={0} 删除Item", id);
+    return TypedResults.Ok("删除成功！");
+});
+
+app.MapPut("item", ([FromBody] Item item) =>
+{
+    logger.LogInformation("按 ID={0} 修改Item", item.ID);
+    item.Describe = "White+Black";
+    return item;
 });
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+public record Item
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Describe { get; set; }
 }
---
+++ Asp.NetCoreExperiment/DOTNET8/MutilFromBodyDemo04/Program.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+var app = builder.Build();
+
+
+app.MapPost("/todos", ([FromBody] Todo todo, [FromBody] User user) =>
+{
+    app.Logger.LogInformation(todo.ToString());
+    app.Logger.LogInformation(user.ToString());
+});
+
+app.Run();
+
+
+record Todo
+{
+    public string Id { get; set; }
+}
+record User
+{
+    public string Name { get; set; }
+}
