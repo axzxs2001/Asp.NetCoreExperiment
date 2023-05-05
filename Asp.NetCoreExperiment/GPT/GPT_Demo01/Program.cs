13f6fee97ec0f290b63759a4e527bf3bece55037
桂素伟 <axzxs2001@163.com>
2023-05-05 21:08:15 +0900

    GPT

---
+++ Asp.NetCoreExperiment/GPT/GPT_Demo01/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.AI.OpenAI.TextEmbedding;
+using Microsoft.SemanticKernel.Memory;
+using System;
+using System.Runtime.InteropServices;
+
+var key = File.ReadAllText(@"E:\\GPT\key.txt");
+
+var kernel = Kernel.Builder
+
+    .WithMemoryStorage(new VolatileMemoryStore())
+    .Configure(c =>
+    {
+        c.AddOpenAITextCompletionService("davinci-openai", "text-davinci-003", key);
+
+        c.AddOpenAITextEmbeddingGenerationService("davinci-openai", "text-embedding-ada-002", key);
+
+
+
+    })
+    .Build();
+
+
+var prompt = @"{{$input}}";
+
+var summarize = kernel.CreateSemanticFunction(prompt);
+
+while (true)
+{
+    Console.WriteLine("问题：");
+    string text1 = Console.ReadLine();
+    var context = await summarize.InvokeAsync(text1);
+   
+    Console.WriteLine(context.Result);
+}
