a4708a3515400c3e5b45c7197169b4edfcb212ae
桂素伟 <axzxs2001@163.com>
2023-05-26 17:02:38 +0900

    对齐

--- Asp.NetCoreExperiment/GPT/GPT_WebBot/Program.cs
+++ Asp.NetCoreExperiment/GPT/GPT_WebBot/Program.cs
@@ -38,7 +38,7 @@ public static class BuilderExt
 
         const string MemoryCollectionName = "gsw";
         await kernel.Memory.SaveInformationAsync(MemoryCollectionName, id: "info0", text: "名字叫桂素伟");
-        await kernel.Memory.SaveInformationAsync(MemoryCollectionName, id: "info1", text: "性别男，身高171cm，体重75千克");
+        await kernel.Memory.SaveInformationAsync(MemoryCollectionName, id: "info1", text: "性别男，身高171cm，\r\n体重75千克");
         await kernel.Memory.SaveInformationAsync(MemoryCollectionName, id: "info2", text: "职业是农民，他擅长种茄子");
         await kernel.Memory.SaveInformationAsync(MemoryCollectionName, id: "info3", text: "有20年的种地经验");
         await kernel.Memory.SaveInformationAsync(MemoryCollectionName, id: "info4", text: "现在住在五十亩村");
@@ -48,12 +48,12 @@ public static class BuilderExt
 
 
         var prompt = """
-    给出答案或者不知道答案时说“非常抱歉，我没有找到你要的问题！”     
-    对话中的关于桂素伟的信息:
-    {{ $fact }}     
-    用户: {{ $ask }}
-    机器人:
-    """;
+        给出答案或者不知道答案时说“非常抱歉，我没有找到你要的问题！”     
+        对话中的关于桂素伟的信息:
+        {{ $fact }}     
+        用户: {{ $ask }}
+        机器人:
+        """;
         var semanticFunction = kernel.CreateSemanticFunction(prompt, temperature: 0.7, topP: 0.5);
         var context = kernel.CreateNewContext();
         builder.Services.AddSingleton(kernel);
