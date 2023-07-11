83b84c6a835c53840bd7ae621047442a8d94e997
桂素伟 <axzxs2001@163.com>
2023-07-11 17:08:53 +0900

    PDF  to Image

---
+++ Asp.NetCoreExperiment/QuestPDF/WinFormPDFRevie/Form2.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace WinFormPDFRevie
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 54);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 396);
+            panel1.TabIndex = 3;
+            panel1.Paint += panel1_Paint;
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Top;
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(800, 54);
+            button1.TabIndex = 2;
+            button1.Text = "加载PDF";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Controls.Add(button1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+    }
+}
---
+++ Asp.NetCoreExperiment/QuestPDF/WinFormPDFRevie/Form2.cs
@@ -0,0 +1,56 @@
+﻿using Ghostscript.NET.Rasterizer;
+using Ghostscript.NET;
+using QuestPDF.Fluent;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ghostscript.NET.Rasterizer;
+using Ghostscript.NET;
+using System.Drawing.Imaging;
+
+namespace WinFormPDFRevie
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ConvertPdfToImages(Directory.GetCurrentDirectory() + "//test.pdf", Directory.GetCurrentDirectory(), 1000);
+
+        }
+
+
+        public void ConvertPdfToImages(string inputFile, string outputDirectory, int dpi)
+        {
+            using (GhostscriptRasterizer rasterizer = new GhostscriptRasterizer())
+            {
+                var version = GhostscriptVersionInfo.GetLastInstalledVersion();
+                {
+                    rasterizer.Open(inputFile, version, false);
+
+                    for (int pageNumber = 1; pageNumber <= rasterizer.PageCount; pageNumber++)
+                    {
+                        string pageFilePath = Path.Combine(outputDirectory, $"page{pageNumber}.png");
+
+                        Image img = rasterizer.GetPage(dpi, pageNumber);
+                        img.Save(pageFilePath, ImageFormat.Png);
+                    }
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+    }
+}
