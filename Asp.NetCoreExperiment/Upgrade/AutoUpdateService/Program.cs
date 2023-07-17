aefbe37c6c4ef1bdd386602a33ff9d3277fc97ad
桂素伟 <axzxs2001@163.com>
2023-07-17 12:28:11 +0900

    自动升级

---
+++ Asp.NetCoreExperiment/Upgrade/AutoUpdateService/Program.cs
@@ -0,0 +1,17 @@
+using AutoUpdateService;
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+var app = builder.Build();
+
+var sampleTodos = TodoGenerator.GenerateTodos().ToArray();
+
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.Run();
+
