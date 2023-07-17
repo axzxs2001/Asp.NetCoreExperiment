fefb2edea8eeb1f2dcc0b99b9ce3ab69169ca3c5
桂素伟 <axzxs2001@163.com>
2023-07-17 10:52:26 +0900

    fix

--- Asp.NetCoreExperiment/MiniAPI/MiniAPI8_Form/Program.cs
+++ Asp.NetCoreExperiment/MiniAPI/MiniAPI8_Form/Program.cs
@@ -1,10 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
 using MiniAPI8_Form;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
 var app = builder.Build();
 
-var sampleTodos = TodoGenerator.GenerateTodos().ToArray();
+var sampleTodos = TodoGenerator.GenerateTodos().ToList();
 
 var todosApi = app.MapGroup("/todos");
 todosApi.MapGet("/", () => sampleTodos);
@@ -12,6 +13,11 @@ todosApi.MapGet("/{id}", (int id) =>
     sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
         ? Results.Ok(todo)
         : Results.NotFound());
+
+todosApi.MapPost("/", ([FromForm] Todo todo) =>
+{
+    sampleTodos.Add(todo);
+});
 
 app.Run();
 
