0b38922cbf6ccb5c8972209a91b5963477088787
桂素伟 <axzxs2001@163.com>
2023-07-19 12:31:15 +0900

    fix bug

---
+++ Asp.NetCoreExperiment/DOTNET8/APIMetricDemo/Program.cs
@@ -0,0 +1,43 @@
+using APIMetricDemo;
+using OpenTelemetry.Metrics;
+
+
+var builder = WebApplication.CreateSlimBuilder(args);
+
+
+builder.Services.AddOpenTelemetry().WithMetrics(builder =>
+    {
+        builder.AddPrometheusExporter();
+
+        builder.AddMeter("Microsoft.AspNetCore.Hosting",
+                         "Microsoft.AspNetCore.Server.Kestrel"
+                         //"Microsoft.AspNetCore.Http.Connections",
+                         //"System.Runtime",                      
+                         //"System.Net.Http",
+                         //"System.Net.NameResolution",
+                         //"System.Net.Security",
+                         //"System.Net.Sockets"
+                         );
+        builder.AddView("http-server-request-duration",
+            new ExplicitBucketHistogramConfiguration
+            {
+                Boundaries = new double[] { 0, 0.005, 0.01, 0.025, 0.05,
+                       0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10 }
+            });
+    });
+var app = builder.Build();
+
+app.MapPrometheusScrapingEndpoint();
+app.MapGet("/", () => "Hello OpenTelemetry! ticks:"
+                     + DateTime.Now.Ticks.ToString()[^3..]);
+var sampleTodos = TodoGenerator.GenerateTodos().ToArray();
+
+var todosApi = app.MapGroup("/todos");
+todosApi.MapGet("/", () => sampleTodos);
+todosApi.MapGet("/{id}", (int id) =>
+    sampleTodos.FirstOrDefault(a => a.Id == id) is { } todo
+        ? Results.Ok(todo)
+        : Results.NotFound());
+
+app.Run();
+
