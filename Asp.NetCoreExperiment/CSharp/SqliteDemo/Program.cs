eb53ce3d102a0f3b3b11a128162f209bb523cdcc
桂素伟 <axzxs2001@163.com>
2023-09-14 10:13:14 +0900

    sqlite demo test

---
+++ Asp.NetCoreExperiment/CSharp/SqliteDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+using Microsoft.Data.Sqlite;
+using System.Buffers;
+using System.Data;
+
+var sql = "select * from drugGoods where (goodsCode like @value1||'%' or goodsName like '%'|| @value1||'%' or logogram like '%'||@value1||'%' ) and goodsCode is not null";
+var data = GetData(sql, new Dictionary<string, dynamic> { { "value1", ""} });
+
+
+static DataTable GetData(string sql, Dictionary<string, dynamic> dic = null)
+{
+    var constring = @"Data Source=C:\\myfiles\sxyb_db.sqlite";
+    var pars = new List<SqliteParameter>();
+    if (dic != null)
+    {
+        foreach (var item in dic)
+        {
+            pars.Add(new SqliteParameter($"@{item.Key}", $"{item.Value}"));
+        }
+    }
+
+    using var con = new SqliteConnection(constring);
+    using var cmd = new SqliteCommand(sql, con);
+    cmd.Parameters.AddRange(pars);
+    con.Open();
+    using var reader = cmd.ExecuteReader();
+    var dt = new DataTable();
+    dt.Load(reader);
+    con.Close();
+    return dt;
+}
