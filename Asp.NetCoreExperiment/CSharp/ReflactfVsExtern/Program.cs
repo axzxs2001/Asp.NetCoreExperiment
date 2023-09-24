d709921425c6d511a6530731c4bf348133028b97
桂素伟 <axzxs2001@163.com>
2023-09-24 22:23:51 +0900

    像反射一亲用私用方法成员。

---
+++ Asp.NetCoreExperiment/CSharp/ReflactfVsExtern/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+var test = new TestClass();
+
+Console.WriteLine(Method(test));
+
+
+
+[UnsafeAccessor(UnsafeAccessorKind.Method, Name = "Method")]
+static extern DateTime Method(TestClass test);
+
+
+
+
+public class TestClass
+{
+
+    DateTime Method()
+    {
+        return DateTime.Now;
+    }
+}
