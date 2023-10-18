421a9763486f8cbe13cb7f17b79e75312caa310c
桂素伟 <axzxs2001@163.com>
2023-10-18 11:51:13 +0900

    完成class和record的比较

--- Asp.NetCoreExperiment/CSharp/recordORclass/Program.cs
+++ Asp.NetCoreExperiment/CSharp/recordORclass/Program.cs
@@ -1,27 +1,32 @@
 ﻿
 var order1 = new Order(1, "张三", "苹果");
 Console.WriteLine(order1);
+Console.WriteLine(order1.ToString());
 var order2 = order1 with { Id = 2, Name = "李四" };
 Console.WriteLine(order2);
 
 order1.Deconstruct(out var id, out var name, out var description);
 Console.WriteLine($"Id:{id}, Name:{name}, Description:{description}");
+Console.WriteLine(order1);
+
+Console.WriteLine(order1 != order2);
 
 var goods1 = new Goods(1, "Apple", "苹果");
-
-
+var goods2 = new Goods(1, "Apple", "苹果");
+Console.WriteLine(goods1.GetHashCode());
+Console.WriteLine(goods1 == goods2);
 //Console.WriteLine(order1);
 //var goods2 = goods1 with { Id = 2, Name = "Banana" };
 //Console.WriteLine(goods2);
 
-record Order(int Id, string Name, 
-    string? Description = null)
+public delegate void DL();
+record Order(int Id, string Name,string? Description = null)
 {
-    public void Print()
-    {
-        Console.WriteLine($"Id:{Id}, Name:{Name}, Description:{Description}");
-    }
+    public event DL sj;
+    public int Id { get; } = Id;
+
 }
+
 class Goods(int Id, string Name, string? Description = null)
 {
     public int Id { get; } = Id;
