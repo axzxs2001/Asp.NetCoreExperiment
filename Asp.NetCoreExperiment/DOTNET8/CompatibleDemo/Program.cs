d71357f4a0ddd5ceefb36116a7af2be5f1ff7163
桂素伟 <axzxs2001@163.com>
2023-10-31 17:44:13 +0900

    fix

---
+++ Asp.NetCoreExperiment/DOTNET8/CompatibleDemo/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.Personal));
+Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData));
+Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.MyVideos));
