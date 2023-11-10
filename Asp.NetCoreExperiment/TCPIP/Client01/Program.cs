16623557dae96e07fc473a07e8ef1e6b3f8c39b5
桂素伟 <axzxs2001@163.com>
2023-11-10 15:02:02 +0900

    TCPIP

---
+++ Asp.NetCoreExperiment/TCPIP/Client01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+Console.Title = "客户端";
+Console.ReadLine();
+
+var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+await socket.ConnectAsync(new IPEndPoint(IPAddress.Parse("127.0.0.1"), 20000));
+
+await socket.SendAsync(System.Text.Encoding.UTF8.GetBytes("你好"));
+
+//var server=new TcpListener(IPAddress.Parse("127.0.0.1"), 20000);
+
+Console.ReadLine();
