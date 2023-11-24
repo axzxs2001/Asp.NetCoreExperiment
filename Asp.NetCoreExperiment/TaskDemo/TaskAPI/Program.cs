fe2456d6fb402aa1a49ae8f8a6da863efbfb3a95
桂素伟 <axzxs2001@163.com>
2023-11-24 15:47:35 +0900

    add task

--- Asp.NetCoreExperiment/TaskDemo/TaskAPI/Program.cs
+++ Asp.NetCoreExperiment/TaskDemo/TaskAPI/Program.cs
@@ -3,11 +3,18 @@ using Microsoft.AspNetCore.Http.HttpResults;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 var logger = app.Logger;
-app.MapGet("/testweb", () =>
+app.MapGet("/testweb", (int second = 10, bool isexception = false) =>
 {
     logger.LogInformation("/testweb被请求！");
     Thread.Sleep(10000);
-    return Results.Ok("完成web请求");
+    if (isexception)
+    {
+        throw new Exception("异常测试");
+        }
+    else
+    {
+        return Results.Ok("完成web请求");
+    }
 });
 app.Run();
 
--- Asp.NetCoreExperiment/TaskDemo/TestDemo01/Program.cs
+++ Asp.NetCoreExperiment/TaskDemo/TestDemo01/Program.cs
@@ -15,7 +15,7 @@ async Task<string> GetTest(int second)
     var begintTime = DateTime.Now;
     Console.WriteLine($"非await调用Web方法前：{begintTime:yyyy-MM-dd HH:mm:ss},为以后计算的开始时间");
     Task<string> getStringTask =
-        client.GetStringAsync("http://localhost:5000/weatherforecast");
+        client.GetStringAsync("http://localhost:5000/testweb?second=10&isexception=false");
     Console.WriteLine($"非await调用Web方法后,DoSomething前：{DateTime.Now:yyyy-MM-dd HH:mm:ss}，与开始时差：{(DateTime.Now - begintTime).TotalMilliseconds}毫秒");
     DoSomething(second);
     Console.WriteLine($"DoSomething后：{DateTime.Now:yyyy-MM-dd HH:mm:ss}，与开始时差：{(DateTime.Now - begintTime).TotalSeconds}秒");
