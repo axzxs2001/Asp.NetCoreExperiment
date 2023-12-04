8f89d5d8a3ea6310248131a1d9e3b26247b36c8d
桂素伟 <axzxs2001@163.com>
2023-12-04 14:19:48 +0900

    fix

---
+++ Asp.NetCoreExperiment/Interceptors/InterceptorsDemo1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+var c = new C();
+c.InterceptableMethod(10);
+
+
+class C
+{
+    public void InterceptableMethod(int param)
+    {
+        Console.WriteLine($"原始 {param}");
+    }
+}
+
+namespace ABC.ABC
+{
+    static class D
+    {
+        [InterceptsLocation("C:\\MyFile\\Source\\Repos\\Asp.NetCoreExperiment\\Asp.NetCoreExperiment\\Interceptors\\InterceptorsDemo1\\Program.cs", 5, 3)]
+        internal static void InterceptorMethod(this C c, int param)
+        {
+            Console.WriteLine($"拦截 {param}");
+        }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    internal sealed class InterceptsLocationAttribute(string filePath, int line, int column) : Attribute
+    {
+    }
+}
