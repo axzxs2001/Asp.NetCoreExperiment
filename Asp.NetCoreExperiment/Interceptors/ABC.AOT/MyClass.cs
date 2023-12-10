3b475c54c80766b2f2c27d2ac5632f421e42ba51
桂素伟 <axzxs2001@163.com>
2023-12-10 20:37:22 +0900

    完成拦截器

---
+++ Asp.NetCoreExperiment/Interceptors/ABC.AOT/MyClass.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System.Globalization;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+
+namespace ABC.AOT
+{
+    public class MyClass : ABC.MyClass
+    {
+        public  void Print(string s)
+        {
+            base.Print(s);
+        }
+    }
+}
+
+namespace ABC.AOT
+{
+    public static class MyClassIntercepts
+    {
+        [InterceptsLocation("C:\\MyFile\\Asp.NetCoreExperiment\\Asp.NetCoreExperiment\\Interceptors\\ABC.AOT\\MyClass.cs", 13, 18)]
+        public static void InterceptorPrint(this ABC.MyClass myclass, string s)
+        {
+            Console.WriteLine($"ABC.AOT下 MyClass.InterceptorPrint 拦截 ABC下MyClass.Print方法，参数是：{s}");
+
+        }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    internal sealed class InterceptsLocationAttribute(string filePath, int line, int column) : Attribute
+    {
+    }
+}
---
+++ Asp.NetCoreExperiment/Interceptors/ABC/MyClass.cs
@@ -0,0 +1,15 @@
+﻿namespace ABC
+{
+    public class MyClass
+    {
+        public void Print(string s)
+        {
+            Write(s);
+        }
+        void Write(string s)
+        {
+            System.Console.WriteLine($"MyClass的Print方法，参数是：{s}");
+        }
+
+    }
+}
