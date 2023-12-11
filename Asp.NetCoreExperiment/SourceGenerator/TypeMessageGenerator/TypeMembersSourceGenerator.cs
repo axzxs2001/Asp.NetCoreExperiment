80ed6cffeee463563ed29b80a606d2e92292d0f7
桂素伟 <axzxs2001@163.com>
2023-12-11 17:42:01 +0900

    fix

---
+++ Asp.NetCoreExperiment/SourceGenerator/TypeMessageGenerator/TypeMembersSourceGenerator.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace TypeMessageGenerator
+{
+    [Generator]
+    public class TypeMembersSourceGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            var mainMethod = context.Compilation.GetEntryPoint(context.CancellationToken);
+
+            var path = "C:\\MyFile\\abc\\mainMethod.txt";
+            File.WriteAllText(path, "");          
+            File.AppendAllText(path, context.Compilation.Assembly.Name + "\r\n");
+            File.AppendAllText(path, "========================\r\n");
+
+            foreach (var typename in context.Compilation.Assembly.TypeNames)
+            {
+                File.AppendAllText(path, typename + "\r\n");
+                try
+                {
+                    var mytype = context.Compilation.Assembly.GetTypeByMetadataName(typename);
+                    if (mytype == null)
+                    {
+                        mytype = context.Compilation.Assembly.GetTypeByMetadataName($"{context.Compilation.Assembly.Name}.{typename}");
+                    }
+                    if (mytype == null)
+                    {
+                        continue;
+                    }
+
+                    foreach (var member in mytype.GetMembers())
+                    {
+                        try
+                        {
+
+                            File.AppendAllText(path, $"-- {member.Name} {member.Kind} \r\n");
+                            foreach (var location in member.Locations)
+                            {
+                                try
+                                {
+
+                                    File.AppendAllText(path, $"---- {location.GetLineSpan().StartLinePosition.Line},{location.GetLineSpan().EndLinePosition.Character} {location.GetLineSpan().Path}\r\n");
+                                }
+                                catch (Exception exc)
+                                {
+                                    File.AppendAllText(path, $"1   {exc.Message} \r\n");
+                                }
+                            }
+                        }
+                        catch (Exception exc)
+                        {
+                            File.AppendAllText(path, $"2   {exc.Message} \r\n");
+                        }
+                    }
+                }
+                catch (Exception exc)
+                {
+                    File.AppendAllText(path, $"3   {exc.Message} \r\n");
+                }
+            }
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+    }
+}
---
+++ Asp.NetCoreExperiment/SourceGenerator/UserDemoTypeMessageGenerator/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace UserDemoTypeMessageGenerator
+{
+    public class Person
+    {
+        public int ID { get; set; }
+
+        public int Name { get; set; }
+
+        public void PrintPerson()
+        {
+        }
+    }
+}
