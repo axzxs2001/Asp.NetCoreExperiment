f2c0e24cf9f5b6f5a4d00a49866ab11d84e9e4fc
桂素伟 <axzxs2001@163.com>
2023-12-14 14:29:37 +0900

    fix pdf

---
+++ Asp.NetCoreExperiment/QuestPDF/PdfPigDemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig;
+
+using (PdfDocument document = PdfDocument.Open(@"C:\Documents\document.pdf"))
+{
+    foreach (Page page in document.GetPages())
+    {
+        string pageText = page.Text;
+
+        foreach (Word word in page.GetWords())
+        {
+            Console.WriteLine(word.Text);
+        }
+    }
+}
