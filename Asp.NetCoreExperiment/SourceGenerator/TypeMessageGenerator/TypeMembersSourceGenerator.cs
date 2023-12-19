27acdbc1b8dc4a614e5afbb304ee6e17ad14a288
桂素伟 <axzxs2001@163.com>
2023-12-19 16:56:05 +0900

    源生成器

--- Asp.NetCoreExperiment/SourceGenerator/TypeMessageGenerator/TypeMembersSourceGenerator.cs
+++ Asp.NetCoreExperiment/SourceGenerator/TypeMessageGenerator/TypeMembersSourceGenerator.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.CodeAnalysis;
+using System.IO;
+using System.Reflection;
 
 namespace TypeMessageGenerator
 {
@@ -7,62 +9,98 @@ namespace TypeMessageGenerator
     {
         public void Execute(GeneratorExecutionContext context)
         {
-            var mainMethod = context.Compilation.GetEntryPoint(context.CancellationToken);
-
-            var path = "C:\\MyFile\\abc\\mainMethod.txt";
-            File.WriteAllText(path, "");          
-            File.AppendAllText(path, context.Compilation.Assembly.Name + "\r\n");
-            File.AppendAllText(path, "========================\r\n");
-
-            foreach (var typename in context.Compilation.Assembly.TypeNames)
+            try
             {
-                File.AppendAllText(path, typename + "\r\n");
-                try
+                var path = "";
+                var mainMethod = context.Compilation.GetEntryPoint(context.CancellationToken);
+                foreach (var location in mainMethod.ContainingModule.Locations)
                 {
-                    var mytype = context.Compilation.Assembly.GetTypeByMetadataName(typename);
-                    if (mytype == null)
+                    if (location.GetLineSpan().Path.ToLower().Contains("program.cs"))
                     {
-                        mytype = context.Compilation.Assembly.GetTypeByMetadataName($"{context.Compilation.Assembly.Name}.{typename}");
-                    }
-                    if (mytype == null)
-                    {
-                        continue;
-                    }
+                        path = Path.GetDirectoryName(location.GetLineSpan().Path);
+                        break;
+                    }                    
+                }
+                var content = """
+                	<ItemGroup>
+                  		<None Remove="type.txt" />
+                	</ItemGroup>
+                	<ItemGroup>
+                  		<EmbeddedResource Include="type.txt" />
+                	</ItemGroup>
+                """;
 
-                    foreach (var member in mytype.GetMembers())
+                foreach (var filePath in Directory.GetFiles(path, "*.csproj"))
+                {
+                    if (!File.ReadAllText(filePath).Contains("type.txt"))
                     {
-                        try
+                        var list = new List<string>(File.ReadAllLines(filePath));
+                        list.Insert(list.Count - 1, content);
+                        File.WriteAllLines(filePath, list);
+                    }
+                }
+
+
+                path = path + "\\type.txt";
+                File.WriteAllText(path, "");
+                File.AppendAllText(path, context.Compilation.Assembly.Name + "\r\n");
+                File.AppendAllText(path, "========================\r\n");
+
+                foreach (var typename in context.Compilation.Assembly.TypeNames)
+                {
+                    File.AppendAllText(path, typename + "\r\n");
+                    try
+                    {
+                        var mytype = context.Compilation.Assembly.GetTypeByMetadataName(typename);
+                        if (mytype == null)
                         {
+                            mytype = context.Compilation.Assembly.GetTypeByMetadataName($"{context.Compilation.Assembly.Name}.{typename}");
+                        }
+                        if (mytype == null)
+                        {
+                            continue;
+                        }
 
-                            File.AppendAllText(path, $"-- {member.Name} {member.Kind} \r\n");
-                            foreach (var location in member.Locations)
+                        foreach (var member in mytype.GetMembers())
+                        {
+                            try
                             {
-                                try
-                                {
 
-                                    File.AppendAllText(path, $"---- {location.GetLineSpan().StartLinePosition.Line},{location.GetLineSpan().EndLinePosition.Character} {location.GetLineSpan().Path}\r\n");
-                                }
-                                catch (Exception exc)
+                                File.AppendAllText(path, $"-- {member.Name} {member.Kind} \r\n");
+                                foreach (var location in member.Locations)
                                 {
-                                    File.AppendAllText(path, $"1   {exc.Message} \r\n");
+                                    try
+                                    {
+
+                                        File.AppendAllText(path, $"---- {location.GetLineSpan().StartLinePosition.Line},{location.GetLineSpan().EndLinePosition.Character} {location.GetLineSpan().Path}\r\n");
+                                    }
+                                    catch (Exception exc)
+                                    {
+                                        File.AppendAllText(path, $"1   {exc.Message} \r\n");
+                                    }
                                 }
                             }
-                        }
-                        catch (Exception exc)
-                        {
-                            File.AppendAllText(path, $"2   {exc.Message} \r\n");
+                            catch (Exception exc)
+                            {
+                                File.AppendAllText(path, $"2   {exc.Message} \r\n");
+                            }
                         }
                     }
+                    catch (Exception exc)
+                    {
+                        File.AppendAllText(path, $"3   {exc.Message} \r\n");
+                    }
                 }
-                catch (Exception exc)
-                {
-                    File.AppendAllText(path, $"3   {exc.Message} \r\n");
-                }
+            }
+            catch (Exception exc)
+            {
+                File.AppendAllText(@"C:\MyFile\temp\error.txt", $" {exc.Message} \r\n");
             }
         }
 
         public void Initialize(GeneratorInitializationContext context)
         {
+
 
         }
     }
--- Asp.NetCoreExperiment/SourceGenerator/UserDemoTypeMessageGenerator/Program.cs
+++ Asp.NetCoreExperiment/SourceGenerator/UserDemoTypeMessageGenerator/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+var assembly = Assembly.GetExecutingAssembly();
+var stream = assembly.GetManifestResourceStream("UserDemoTypeMessageGenerator.type.txt");
+var reader = new StreamReader(stream!);
+Console.WriteLine(reader.ReadToEnd());
+
+
+Console.ReadLine();
