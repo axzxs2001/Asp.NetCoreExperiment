2224cc24c4b44436a18ba36280b4131226411e35
桂素伟 <axzxs2001@163.com>
2023-12-25 18:17:26 +0900

    fix

---
+++ Asp.NetCoreExperiment/aot/classlib/DllTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace classlib
+{
+    //发布命令 dotnet publish /p:NativeLib=Shared --use-current-runtime
+    public static class DllTest
+    {
+        [UnmanagedCallersOnly(EntryPoint = "int_add")]
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "string_add")]
+        public static IntPtr StringTest(IntPtr ptr1, IntPtr ptr22)
+        {
+            var s = Marshal.PtrToStringUTF8(ptr1) + Marshal.PtrToStringUTF8(ptr22);
+            return Marshal.StringToCoTaskMemUTF8(s);
+        }
+
+        [UnmanagedCallersOnly(EntryPoint = "get_strings")]
+        public static IntPtr StringTest()
+        {
+            var arr = new string[] { "a", "b", "c" }; 
+          
+            return Marshal.UnsafeAddrOfPinnedArrayElement(arr, 0);         
+        }
+    }
+}
+
---
+++ Asp.NetCoreExperiment/aot/classlibTest/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+Console.WriteLine(MyClass.int_add(1, 2));
+Console.WriteLine(MyClass.string_add("abcd", "efg"));
+
+var resultArray = new nint[6];
+var resultPtr = MyClass.get_strings();
+
+//for (int i = 0; i < 3; i++)
+//{
+//    var value = Marshal.ReadIntPtr(resultPtr + i);
+//    Console.WriteLine(Marshal.PtrToStringBSTR(value));
+//}
+//Marshal.Copy(resultPtr, resultArray, 0, resultArray.Length);
+//foreach (var i in resultArray)
+//{
+//    Console.WriteLine(Marshal.PtrToStringUTF8(i));
+//}
+
+public class MyClass
+{
+    // 定义非托管函数的签名
+    [DllImport("classlib.dll")] // 指定DLL文件名
+    public static extern int int_add(int a, int b);
+
+    [DllImport("classlib.dll")] // 指定DLL文件名
+    public static extern string string_add(string a, string b);
+
+
+    [DllImport("classlib.dll")] // 指定DLL文件名
+    public static extern IntPtr get_strings();
+}
