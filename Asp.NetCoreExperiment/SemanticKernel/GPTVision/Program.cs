cc7b1905b67907544173409254285192c85e61b0
桂素伟 <axzxs2001@163.com>
2023-12-27 16:20:37 +0900

    上传图片

---
+++ Asp.NetCoreExperiment/SemanticKernel/GPTVision/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel;
+
+
+var key = File.ReadAllText(@"C:\GPT\key.txt");
+const string ImageUri = "https://upload.wikimedia.org/wikipedia/commons/d/d5/Half-timbered_mansion%2C_Zirkel%2C_East_view.jpg";
+
+var kernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion("gpt-4-vision-preview", key)
+    .Build();
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+var chatHistory = new ChatHistory("You are a friendly assistant.");
+
+chatHistory.AddUserMessage(new ChatMessageContentItemCollection
+        {
+            new TextContent("这个图片中有什么"),
+            new ImageContent(new Uri(ImageUri))
+        });
+
+
+var list = chatCompletionService.GetStreamingChatMessageContentsAsync(chatHistory);
+
+await foreach (var item in list)
+{
+    Console.Write($"{item.Content}");
+}
+
+//var reply = await chatCompletionService.GetChatMessageContentAsync(chatHistory);
+//Console.WriteLine(reply.Content);
+
+Console.ReadKey();
