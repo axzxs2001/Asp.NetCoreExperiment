299a29ea967392a8e40a09179b4205385932075b
桂素伟 <axzxs2001@163.com>
2023-12-27 15:41:10 +0900

    chatGPT

---
+++ Asp.NetCoreExperiment/SemanticKernel/ChatGPT/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+await OpenAIChatSampleAsync();
+
+static async Task OpenAIChatSampleAsync()
+{
+    Console.WriteLine("======== Open AI - ChatGPT ========");
+    var key = File.ReadAllText(@"C:\GPT\key.txt");
+    var chatModelId = "gpt-4";
+    OpenAIChatCompletionService chatCompletionService = new(chatModelId, key);
+    await StartChatAsync(chatCompletionService);
+}
+
+static async Task StartChatAsync(IChatCompletionService chatGPT)
+{
+    Console.WriteLine("开始聊天:");
+    Console.WriteLine("------------------------");
+
+    var chatHistory = new ChatHistory("你是一个聊天机器人");
+
+    while (true)
+    {
+        Console.WriteLine("您输入问题：");
+        chatHistory.AddUserMessage(Console.ReadLine());
+        var reply = await chatGPT.GetChatMessageContentAsync(chatHistory);
+        await MessageStreamOutputAsync(chatGPT, chatHistory);
+        chatHistory.Add(reply);
+        Console.WriteLine();
+    }
+}
+
+static async Task MessageStreamOutputAsync(IChatCompletionService chatGPT, ChatHistory chatHistory)
+{
+    var list = chatGPT.GetStreamingChatMessageContentsAsync(chatHistory);
+    var first = true;
+    AuthorRole? role = AuthorRole.Assistant;
+    var fullMessage = string.Empty;
+    await foreach (var item in list)
+    {
+        if (first)
+        {
+            role = item.Role;
+            first = false;
+            Console.Write($"{role}：");
+        }
+        if (item == null)
+        {
+            continue;
+        }
+        fullMessage += item.Content;
+        Console.Write($"{item.Content}");
+    }
+    chatHistory.AddMessage(role.Value, fullMessage);
+}
---
+++ Asp.NetCoreExperiment/SemanticKernel/Demo01/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
+
+var key = File.ReadAllText(@"C:\GPT\key.txt");
+
+ILoggerFactory myLoggerFactory = NullLoggerFactory.Instance;
+var builder = Kernel.CreateBuilder();
+builder.Services.AddSingleton(myLoggerFactory);
+
+builder.AddOpenAIChatCompletion(
+         "gpt-3.5-turbo",
+         key);
+
+var kernel = builder.Build();
+
+
+
+
+
+
+var prompt = @"{{$input}}
+
+One line TLDR with the fewest words.";
+
+var summarize = kernel.CreateFunctionFromPrompt(prompt, executionSettings: new OpenAIPromptExecutionSettings { MaxTokens = 100 });
+
+string text1 = @"
+1st Law of Thermodynamics - Energy cannot be created or destroyed.
+2nd Law of Thermodynamics - For a spontaneous process, the entropy of the universe increases.
+3rd Law of Thermodynamics - A perfect crystal at zero Kelvin has zero entropy.";
+
+string text2 = @"
+1. An object at rest remains at rest, and an object in motion remains in motion at constant speed and in a straight line unless acted on by an unbalanced force.
+2. The acceleration of an object depends on the mass of the object and the amount of force applied.
+3. Whenever one object exerts a force on another object, the second object exerts an equal and opposite on the first.";
+
+Console.WriteLine(await kernel.InvokeAsync(summarize, new KernelArguments() { ["input"] = text1 }));
+
+Console.WriteLine(await kernel.InvokeAsync(summarize, new KernelArguments() { ["input"] = text2 }));
+
+// Output:
+//   Energy conserved, entropy increases, zero entropy at 0K.
+//   Objects move in response to forces.
+
+Console.ReadLine();
