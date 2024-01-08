0996e3690121539aa557ab390446c5de5e155c0f
gsw <axzxs2001@163.com>
2024-01-08 11:17:50 +0900

    知识库

---
+++ Asp.NetCoreExperiment/SemanticKernel/PluginsDemo01/Program.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using System.ComponentModel;
+
+var key = File.ReadAllText(@"C:\GPT\key.txt");
+
+ILoggerFactory myLoggerFactory = NullLoggerFactory.Instance;
+var builder = Kernel.CreateBuilder();
+builder.Plugins.AddFromType<TimeInformation>();
+builder.Services.AddSingleton(myLoggerFactory);
+
+builder.AddOpenAIChatCompletion(
+         "gpt-3.5-turbo",
+         key);
+
+var kernel = builder.Build();
+
+
+
+
+// Add the handlers to the kernel
+#pragma warning disable SKEXP0004 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
+kernel.PromptRendering += MyRenderingHandler;
+kernel.PromptRendered += MyRenderedHandler;
+
+KernelArguments arguments = new() { { "card_number", "4444 3333 2222 1111" } };
+Console.WriteLine(await kernel.InvokePromptAsync("告诉我有关该信用卡号的一些有用信息 {{$card_number}}?", arguments));
+
+Console.ReadLine();
+//案例
+#pragma warning disable SKEXP0004 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
+void MyRenderingHandler(object? sender, PromptRenderingEventArgs e)
+{
+    if (e.Arguments.ContainsName("card_number"))
+    {
+        e.Arguments["card_number"] = "**** **** **** ****";
+    }
+}
+// Handler which is called after a prompt is rendered
+void MyRenderedHandler(object? sender, PromptRenderedEventArgs e)
+{
+    e.RenderedPrompt += " 没有性别歧视、种族主义或其他偏见/偏执";
+
+    Console.WriteLine(e.RenderedPrompt);
+}
+#pragma warning disable SKEXP0004 // 类型仅用于评估，在将来的更新中可能会被更改或删除。取消此诊断以继续。
+
+
+//案例
+
+//string chatPrompt = @"
+//            <message role=""user"">What is Seattle?</message>
+//            <message role=""system"">Respond with JSON.</message>
+//        ";
+//Console.WriteLine(await kernel.InvokePromptAsync(chatPrompt));
+
+
+
+//案例
+
+// Example 1. Invoke the kernel with a prompt that asks the AI for inromation it cannot provide and may hallucinate
+//Console.WriteLine(await kernel.InvokePromptAsync("中国春节还有多少天?"));
+
+// Example 2. Invoke the kernel with a templated prompt that invokes a plugin and display the result
+//Console.WriteLine(await kernel.InvokePromptAsync("现在是{{TimeInformation.GetCurrentUtcTime}}. 中国春节还有多少天?"));
+
+// Example 3. Invoke the kernel with a prompt and allow the AI to automatically invoke functions
+//OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+//Console.WriteLine(await kernel.InvokePromptAsync("现在是什么日期？中国春节还有多少天? 解释你的思考.", new(settings)));
+
+
+
+
+/// <summary>
+/// A plugin that returns the current time.
+/// </summary>
+public class TimeInformation
+{
+    [KernelFunction]
+    [Description("返回一个UTC时间")]
+    public string GetCurrentUtcTime() => DateTime.UtcNow.ToString("R");
+}
---
+++ Asp.NetCoreExperiment/SemanticKernel/SemanticMemory/Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Memory;
+const string MemoryCollectionName = "SKGitHub";
+
+
+await OpenAIChatSampleAsync();
+Console.ReadLine();
+static async Task OpenAIChatSampleAsync()
+{
+    var key = File.ReadAllText(@"C:\GPT\key.txt");
+    //var chatModelId = "gpt-4";
+    //OpenAIChatCompletionService chatCompletionService = new(chatModelId, key);
+
+#pragma warning disable SKEXP0003
+#pragma warning disable SKEXP0011
+#pragma warning disable SKEXP0052
+    var memoryWithCustomDb = new MemoryBuilder()
+        .WithOpenAITextEmbeddingGeneration("text-embedding-ada-002", key)
+        .WithMemoryStore(new VolatileMemoryStore())
+        .Build();
+    await RunExampleAsync(memoryWithCustomDb);
+
+
+}
+
+
+static async Task RunExampleAsync(ISemanticTextMemory memory)
+{
+    await StoreMemoryAsync(memory);
+
+    await SearchMemoryAsync(memory, "你多大了?");
+
+    await SearchMemoryAsync(memory, "你叫什么?");
+
+
+}
+
+static async Task SearchMemoryAsync(ISemanticTextMemory memory, string query)
+{
+    Console.WriteLine("\nQuery: " + query + "\n");
+
+    var memoryResults = memory.SearchAsync(MemoryCollectionName, query, limit: 2, minRelevanceScore: 0.5);
+
+    int i = 0;
+    await foreach (MemoryQueryResult memoryResult in memoryResults)
+    {
+        Console.WriteLine($"Result {++i}:");
+        Console.WriteLine("  URL:     : " + memoryResult.Metadata.Id);
+        Console.WriteLine("  Title    : " + memoryResult.Metadata.Description);
+        Console.WriteLine("  Relevance: " + memoryResult.Relevance);
+        Console.WriteLine();
+    }
+
+    Console.WriteLine("----------------------");
+}
+
+static async Task StoreMemoryAsync(ISemanticTextMemory memory)
+{
+    var githubFiles = SampleData();
+    var i = 0;
+    foreach (var entry in githubFiles)
+    {
+        await memory.SaveReferenceAsync(
+            collection: MemoryCollectionName,
+            externalSourceName: "GitHub",
+            externalId: entry.Key,
+            description: entry.Value,
+            text: entry.Value);
+
+        Console.Write($" #{++i} saved.");
+    }
+
+    Console.WriteLine("\n----------------------");
+}
+
+static Dictionary<string, string> SampleData()
+{
+    return new Dictionary<string, string>
+    {
+        ["姓名"] = "说明：我叫桂素伟",
+        ["年龄"] = "说明：今年21岁"
+        //["https://github.com/microsoft/semantic-kernel/blob/main/README.md"]
+        //    = "README: Installation, getting started, and how to contribute",
+        //["https://github.com/microsoft/semantic-kernel/blob/main/dotnet/notebooks/02-running-prompts-from-file.ipynb"]
+        //    = "Jupyter notebook describing how to pass prompts from a file to a semantic plugin or function",
+        //["https://github.com/microsoft/semantic-kernel/blob/main/dotnet/notebooks//00-getting-started.ipynb"]
+        //    = "Jupyter notebook describing how to get started with the Semantic Kernel",
+        //["https://github.com/microsoft/semantic-kernel/tree/main/samples/plugins/ChatPlugin/ChatGPT"]
+        //    = "Sample demonstrating how to create a chat plugin interfacing with ChatGPT",
+        //["https://github.com/microsoft/semantic-kernel/blob/main/dotnet/src/SemanticKernel/Memory/VolatileMemoryStore.cs"]
+        //    = "C# class that defines a volatile embedding store",
+    };
+}
+
