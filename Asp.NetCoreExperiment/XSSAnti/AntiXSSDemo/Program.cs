8a9f877721f8144762187105d42c14e1ae7bd375
桂素伟 <axzxs2001@163.com>
2024-01-31 14:50:30 +0900

    fix

--- Asp.NetCoreExperiment/XSSAnti/AntiXSSDemo/Program.cs
+++ Asp.NetCoreExperiment/XSSAnti/AntiXSSDemo/Program.cs
@@ -1,64 +1,87 @@
 ﻿using Ganss.Xss;
 using OWASP.AntiSamy.Html;
 using System.Globalization;
+using System.Reflection;
+using System.Xml;
 
-Policy policy = Policy.GetInstance(TestConstants.DEFAULT_POLICY_PATH);
+List<int> subset = new List<int> { 1, 2, 3 ,0};
+List<int> superset = new List<int> { 1, 2, 3, 4, 5, 6 };
+
+// 使用LINQ判断subset是否是superset的子集
+bool isSubset = subset.All(element => superset.Contains(element));
+
+// 输出结果
+Console.WriteLine(isSubset ? "是子集" : "不是子集");
 
 
-var html = "test<script>alert(document.cookie)</script>";
-html = "test<a onclick='alert(document.cookie)'>pee</a>";
 
-
-//Thread.CurrentThread.CurrentCulture = new CultureInfo("ja-JP", true);
-//Thread.CurrentThread.CurrentUICulture = new CultureInfo("ja-JP", true);
-html = "<script>al()</script><p>各位观众</p><p><br></p><a target='_blank'></a>\r\n";
-var antiSamy = new AntiSamy();
-CleanResults results = antiSamy.Scan(html, policy);
-
-Console.WriteLine(results.GetNumberOfErrors());
-
-foreach (var message in results.GetErrorMessages())
+var assembly = Assembly.GetExecutingAssembly();
+var resourceName = "AntiXSSDemo.AntiSamyPolicyExamples.antisamy.xml";
+var html = "";
+using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+//using (StreamReader reader = new StreamReader(stream))
 {
-    Console.WriteLine(message);
+    //string result = reader.ReadToEnd();
+
+
+    //Policy policy = Policy.GetInstance(TestConstants.DEFAULT_POLICY_PATH);
+
+
+    html = "test<script>alert(document.cookie)</script>";
+    html = "test<a onClick='alert(document.cookie)'>pee</a>";
+
+
+    //Thread.CurrentThread.CurrentCulture = new CultureInfo("ja-JP", true);
+    //Thread.CurrentThread.CurrentUICulture = new CultureInfo("ja-JP", true);
+    html = "<script>al()</script><p>各位观众</p><p><br></p><a onClick='alert(document.cookie)' onFocus=\"alter()\" target=\"_blank\"></a>\r\n";
+    //    var antiSamy = new AntiSamy();
+    //    CleanResults results = antiSamy.Scan(html, stream);
+
+    //    Console.WriteLine(results.GetNumberOfErrors());
+
+    //    foreach (var message in results.GetErrorMessages())
+    //    {
+    //        Console.WriteLine(message);
+    //    }
+
+
+    //    Console.WriteLine(results.GetCleanHtml()); // Some custom function
+    //}
+    //Console.ReadLine();
+
+    Console.WriteLine("-------------------------");
+    var sanitizer = new HtmlSanitizer();
+    sanitizer.RemovingAttribute += (s, e) =>
+    {
+        Console.WriteLine($"Removing attribute {e.Attribute} from element {e.Tag}");
+    };
+    sanitizer.RemovingTag += (s, e) =>
+    {
+        Console.WriteLine($"Removing tag {e.Tag}");
+    };
+    sanitizer.RemovingStyle += (s, e) =>
+    {
+        Console.WriteLine($"Removing style {e.Style}");
+    };
+    sanitizer.RemovingAtRule += (s, e) =>
+    {
+        Console.WriteLine($"Removing at-rule {e.Rule}");
+    };
+    sanitizer.RemovingComment += (s, e) =>
+    {
+        Console.WriteLine($"Removing comment {e.Comment}");
+    };
+    sanitizer.RemovingCssClass += (s, e) =>
+    {
+        Console.WriteLine($"Removing css class {e.CssClass}");
+    };
+
+    var formatter = AngleSharp.Xhtml.XhtmlMarkupFormatter.Instance;
+
+    Console.WriteLine(sanitizer.Sanitize(html, outputFormatter: formatter));
+
+
 }
-
-
-Console.WriteLine(results.GetCleanHtml()); // Some custom function
-
-
-Console.WriteLine("-------------------------");
-var sanitizer = new HtmlSanitizer();
-sanitizer.RemovingAttribute += (s, e) =>
-{
-    Console.WriteLine($"Removing attribute {e.Attribute} from element {e.Tag}");
-};
-sanitizer.RemovingTag += (s, e) =>
-{
-    Console.WriteLine($"Removing tag {e.Tag}");
-};
-sanitizer.RemovingStyle += (s, e) =>
-{
-    Console.WriteLine($"Removing style {e.Style}");
-};
-sanitizer.RemovingAtRule += (s, e) =>
-{
-    Console.WriteLine($"Removing at-rule {e.Rule}");
-};
-sanitizer.RemovingComment += (s, e) =>
-{
-    Console.WriteLine($"Removing comment {e.Comment}");
-};
-sanitizer.RemovingCssClass += (s, e) =>
-{
-    Console.WriteLine($"Removing css class {e.CssClass}");
-};
-
-var formatter = AngleSharp.Xhtml.XhtmlMarkupFormatter.Instance;
-
-Console.WriteLine(sanitizer.Sanitize(html, outputFormatter: formatter));
-
-
-
 
 public static class TestConstants
 {
