6c6e04a3fbee54ec40a1ff18b3183453eca22950
gsw <axzxs2001@163.com>
2024-02-03 09:57:55 +0900

    scheduler

---
+++ Asp.NetCoreExperiment/DOTNET8/schedulerdemo/Program.cs
@@ -0,0 +1,15 @@
+﻿using Coravel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+builder.Services.AddScheduler();
+var app = builder.Build();
+app.Services.UseScheduler(scheduler =>
+{
+    scheduler.Schedule(
+        () => Console.WriteLine($"{DateTime.Now:HH:mm:ss.fffffff}")
+    )
+    .EverySeconds(2);
+});
+app.Run();
