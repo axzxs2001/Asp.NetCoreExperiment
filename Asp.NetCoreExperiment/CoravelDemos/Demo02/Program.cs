bc8f5b3474d783c3b3a239ba5f8649e9a3a3b603
桂素伟 <axzxs2001@163.com>
2024-02-06 16:00:00 +0900

    fix

--- Asp.NetCoreExperiment/CoravelDemos/Demo02/Program.cs
+++ Asp.NetCoreExperiment/CoravelDemos/Demo02/Program.cs
@@ -7,58 +7,39 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<MyQueueInvocable>();
 builder.Services.AddQueue();
 
-
 var app = builder.Build();
-//app.Services.ConfigureQueue();
 
-
-// Configure the HTTP request pipeline.
-
-var summaries = new[]
+app.MapGet("/addqueue1", (IQueue queue) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", (IQueue queue) =>
-{
-    //queue.QueueInvocable<MyQueueInvocable>();
-
-    var guid = queue.QueueInvocableWithPayload<MyQueueInvocable, string>("开始Queue，添加时间：" + DateTime.Now);
-
+    var guid = queue.QueueTask(() =>
+    {
+        app.Logger.LogInformation("简单的Queue");
+    });
     app.Logger.LogInformation(guid.ToString());
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
+});
 
-
-    return forecast;
+app.MapGet("/addqueue2", (IQueue queue) =>
+{
+    var guid = queue.QueueInvocableWithPayload<MyQueueInvocable, string>("开始Queue，添加时间：" + DateTime.Now);
+    app.Logger.LogInformation(guid.ToString());
 });
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
-
-
+/// <summary>
+/// 负载
+/// </summary>
 public class MyQueueInvocable : IInvocable, IInvocableWithPayload<string>
 {
-
     readonly ILogger<MyQueueInvocable> _logger;
     public MyQueueInvocable(ILogger<MyQueueInvocable> logger)
     {
         _logger = logger;
     }
-    public string Payload { get; set; }
-
+    public string Payload { get; set; } = string.Empty;
     public async Task Invoke()
     {
         _logger.LogInformation(Payload + "，执行时间：{time}", DateTime.Now);
+        await Task.CompletedTask;
     }
 }
