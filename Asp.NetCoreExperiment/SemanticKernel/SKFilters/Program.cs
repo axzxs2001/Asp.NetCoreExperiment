2eec155a02f881d8890160774636c8f4183a12d2
gsw <axzxs2001@163.com>
2024-05-19 15:14:39 +0900

    fix

---
+++ Asp.NetCoreExperiment/SemanticKernel/SKFilters/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.SemanticKernel;
+#pragma warning disable SKEXP0001
+var key = File.ReadAllText(@"C:\GPT\key.txt");
+var chatModelId = "gpt-4";
+
+var builder = Kernel.CreateBuilder();
+
+builder.AddOpenAIChatCompletion(chatModelId, key);
+
+builder.Services.AddSingleton<IFunctionInvocationFilter, FirstFunctionFilter>();
+builder.Services.AddSingleton<IFunctionInvocationFilter, SecondFunctionFilter>();
+
+var kernel = builder.Build();
+
+var function = kernel.CreateFunctionFromPrompt("What is Seattle", functionName: "MyFunction");
+kernel.Plugins.Add(KernelPluginFactory.CreateFromFunctions("MyPlugin", functions: [function]));
+var result = await kernel.InvokeAsync(kernel.Plugins["MyPlugin"]["MyFunction"]);
+
+Console.WriteLine(result);
+
+
+class FirstFunctionFilter : IFunctionInvocationFilter
+{
+
+    public async Task OnFunctionInvocationAsync(FunctionInvocationContext context, Func<FunctionInvocationContext, Task> next)
+    {
+        Console.WriteLine($"{nameof(FirstFunctionFilter)}.FunctionInvoking - {context.Function.PluginName}.{context.Function.Name}");
+
+      
+        await next(context);
+
+        Console.WriteLine($"{nameof(FirstFunctionFilter)}.FunctionInvoked - {context.Function.PluginName}.{context.Function.Name}");
+    }
+}
+
+class SecondFunctionFilter : IFunctionInvocationFilter
+{
+    public async Task OnFunctionInvocationAsync(FunctionInvocationContext context, Func<FunctionInvocationContext, Task> next)
+    {
+        Console.WriteLine($"{nameof(SecondFunctionFilter)}.FunctionInvoking - {context.Function.PluginName}.{context.Function.Name}");
+
+        await next(context);
+
+        Console.WriteLine($"{nameof(SecondFunctionFilter)}.FunctionInvoked - {context.Function.PluginName}.{context.Function.Name}");
+    }
+}
---
+++ Asp.NetCoreExperiment/SemanticKernelArticle/Demo01-Kernel/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.SemanticKernel;
+
+
+
+var chatModelId = "gpt-4";
+var key = File.ReadAllText(@"C:\GPT\key.txt");
+#pragma warning disable SKEXP0010
+Kernel kernel = Kernel.CreateBuilder()
+    .AddOpenAITextToImage(key) // Add your text to image service
+    .AddOpenAIChatCompletion(chatModelId, key) // Add your chat completion service
+    .Build();
