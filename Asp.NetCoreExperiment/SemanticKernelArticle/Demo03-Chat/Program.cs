73309096e1ad01969e2cbd100f562b1d9e3c6ce2
桂素伟 <axzxs2001@163.com>
2024-06-24 10:43:16 +0900

    fix

---
+++ Asp.NetCoreExperiment/SemanticKernelArticle/Demo03-Chat/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+
+var chatModelId = "gpt-4o";
+var key = File.ReadAllText(@"C:\GPT\key.txt");
+
+var kernel = Kernel.CreateBuilder()
+    .AddOpenAIChatCompletion(chatModelId, key)
+   .Build();
+
+var chatHistory = new ChatHistory(systemMessage: "")
+{
+
+};
+var chat = kernel.GetRequiredService<IChatCompletionService>();
+chatHistory.AddUserMessage("Hi, I'm looking for new power tools, any suggestion?");
+
+
+var settings = new PromptExecutionSettings
+{
+    ExtensionData = new Dictionary<string, object>
+    {
+        ["max_tokens"] = 1000,
+        ["temperature"] = 0.2,
+        ["top_p"] = 0.8,
+        ["presence_penalty"] = 0.0,
+        ["frequency_penalty"] = 0.0
+    }
+};
+
+var reply = await chat.GetChatMessageContentAsync(chatHistory, settings);
