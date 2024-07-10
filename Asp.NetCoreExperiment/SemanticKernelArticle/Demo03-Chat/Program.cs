c5e997144f8711b7497e663eadb784e991edd86c
桂素伟 <axzxs2001@163.com>
2024-07-10 10:21:07 +0900

    fix

--- Asp.NetCoreExperiment/SemanticKernelArticle/Demo03-Chat/Program.cs
+++ Asp.NetCoreExperiment/SemanticKernelArticle/Demo03-Chat/Program.cs
@@ -1,21 +1,35 @@
 ﻿using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
 using System.Runtime.CompilerServices;
 using System.Text;
 
 var chatModelId = "gpt-4o";
-var geminiModelId = "gemini-1.5-flash-latest";
-var geminiKey = File.ReadAllText(@"C:\GPT\gemini.txt");
+//var geminiModelId = "gemini-1.5-flash-latest";
+//var geminiKey = File.ReadAllText(@"C:\GPT\gemini.txt");
 var key = File.ReadAllText(@"C:\GPT\key.txt");
 #pragma warning disable SKEXP0070
+#pragma warning disable SKEXP0010
+#pragma warning disable SKEXP0001
+#pragma warning disable SKEXP0110
 var kernel = Kernel.CreateBuilder()
    .AddOpenAIChatCompletion(chatModelId, key)
-   .AddGoogleAIGeminiChatCompletion(geminiModelId, geminiKey)
+   //.AddGoogleAIGeminiChatCompletion(geminiModelId, geminiKey)
+   .AddOpenAIFiles(apiKey: key)
    .Build();
 
-var chatHistory = new ChatHistory(systemMessage: "你是一位.net高级讲师，回答问题言简意赅。");
+var chatHistory = new ChatHistory();// new ChatHistory(systemMessage: "你是一位.net高级讲师，回答问题言简意赅。");
+
 var chat = kernel.GetRequiredService<IChatCompletionService>();
 
+//var fileSev = kernel.GetRequiredService<OpenAIFileService>();
+//var refFile = await fileSev.UploadContentAsync(new BinaryContent(File.ReadAllBytes(@"C:\NetStars\短域名服务\短域名服务会访纪要_202103121100.docx"), "text/plain"), new OpenAIFileUploadExecutionSettings("短域名服务会访纪要_202103121100.docx", OpenAIFilePurpose.Assistants));
+chatHistory.AddUserMessage(new ChatMessageContentItemCollection()
+{
+    //new FileReferenceContent(refFile.Id)
+   
+    new BinaryContent(File.ReadAllBytes(@"C:\NetStars\短域名服务\短域名服务会访纪要_202103121100.docx"),"application/msword")
+});
 var settings = new PromptExecutionSettings
 {
     ExtensionData = new Dictionary<string, object>
@@ -31,7 +45,7 @@ while (true)
 {
     Console.ResetColor();
     Console.WriteLine("----------学生提问：----------");
-    chatHistory.AddUserMessage(Console.ReadLine());
+    chatHistory.AddUserMessage("参会的人都有谁");
     Console.WriteLine();
 
     //var reply = await chat.GetChatMessageContentAsync(chatHistory, settings);
