5c840b06d8e9607e4631d27a3ce6c5628f8280f3
桂素伟 <axzxs2001@163.com>
2024-08-13 09:35:57 +0900

    fix 文生图

--- Asp.NetCoreExperiment/SemanticKernelArticle/Demo07-TextToPicture/Program.cs
+++ Asp.NetCoreExperiment/SemanticKernelArticle/Demo07-TextToPicture/Program.cs
@@ -7,7 +7,7 @@ Console.WriteLine("开始生成……");
 #pragma warning disable SKEXP0010
 var key = File.ReadAllText(@"C:\GPT\key.txt");
 var kernel = Kernel.CreateBuilder()
-    .AddOpenAITextToImage(key,modelId:"gpt-4o")
+    .AddOpenAITextToImage(key,modelId: "dall-e-3")
     .Build();
 var prompt1 = $"背景是白色，用墨水，画一匹腾空跃起的骏马。要求马位中图的中央，显示完整。";
 await CreateImageAsync(prompt1);
@@ -16,17 +16,17 @@ async Task CreateImageAsync(string prompt)
 {
 #pragma warning disable SKEXP0001
     var dallE = kernel.GetRequiredService<ITextToImageService>();
-    var imageUrl = await dallE.GenerateImageAsync(prompt, 1024, 1024);
+    var imageUrl = await dallE.GenerateImageAsync(prompt, 1024, 1792);
     await DownLoadImageAsync(imageUrl);
 }
 async Task DownLoadImageAsync(string imageUrl)
 {
-    string localPath = "downloaded_image.jpg";
-    using (HttpClient client = new HttpClient())
+    var localPath = "downloaded_image.jpg";
+    using (var client = new HttpClient())
     {
-        HttpResponseMessage response = await client.GetAsync(imageUrl);
+        var response = await client.GetAsync(imageUrl);
         response.EnsureSuccessStatusCode();
-        byte[] imageBytes = await response.Content.ReadAsByteArrayAsync();
+        var imageBytes = await response.Content.ReadAsByteArrayAsync();
         await File.WriteAllBytesAsync(localPath, imageBytes);
         Console.WriteLine("图片下载成功，保存在：" + localPath);
     }
