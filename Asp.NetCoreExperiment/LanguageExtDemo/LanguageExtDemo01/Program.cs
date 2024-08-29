72f4765bae4371d0dec0ad807e1f223c3ca9e7ab
桂素伟 <axzxs2001@163.com>
2024-08-29 17:37:18 +0900

    fix

---
+++ Asp.NetCoreExperiment/LanguageExtDemo/LanguageExtDemo01/Program.cs
@@ -0,0 +1,14 @@
+﻿using LanguageExt;
+using LanguageExt.Common;
+
+Option<int> x = Some(123);
+Option<int> y = Option<int>.None;
+
+Option<int> Some(int i) => i + 123;
+
+Console.WriteLine();
+
+Result<T> fff()
+{
+    return new Result<T>(new Exception("ddd"));
+}
