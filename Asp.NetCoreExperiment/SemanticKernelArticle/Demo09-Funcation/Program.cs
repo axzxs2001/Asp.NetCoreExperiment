19342ac78d56925433f678d62aa7e8046fce2fa9
桂素伟 <axzxs2001@163.com>
2024-09-04 17:41:29 +0900

    Funcation完成

--- Asp.NetCoreExperiment/SemanticKernelArticle/Demo09-Funcation/Program.cs
+++ Asp.NetCoreExperiment/SemanticKernelArticle/Demo09-Funcation/Program.cs
@@ -2,35 +2,94 @@
 using System.Text;
 using System.Threading;
 using System.Collections.Generic;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using System.ComponentModel;
+using System.Globalization;
 
-await foreach (var item in GetDataAsync())
+var chatModelId = "gpt-4o";
+var key = File.ReadAllText(@"C:\GPT\key.txt");
+
+
+var kernel = Kernel.CreateBuilder()
+   .AddOpenAIChatCompletion(chatModelId, key)
+   .Build();
+
+
+//string FunctionDefinition = "请给我5个专有名词关于: {{$input}}";
+//var myFunction = kernel.CreateFunctionFromPrompt(FunctionDefinition);
+//var result = await kernel.InvokeAsync(myFunction, new() { ["input"] = ".NET" });
+//Console.WriteLine(result.GetValue<string>());
+//Console.WriteLine(JsonSerializer.Serialize(result.Metadata?["Usage"]));
+
+var plugin = new FunctionsChainingPlugin();
+var myFunction = kernel.CreateFunctionFromMethod(typeof(FunctionsChainingPlugin).GetMethod("Function1Async"),target: plugin);
+var result = await myFunction.InvokeAsync(kernel);
+var customType = result.GetValue<MyCustomType>();
+
+Console.WriteLine($"CustomType.Number: {customType!.Number}");
+Console.WriteLine($"CustomType.Text: {customType.Text}");
+
+//var result = await kernel.InvokePromptAsync("1 + 1 = ?");
+//Console.WriteLine(result.GetValue<string>());
+//Console.WriteLine(JsonSerializer.Serialize(result.Metadata?["Usage"]));
+
+
+//var functions = kernel.ImportPluginFromType<FunctionsChainingPlugin>();
+//var customType = await kernel.InvokeAsync<MyCustomType>(functions["Function1"]);
+//Console.WriteLine($"CustomType.Number: {customType!.Number}");
+//Console.WriteLine($"CustomType.Text: {customType.Text}");
+
+
+Console.WriteLine();
+
+
+class FunctionsChainingPlugin
 {
-    Console.WriteLine(item);
+    private const string PluginName = nameof(FunctionsChainingPlugin);
+
+    [KernelFunction]
+    public async Task<MyCustomType> Function1Async(Kernel kernel)
+    {
+        return new MyCustomType
+        {
+            Number = 1,
+            Text = "Function1"
+        };
+    }
 }
 
-async IAsyncEnumerable<string> GetDataAsync()
+class MyCustomType
 {
-    var requestDto = """
+    public int Number { get; set; }
+
+    public string? Text { get; set; }
+}
+
+/// <summary>
+/// Implementation of <see cref="TypeConverter"/> for <see cref="MyCustomType"/>.
+/// In this example, object instance is serialized with <see cref="JsonSerializer"/> from System.Text.Json,
+/// but it's possible to convert object to string using any other serialization logic.
+/// </summary>
+class MyCustomTypeConverter : TypeConverter
+{
+    public override bool CanConvertFrom(ITypeDescriptorContext? context, Type sourceType) => true;
+
+    /// <summary>
+    /// This method is used to convert object from string to actual type. This will allow to pass object to
+    /// method function which requires it.
+    /// </summary>
+    public override object? ConvertFrom(ITypeDescriptorContext? context, CultureInfo? culture, object value)
     {
-      "model": "phi3.5",
-      "prompt": ".net是什么，请给出解释？", 
-      "stream": true
+        return JsonSerializer.Deserialize<MyCustomType>((string)value);
     }
-    """;
-    var content = new StringContent(requestDto, Encoding.UTF8, "application/json");
-    var client = new HttpClient();
-    var response = await client.PostAsync("http://localhost:11434/api/generate", content);
-    var responseStream = await response.Content.ReadAsStreamAsync();
-    var streamReader = new StreamReader(responseStream);
-    while (!streamReader.EndOfStream)
+
+    /// <summary>
+    /// This method is used to convert actual type to string representation, so it can be passed to AI
+    /// for further processing.
+    /// </summary>
+    public override object? ConvertTo(ITypeDescriptorContext? context, CultureInfo? culture, object? value, Type destinationType)
     {
-        //var arr = new byte[1024];
-        //await responseStream.ReadAsync(arr, CancellationToken.None);
-        //yield return Encoding.UTF8.GetString(arr);
-        var line = await streamReader.ReadLineAsync();
-        if (!string.IsNullOrEmpty(line))
-        {
-            yield return line;
-        }
+        return JsonSerializer.Serialize(value);
     }
 }
