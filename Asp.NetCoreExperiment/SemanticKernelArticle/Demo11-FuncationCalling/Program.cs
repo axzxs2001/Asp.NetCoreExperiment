96e36b67990a7586802ad65373a6f83573b1cab9
桂素伟 <axzxs2001@163.com>
2024-09-06 11:32:17 +0900

    fix

--- Asp.NetCoreExperiment/SemanticKernelArticle/Demo11-FuncationCalling/Program.cs
+++ Asp.NetCoreExperiment/SemanticKernelArticle/Demo11-FuncationCalling/Program.cs
@@ -1,18 +1,226 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using System.Text;
+#pragma warning disable SKEXP0001
+
+
+//await RunNonStreamingPromptWithAutoFunctionCallingAsync();
+//await RunStreamingPromptAutoFunctionCallingAsync();
+await RunNonStreamingChatAPIWithManualFunctionCallingAsync();
+//await RunStreamingChatAPIWithManualFunctionCallingAsync();
+//await RunNonStreamingPromptWithSimulatedFunctionAsync();
+//await RunStreamingChatWithAutoFunctionCallingAsync();
+
+async Task RunNonStreamingPromptWithAutoFunctionCallingAsync()
+{
+    Console.WriteLine("Auto function calling with a non-streaming prompt.");
+
+    Kernel kernel = CreateKernel();
+
+    OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+
+    Console.WriteLine(await kernel.InvokePromptAsync("鉴于当前的时间和天气，波士顿的天空可能是什么颜色？", new(settings)));
+    Console.WriteLine("--------------------------------------");
+}
+
+async Task RunStreamingPromptAutoFunctionCallingAsync()
+{
+    Console.WriteLine("Auto function calling with a streaming prompt.");
+
+    Kernel kernel = CreateKernel();
+
+    OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+
+    await foreach (StreamingKernelContent update in kernel.InvokePromptStreamingAsync("鉴于当前的时间和天气，波士顿的天空可能是什么颜色？", new(settings)))
+    {
+        Console.Write(update);
+    }
+    Console.WriteLine();
+    Console.WriteLine("--------------------------------------");
+}
+
+async Task RunNonStreamingChatAPIWithManualFunctionCallingAsync()
+{
+    Console.WriteLine("Manual function calling with a non-streaming prompt.");
+    Kernel kernel = CreateKernel();
+    IChatCompletionService chat = kernel.GetRequiredService<IChatCompletionService>();
+    OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.EnableKernelFunctions };
+
+    ChatHistory chatHistory = new();
+    chatHistory.AddUserMessage("鉴于当前的时间和天气，波士顿的天空可能是什么颜色？");
+
+    while (true)
+    {
+        ChatMessageContent result = await chat.GetChatMessageContentAsync(chatHistory, settings, kernel);
+        if (result.Content is not null)
+        {
+            Console.Write(result.Content);
+        }
+        IEnumerable<FunctionCallContent> functionCalls = FunctionCallContent.GetFunctionCalls(result);
+        if (!functionCalls.Any())
+        {
+            break;
+        }
+        chatHistory.Add(result);
+        foreach (FunctionCallContent functionCall in functionCalls)
+        {
+            try
+            {
+                FunctionResultContent resultContent = await functionCall.InvokeAsync(kernel);
+                Console.WriteLine($"FunctionName：{resultContent.FunctionName}，Return：{resultContent.InnerContent}");
+                chatHistory.Add(resultContent.ToChatMessage());
+            }
+            catch (Exception ex)
+            {
+                chatHistory.Add(new FunctionResultContent(functionCall, ex).ToChatMessage());
+                // or
+                //chatHistory.Add(new FunctionResultContent(functionCall, "Error details that LLM can reason about.").ToChatMessage());
+            }
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("--------------------------------------");
+}
+
+async Task RunStreamingChatAPIWithManualFunctionCallingAsync()
+{
+    Console.WriteLine("Manual function calling with a streaming prompt.");
+    Kernel kernel = CreateKernel();
+
+    IChatCompletionService chat = kernel.GetRequiredService<IChatCompletionService>();
+    OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.EnableKernelFunctions };
+    ChatHistory chatHistory = new();
+    chatHistory.AddUserMessage("鉴于当前的时间和天气，波士顿的天空可能是什么颜色？");
+
+    while (true)
+    {
+        AuthorRole? authorRole = null;
+        var fccBuilder = new FunctionCallContentBuilder();
+        await foreach (var streamingContent in chat.GetStreamingChatMessageContentsAsync(chatHistory, settings, kernel))
+        {
+            if (streamingContent.Content is not null)
+            {
+                Console.Write(streamingContent.Content);
+            }
+            authorRole ??= streamingContent.Role;
+            fccBuilder.Append(streamingContent);
+        }
+        var functionCalls = fccBuilder.Build();
+        if (!functionCalls.Any())
+        {
+            break;
+        }
+        var fcContent = new ChatMessageContent(role: authorRole ?? default, content: null);
+        chatHistory.Add(fcContent);
+
+        foreach (var functionCall in functionCalls)
+        {
+            fcContent.Items.Add(functionCall);
+            var functionResult = await functionCall.InvokeAsync(kernel);
+            Console.WriteLine($"FunctionName：{functionResult.FunctionName}，Return：{functionResult.InnerContent}");
+            chatHistory.Add(functionResult.ToChatMessage());
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("--------------------------------------");
+}
+
+async Task RunNonStreamingPromptWithSimulatedFunctionAsync()
+{
+    Console.WriteLine("Simulated function calling with a non-streaming prompt.");
+
+    Kernel kernel = CreateKernel();
+
+    IChatCompletionService chat = kernel.GetRequiredService<IChatCompletionService>();
+
+    OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.EnableKernelFunctions };
+
+    ChatHistory chatHistory = new();
+    chatHistory.AddUserMessage("鉴于当前的时间和天气，波士顿的天空可能是什么颜色？");
+
+    while (true)
+    {
+        ChatMessageContent result = await chat.GetChatMessageContentAsync(chatHistory, settings, kernel);
+        if (result.Content is not null)
+        {
+            Console.Write(result.Content);
+        }
+
+        chatHistory.Add(result);
+        IEnumerable<FunctionCallContent> functionCalls = FunctionCallContent.GetFunctionCalls(result);
+        if (!functionCalls.Any())
+        {
+            break;
+        }
+        foreach (FunctionCallContent functionCall in functionCalls)
+        {
+            FunctionResultContent resultContent = await functionCall.InvokeAsync(kernel); // Executing each function.
+
+            chatHistory.Add(resultContent.ToChatMessage());
+        }
+        FunctionCallContent simulatedFunctionCall = new("weather-alert", id: "call_123");
+        result.Items.Add(simulatedFunctionCall);
+
+        string simulatedFunctionResult = "A Tornado Watch has been issued, with potential for severe thunderstorms causing unusual sky colors like green, yellow, or dark gray. Stay informed and follow safety instructions from authorities.";
+        chatHistory.Add(new FunctionResultContent(simulatedFunctionCall, simulatedFunctionResult).ToChatMessage());
+
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+    Console.WriteLine("--------------------------------------");
+}
+
+async Task RunStreamingChatWithAutoFunctionCallingAsync()
+{
+    Console.WriteLine("Auto function calling with a streaming chat");
+
+    Kernel kernel = CreateKernel();
+
+    OpenAIPromptExecutionSettings settings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
+    IChatCompletionService chat = kernel.GetRequiredService<IChatCompletionService>();
+    ChatHistory chatHistory = new();
+    int iteration = 0;
+
+    while (true)
+    {
+        Console.Write("Question (Type \"quit\" to leave): ");
+
+        string question = iteration == 0 ? "鉴于当前的时间和天气，波士顿的天空可能是什么颜色？" : "quit";
+        if (question == "quit")
+        {
+            break;
+        }
+        chatHistory.AddUserMessage(question);
+        StringBuilder sb = new();
+        await foreach (var update in chat.GetStreamingChatMessageContentsAsync(chatHistory, settings, kernel))
+        {
+            if (update.Content is not null)
+            {
+                Console.Write(update.Content);
+                sb.Append(update.Content);
+            }
+        }
+        chatHistory.AddAssistantMessage(sb.ToString());
+        Console.WriteLine();
+        iteration++;
+    }
+    Console.WriteLine();
+    Console.WriteLine("--------------------------------------");
+}
+
 Kernel CreateKernel()
 {
-    var builder = Kernel.CreateBuilder();
     var chatModelId = "gpt-4o";
     var key = File.ReadAllText(@"C:\GPT\key.txt");
-    // i.e. gpt-3.5-turbo-1106 or gpt-4-1106-preview
-    builder.AddOpenAIChatCompletion("gpt-3.5-turbo-1106", key);
-    builder.Services.AddLogging(c =>
-    {
-        c.SetMinimumLevel(LogLevel.Debug);
-    });
-    var kernel = builder.Build();
+
+    var builder = Kernel.CreateBuilder();
+    builder.AddOpenAIChatCompletion(chatModelId, key);
+    Kernel kernel = builder.Build();
 
     kernel.ImportPluginFromFunctions("HelperFunctions",
     [
