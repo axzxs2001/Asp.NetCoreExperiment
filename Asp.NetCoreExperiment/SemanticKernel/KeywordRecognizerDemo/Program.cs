4389cef41e94604cdf4a2007e401953f0f773c3e
桂素伟 <axzxs2001@163.com>
2024-09-18 16:37:28 +0900

    fix

---
+++ Asp.NetCoreExperiment/SemanticKernel/KeywordRecognizerDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.CognitiveServices.Speech.Audio;
+using Microsoft.CognitiveServices.Speech;
+
+var keywordModel = KeywordRecognitionModel.FromFile("znzl.table");
+using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+using var keywordRecognizer = new KeywordRecognizer(audioConfig);
+while (true)
+{
+    Console.WriteLine("开始：");
+    KeywordRecognitionResult result = await keywordRecognizer.RecognizeOnceAsync(keywordModel);
+    Console.WriteLine(result.Text);
+}
