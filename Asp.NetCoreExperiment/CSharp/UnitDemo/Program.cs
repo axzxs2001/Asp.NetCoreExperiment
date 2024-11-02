40be23aaafa7145b611236903028c8bead227c23
gsw <axzxs2001@163.com>
2024-11-02 09:38:28 +0900

    fix

---
+++ Asp.NetCoreExperiment/CSharp/UnitDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Globalization;
+using UnitsNet;
+
+var janpese = new CultureInfo("ja-JP");
+var russian = new CultureInfo("ru-RU");
+var chinese = new CultureInfo("zh-CN");
+var oneKg = Mass.FromKilograms(1);
+
+Thread.CurrentThread.CurrentCulture = russian;
+Console.WriteLine(oneKg.ToString());
+Thread.CurrentThread.CurrentCulture = chinese;
+Console.WriteLine(oneKg.ToString());
+
+
+
