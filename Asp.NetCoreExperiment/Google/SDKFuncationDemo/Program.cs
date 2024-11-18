645003ae85b0e5b056c67b6ed7dff55f3e16e00f
桂素伟 <axzxs2001@163.com>
2024-11-18 16:04:11 +0900

    fix

---
+++ Asp.NetCoreExperiment/Google/SDKFuncationDemo/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Google.Api.Service service = new Google.Api.Service();
+var create = new Google.Cloud.Functions.V2.CreateFunctionRequest();
+create.Function = new Google.Cloud.Functions.V2.Function();
+create.Function.
