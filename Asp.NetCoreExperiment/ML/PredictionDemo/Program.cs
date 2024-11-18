e090b983f41cd647d150debc4eceef55abdc9753
桂素伟 <axzxs2001@163.com>
2024-11-18 16:21:41 +0900

    fix

---
+++ Asp.NetCoreExperiment/ML/PredictionDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.ML.Data;
+using Microsoft.ML;
+
+MLContext mlContext = new MLContext();
+
+// 1. 导入或创建训练数据
+HouseData[] houseData = {
+               new HouseData() { Size = 1.1F, Price = 1.2F },
+               new HouseData() { Size = 1.9F, Price = 2.3F },
+               new HouseData() { Size = 2.8F, Price = 3.0F },
+               new HouseData() { Size = 3.4F, Price = 3.7F } };
+IDataView trainingData = mlContext.Data.LoadFromEnumerable(houseData);
+
+// 2. 指定数据准备和模型训练管道
+var pipeline = mlContext.Transforms.Concatenate("Features", new[] { "Size" })
+    .Append(mlContext.Regression.Trainers.Sdca(labelColumnName: "Price", maximumNumberOfIterations: 100));
+
+
+// 3. 训练模型
+var model = pipeline.Fit(trainingData);
+
+// 4. 进行预测
+var size = new HouseData() { Size = 2.5F };
+var price = mlContext.Model.CreatePredictionEngine<HouseData, Prediction>(model).Predict(size);
+
+Console.WriteLine($"Predicted price for size: {size.Size * 1000} sq ft= {price.Price * 100:C}k");
+// Predicted price for size: 2500 sq ft= $261.98k
+
+
+public class HouseData
+{
+    public float Size { get; set; }
+    public float Price { get; set; }
+}
+
+public class Prediction
+{
+    [ColumnName("Score")]
+    public float Price { get; set; }
+}
