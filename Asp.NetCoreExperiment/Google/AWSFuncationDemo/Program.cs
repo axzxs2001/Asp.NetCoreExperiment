bfb77549cd1330a714ee95c6aba2b553d42f043d
桂素伟 <axzxs2001@163.com>
2024-11-19 09:18:12 +0900

    fix

---
+++ Asp.NetCoreExperiment/Google/AWSFuncationDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using Amazon.Lambda;
+using Amazon.Lambda.Model;
+
+string functionName = "MyLambdaFunction";
+string zipFilePath = "MyLambdaFunction.zip";
+string handler = "MyLambdaFunction::MyLambdaFunction.Function::FunctionHandler";
+string roleArn = "arn:aws:iam::123456789012:role/execution_role";
+Runtime runtime = Runtime.Dotnet6;
+
+
+using (var client = new AmazonLambdaClient())
+{
+    var request = new CreateFunctionRequest
+    {
+        FunctionName = functionName,
+        Handler = handler,
+        Role = roleArn,
+        Runtime = runtime,
+        Architectures = new List<string> { Architecture.Arm64 },
+        Code = new FunctionCode
+        {            
+            ZipFile = new MemoryStream(File.ReadAllBytes(zipFilePath))
+        },
+      
+    };   
+
+    try
+    {
+        var response = await client.CreateFunctionAsync(request);
+        Console.WriteLine($"Function ARN: {response.FunctionArn}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Error creating function: {ex.Message}");
+    }
+}
--- Asp.NetCoreExperiment/Google/SDKFuncationDemo/Program.cs
+++ Asp.NetCoreExperiment/Google/SDKFuncationDemo/Program.cs
@@ -4,4 +4,3 @@ Console.WriteLine("Hello, World!");
 Google.Api.Service service = new Google.Api.Service();
 var create = new Google.Cloud.Functions.V2.CreateFunctionRequest();
 create.Function = new Google.Cloud.Functions.V2.Function();
-create.Function.
