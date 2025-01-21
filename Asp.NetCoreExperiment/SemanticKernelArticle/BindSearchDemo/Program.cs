02c0adfd56d34da52666f6227db75925591f5d11
gsw <axzxs2001@163.com>
2025-01-21 17:42:52 +0900

    完成动态参数

--- Asp.NetCoreExperiment/SemanticKernelArticle/BindSearchDemo/Program.cs
+++ Asp.NetCoreExperiment/SemanticKernelArticle/BindSearchDemo/Program.cs
@@ -1,18 +1,34 @@
 ﻿
-#pragma warning disable SKEXP0050
-#pragma warning disable SKEXP0001
+#pragma warning disable 
 
+
+using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Data;
+using Microsoft.SemanticKernel.Plugins.Web;
 using Microsoft.SemanticKernel.Plugins.Web.Bing;
 
-var textSearch = new BingTextSearch(apiKey: File.ReadAllText(@"C:/gpt/bingkey.txt"));
 
+Kernel kernel = new();
+
+// Load native plugins
+var bing = kernel.ImportPluginFromType<SearchUrlPlugin>("search");
+
+// Run
+var ask = "What's the tallest building in Europe?";
+var result = await kernel.InvokeAsync(bing["BingSearchUrl"], new() { ["query"] = ask });
+
+Console.WriteLine(ask + "\n");
+Console.WriteLine(result.GetValue<string>());
+
+
+var textSearch = new BingTextSearch(apiKey: File.ReadAllText(@"C:/gpt/bingkey.txt"));
 var query = "What is the Semantic Kernel?";
+
 
 // Search and return results
 KernelSearchResults<string> searchResults = await textSearch.SearchAsync(query, new() { Top = 4 });
 var sn = 1;
-await foreach (string result in searchResults.Results)
+await foreach (string item in searchResults.Results)
 {
-    Console.WriteLine($"{sn++}、{result}");
+    Console.WriteLine($"{sn++}、{item}");
 }
---
+++ Asp.NetCoreExperiment/SemanticKernelArticle/FunctionCallingDemo/Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using Microsoft.SemanticKernel.Plugins.Web;
+
+
+
+var key = File.ReadAllText(@"C:\GPT\key.txt");
+var builder = Kernel.CreateBuilder();
+builder.Services.AddOpenAIChatCompletion("gpt-4o", key);
+
+const string p1Schema = """
+            {
+              "$schema": "http://json-schema.org/draft-07/schema#",
+              "type": "object",
+              "properties": {
+                 "category": {
+                    "type": "string",
+                    "description": "订单类别"
+                 },
+                 "date": {
+                     "type": "string",
+                    "description": "订单日期"
+                  }
+               },
+            "required": ["category"]
+            }
+            """;
+
+var function = KernelFunctionFactory.CreateFromMethod(
+           method: Select,
+           functionName: "QueryOrder",
+           description: "查询订单",
+           parameters: new KernelParameterMetadata[] {
+               new KernelParameterMetadata("pars"){               
+               Schema = KernelJsonSchema.Parse(p1Schema)               
+              }             
+           },
+           returnParameter: new KernelReturnParameterMetadata()
+           {
+               Description = "订单结果",
+               ParameterType = typeof(string)              
+           });
+
+builder.Plugins.AddFromFunctions("QueryOrder", new KernelFunction[] { function });
+builder.Plugins.AddFromFunctions("CurrentDate", new KernelFunction[] {
+    KernelFunctionFactory.CreateFromMethod(()=>DateTime .Now,"CurrentDate","获取当前日期")
+});
+var kernel = builder.Build();
+
+ChatHistory history = [];
+
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+var openAIPromptExecutionSettings = new OpenAIPromptExecutionSettings()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+
+while (true)
+{
+    Console.Write("用户 > ");
+    history.AddUserMessage(Console.ReadLine()!);
+    var result = chatCompletionService.GetStreamingChatMessageContentsAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+    Console.Write("助理 > ");
+    var contents = "";
+    AuthorRole role = AuthorRole.Assistant;
+    await foreach (var item in result)
+    {
+        item.Role = role;
+        contents += item.Content;
+        Console.Write(item.Content);
+    }
+    Console.WriteLine();    
+    history.AddMessage(role, contents);
+}
+
+async Task<string> Select(string pars)
+{
+    await Task.Delay(10);
+   // Console.WriteLine(pars);
+    var dic = System.Text.Json.JsonSerializer.Deserialize<Dictionary<string, string>>(pars);
+    if (dic != null)
+    {
+        foreach (var item in dic)
+        {
+            Console.WriteLine(item.Key + " : " + item.Value);
+        }
+    }
+    return $"汾酒：3000件";
+}
