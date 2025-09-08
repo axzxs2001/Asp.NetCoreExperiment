9ef44d7e17133cff601f8e3e78c8b22eaeb3afc4
gsw <axzxs2001@163.com>
2025-09-09 08:50:12 +0900

    fix

---
+++ Asp.NetCoreExperiment/DOTNET10/MoreTechs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+using System.Security;
+
+using var secure=new SecureString();
+secure.AppendChar('a');
+secure.AppendChar('b');
+secure.AppendChar('c');
+secure.AppendChar('d');
+secure.AppendChar('e');
+
+var cred=new NetworkCredential("user",secure);
+Console.WriteLine(cred.SecurePassword.ToString());
